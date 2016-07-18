e02894599cda6a2549b45d57851901c6cec99ea2
Otavio <otaviojava@java.net>
2016-07-17 22:40:14 -0300

    Changes bind method to allow any object to bind

--- diana-api/src/main/java/org/apache/diana/api/column/PreparedStatement.java
+++ diana-api/src/main/java/org/apache/diana/api/column/PreparedStatement.java
@@ -3,7 +3,6 @@ package org.apache.diana.api.column;
 
 import org.apache.diana.api.ExecuteAsyncQueryException;
 
-import java.io.Serializable;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -13,5 +12,5 @@ public interface PreparedStatement extends AutoCloseable {
 
     void executeQueryAsync(Consumer<List<ColumnEntity>> callBack) throws ExecuteAsyncQueryException;
 
-    <T extends Serializable> PreparedStatement bind(T... values);
+    PreparedStatement bind(Object... values);
 }
