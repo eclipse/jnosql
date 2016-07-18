9b13df7a29c8accb587a11ba017953549e30b5e9
Otavio <otaviojava@java.net>
2016-07-18 05:42:55 -0300

    Creates documentation to ColumnFamilyManagerFactory

--- diana-api/src/main/java/org/apache/diana/api/column/ColumnFamilyManagerFactory.java
+++ diana-api/src/main/java/org/apache/diana/api/column/ColumnFamilyManagerFactory.java
@@ -1,7 +1,15 @@
 package org.apache.diana.api.column;
 
-
+/**
+ * {@link ColumnFamilyManager} factory.
+ * When the application has finished using the column family manager factory, and/or at application shutdown, the application should close the column family manager factory.
+ */
 public interface ColumnFamilyManagerFactory extends AutoCloseable {
 
+    /**
+     * Creates a {@link ColumnFamilyManager} from database's name
+     * @param database a database name
+     * @return a new {@link ColumnFamilyManager} instance
+     */
     ColumnFamilyManager getColumnEntityManager(String database);
 }
