061899d86682b28d3b0b765a6419be4a7fbdb699
Otavio <otaviojava@java.net>
2016-07-19 18:41:21 -0300

    updates column package info

--- diana-api/src/main/java/org/apache/diana/api/column/package-info.java
+++ diana-api/src/main/java/org/apache/diana/api/column/package-info.java
@@ -1,5 +1,4 @@
 /**
- *
  * This package contains all objects to use a Column Family API. This API gonna focus in domain, in other words, ubiquitous language.
  * A column family is a NoSQL object that contains columns of related data. It is a tuple (pair)
  * that consists of a key-value pair, where the key is mapped to a value that is a set of columns.
@@ -7,6 +6,7 @@
  * Each column is a tuple (triplet) consisting of a column name, a value, and a timestamp. In a relational database table,
  * this data would be grouped together within a table with other non-related data.
  * Ref: https://en.wikipedia.org/wiki/Column_family
+ *
  * @author Otávio Santana
  */
 package org.apache.diana.api.column;
