117d3dc69c7c4a955c96c5f07323feb66bfcf27e
Otavio <otaviojava@java.net>
2016-07-19 18:27:15 -0300

    Adds sort documentatio

--- diana-api/src/main/java/org/apache/diana/api/Sort.java
+++ diana-api/src/main/java/org/apache/diana/api/Sort.java
@@ -3,6 +3,17 @@ package org.apache.diana.api;
 
 import java.util.Objects;
 
+/**
+ * This element represents a required order to be used in a query, it's has two attributes:
+ * <ul>
+ * * <li>The name - the field's name to be sorted</li>
+ * * <li>The type - the way to be sorted</li>
+ * </ul>
+ *
+ * @author Otávio Santana
+ * @see Sort#of(String, SortType)
+ * @see SortType
+ */
 public class Sort {
 
     private final String name;
@@ -14,6 +25,13 @@ public class Sort {
         this.type = type;
     }
 
+    /**
+     * Creates a bew Sort instance to be used in a NoSQL query.
+     *
+     * @param name - the field name be used in a sort process
+     * @param type - the way to be sorted
+     * @return a sort instance
+     */
     public static Sort of(String name, SortType type) {
         return new Sort(name, type);
     }
@@ -53,7 +71,20 @@ public class Sort {
         return sb.toString();
     }
 
-    public static enum SortType{
-        ASC, DESC;
+    /**
+     * The way to be sorted.
+     *
+     * @author Otávio Santana
+     * @see Sort
+     */
+    public static enum SortType {
+        /**
+         * The ascending way
+         */
+        ASC,
+        /**
+         * The descending way
+         */
+        DESC;
     }
 }
