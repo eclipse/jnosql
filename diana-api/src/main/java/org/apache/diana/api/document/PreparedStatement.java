27c42341e66c6945f5110aacbfc41b73fc87ead7
Otavio <otaviojava@java.net>
2016-07-19 21:44:42 -0300

    Adds new exception to prepare statment from document

--- diana-api/src/main/java/org/apache/diana/api/document/PreparedStatement.java
+++ diana-api/src/main/java/org/apache/diana/api/document/PreparedStatement.java
@@ -1,6 +1,8 @@
 package org.apache.diana.api.document;
 
 
+import org.apache.diana.api.ExecuteAsyncQueryException;
+
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -8,7 +10,7 @@ public interface PreparedStatement extends AutoCloseable {
 
     List<DocumentCollection> executeQuery();
 
-    void executeQueryAsync(Consumer<List<DocumentCollection>> callBack);
+    void executeQueryAsync(Consumer<List<DocumentCollection>> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException
 
     PreparedStatement bind(Object... values);
 }
