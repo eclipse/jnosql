461e2a439c1c8c148574ac37d44f59e3c5595e21
Otavio <otaviojava@java.net>
2016-07-19 21:38:36 -0300

    Adds document documentation

--- diana-api/src/main/java/org/apache/diana/api/document/DocumentCondition.java
+++ diana-api/src/main/java/org/apache/diana/api/document/DocumentCondition.java
@@ -2,25 +2,27 @@ package org.apache.diana.api.document;
 
 
 import org.apache.diana.api.Condition;
-import org.apache.diana.api.column.Column;
-import org.apache.diana.api.column.ColumnFamilyManager;
+import org.apache.diana.api.document.Column;
+import org.apache.diana.api.document.DocumentCondition;
+import org.apache.diana.api.document.ColumnFamilyManager;
+import org.apache.diana.api.document.DefaultDocumentCondition;
 
 import static org.apache.diana.api.Condition.*;
 
 /**
- * An unit condition  to run a column family query
+ * An unit condition  to run a document collection query
  *
  * @author Otávio Santana
- * @see ColumnFamilyManager#find(DocumentQuery)
+ * @see DocumentCollectionManager#find(DocumentQuery)
  */
 public interface DocumentCondition {
 
     /**
-     * Gets the column to be used in the query
+     * Gets the document to be used in the query
      *
-     * @return a column instance
+     * @return a document instance
      */
-    Column getColumn();
+    Document getDocument();
 
     /**
      * Gets the conditions to be used in the query
@@ -32,38 +34,86 @@ public interface DocumentCondition {
 
     /**
      * Creates a {@link DocumentCondition} that has a {@link Condition#EQUALS}, it means a query will scanning to a
-     * column family that has the same name and equals value informed in this column.
+     * document collection that has the same name and equals value informed in this document.
      *
-     * @param column a column instance
+     * @param document a column instance
      * @return a {@link DocumentCondition} with {@link Condition#EQUALS}
      * @throws NullPointerException when column is null
      */
-    static DocumentCondition eq(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, EQUALS);
+    static DocumentCondition eq(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, EQUALS);
     }
 
-    static DocumentCondition gt(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, GREATER_THAN);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#GREATER_THAN}, it means a query will scanning to a
+     * document collection that has the same name and the value  greater than informed in this document.
+     *
+     * @param document a column instance
+     * @return a {@link DocumentCondition} with {@link Condition#GREATER_THAN}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition gt(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, GREATER_THAN);
     }
 
-    static DocumentCondition gte(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, GREATER_EQUALS_THAN);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#GREATER_EQUALS_THAN}, it means a query will scanning to a
+     * document collection that has the same name and the value  greater or equals than informed in this document.
+     *
+     * @param document a column instance
+     * @return a {@link DocumentCondition} with {@link Condition#GREATER_EQUALS_THAN}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition gte(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, GREATER_EQUALS_THAN);
     }
 
-    static DocumentCondition lt(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, LESSER_THAN);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#LESSER_THAN}, it means a query will scanning to a
+     * document collection that has the same name and the value  lesser than informed in this document.
+     *
+     * @param document a column instance
+     * @return a {@link DocumentCondition} with {@link Condition#LESSER_THAN}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition lt(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, LESSER_THAN);
     }
 
-    static DocumentCondition lte(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, LESSER_EQUALS_THAN);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#LESSER_EQUALS_THAN}, it means a query will scanning to a
+     * document collection that has the same name and the value  lesser or equals than informed in this document.
+     *
+     * @param document a document instance
+     * @return a {@link DocumentCondition} with {@link Condition#LESSER_EQUALS_THAN}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition lte(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, LESSER_EQUALS_THAN);
     }
 
-    static DocumentCondition in(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, IN);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#IN}, it means a query will scanning to a
+     * document collection that has the same name and the value is within informed in this document.
+     *
+     * @param document a column instance
+     * @return a {@link DocumentCondition} with {@link Condition#IN}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition in(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, IN);
     }
 
-    static DocumentCondition like(Column column) throws NullPointerException {
-        return DefaultDocumentCondition.of(column, LIKE);
+    /**
+     * Creates a {@link DocumentCondition} that has a {@link Condition#LIKE}, it means a query will scanning to a
+     * document collection that has the same name and the value  is like than informed in this document.
+     *
+     * @param document a column instance
+     * @return a {@link DocumentCondition} with {@link Condition#LIKE}
+     * @throws NullPointerException when column is null
+     */
+    static DocumentCondition like(Document document) throws NullPointerException {
+        return DefaultDocumentCondition.of(document, LIKE);
     }
 
 }
