233f2c9f8f8477cf70082b8924002a6145be9fd3
Otavio <otaviojava@java.net>
2016-07-19 21:39:06 -0300

    Adds columncondition documentation

--- diana-api/src/main/java/org/apache/diana/api/column/ColumnCondition.java
+++ diana-api/src/main/java/org/apache/diana/api/column/ColumnCondition.java
@@ -40,26 +40,74 @@ public interface ColumnCondition {
         return DefaultColumnCondition.of(column, EQUALS);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#GREATER_THAN}, it means a query will scanning to a
+     * column family that has the same name and the value  greater than informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#GREATER_THAN}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition gt(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, GREATER_THAN);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#GREATER_EQUALS_THAN}, it means a query will scanning to a
+     * column family that has the same name and the value  greater or equals than informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#GREATER_EQUALS_THAN}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition gte(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, GREATER_EQUALS_THAN);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#LESSER_THAN}, it means a query will scanning to a
+     * column family that has the same name and the value  lesser than informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#LESSER_THAN}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition lt(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, LESSER_THAN);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#LESSER_EQUALS_THAN}, it means a query will scanning to a
+     * column family that has the same name and the value  lesser or equals than informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#LESSER_EQUALS_THAN}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition lte(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, LESSER_EQUALS_THAN);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#IN}, it means a query will scanning to a
+     * column family that has the same name and the value is within informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#IN}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition in(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, IN);
     }
 
+    /**
+     * Creates a {@link ColumnCondition} that has a {@link Condition#LIKE}, it means a query will scanning to a
+     * column family that has the same name and the value  is like than informed in this column.
+     *
+     * @param column a column instance
+     * @return a {@link ColumnCondition} with {@link Condition#LIKE}
+     * @throws NullPointerException when column is null
+     */
     static ColumnCondition like(Column column) throws NullPointerException {
         return DefaultColumnCondition.of(column, LIKE);
     }
