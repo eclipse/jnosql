fcf7230876d44c0264584e5c6bdaccec86376033
Otavio <otaviojava@java.net>
2016-07-20 21:08:51 -0300

    Creates reader package

---
+++ diana-api/src/main/java/org/apache/diana/api/reader/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * The {@link org.apache.diana.api.ReaderField} has the goal read a information from Value by Class informed from {@link org.apache.diana.api.Value#get(java.lang.Class)}.
+ * This package has all implementations supported by default in Diana Project
+ * @author  Otávio Santana
+ */
+package org.apache.diana.api.reader;
