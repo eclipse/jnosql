966ed6541a478323ad8f00e7c36c3ee43f2083f6
Otavio <otaviojava@java.net>
2016-07-20 22:16:19 -0300

    Creates number reader test

--- diana-api/src/main/java/org/apache/diana/api/reader/NumberReader.java
+++ diana-api/src/main/java/org/apache/diana/api/reader/NumberReader.java
@@ -3,15 +3,27 @@ package org.apache.diana.api.reader;
 
 import org.apache.diana.api.ReaderField;
 
+/**
+ * Class reader for {@link Number}, this converter first verify if the object is a Number instance,
+ * if it will return itself, otherwise convert to String and then to {@link Double}
+ *
+ * @author Otávio Santana
+ */
+final class NumberReader implements ReaderField {
 
-final class NumberReader implements ReaderField{
     @Override
     public boolean isCompatible(Class clazz) {
-        return Number.class.isAssignableFrom(clazz);
+        return Number.class.equals(clazz);
     }
 
     @Override
     public <T> T read(Class<T> clazz, Object value) {
-        return null;
+        if (Number.class.isInstance(value)) {
+            return (T) value;
+        } else {
+            return (T) Double.valueOf(value.toString());
+        }
     }
+
+
 }
--- diana-api/src/test/java/org/apache/diana/api/reader/NumberReaderTest.java
+++ diana-api/src/test/java/org/apache/diana/api/reader/NumberReaderTest.java
@@ -1,10 +1,35 @@
 package org.apache.diana.api.reader;
 
+import org.apache.diana.api.ReaderField;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by otaviojava on 20/07/16.
- */
+
 public class NumberReaderTest {
+
+    private ReaderField readerField;
+
+    @Before
+    public void init() {
+        readerField = new NumberReader();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenClassIsNumber() {
+        assertTrue(readerField.isCompatible(Number.class));
+        assertFalse(readerField.isCompatible(AtomicBoolean.class));
+        assertFalse(readerField.isCompatible(Boolean.class));
+    }
+
+    @Test
+    public void shouldConvert() {
+        assertEquals(10D, readerField.read(Number.class, 10.00));
+        assertEquals(10D, readerField.read(Number.class, "10"));
+    }
+
 
 }
