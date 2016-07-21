3a67330617d846d61cfff507d3131008c6110929
Otavio <otaviojava@java.net>
2016-07-20 21:56:55 -0300

    creates ByteReader

---
+++ diana-api/src/main/java/org/apache/diana/api/reader/ByteReader.java
@@ -0,0 +1,31 @@
+package org.apache.diana.api.reader;
+
+
+import org.apache.diana.api.ReaderField;
+
+/**
+ * Class to reads and converts to {@link Byte}, first it verify if is Double if yes return itself then verifies if is
+ * {@link Number} and use {@link Number#byteValue()} otherwise convert to {@link String} and then {@link Byte}
+ *
+ * @author Otávio Santana
+ */
+final class ByteReader implements ReaderField {
+
+    @Override
+    public boolean isCompatible(Class clazz) {
+        return Byte.class.equals(clazz);
+    }
+
+    @Override
+    public <T> T read(Class<T> clazz, Object value) {
+
+        if (Byte.class.isInstance(value)) {
+            return (T) value;
+        }
+        if (Number.class.isInstance(value)) {
+            return (T) Byte.valueOf(Number.class.cast(value).byteValue());
+        } else {
+            return (T) Byte.valueOf(value.toString());
+        }
+    }
+}
