3d82a661112ef31abda1a76f573f26648dde9770
Otavio <otaviojava@java.net>
2016-07-20 21:58:04 -0300

    Creates short reader

---
+++ diana-api/src/main/java/org/apache/diana/api/reader/ShortReader.java
@@ -0,0 +1,31 @@
+package org.apache.diana.api.reader;
+
+
+import org.apache.diana.api.ReaderField;
+
+/**
+ * Class to reads and converts to {@link Double}, first it verify if is Double if yes return itself then verifies if is
+ * {@link Number} and use {@link Number#doubleValue()} otherwise convert to {@link String} and then {@link Double}
+ *
+ * @author Otávio Santana
+ */
+final class ShortReader implements ReaderField {
+
+    @Override
+    public boolean isCompatible(Class clazz) {
+        return Double.class.equals(clazz);
+    }
+
+    @Override
+    public <T> T read(Class<T> clazz, Object value) {
+
+        if (Double.class.isInstance(value)) {
+            return (T) value;
+        }
+        if (Number.class.isInstance(value)) {
+            return (T) Double.valueOf(Number.class.cast(value).doubleValue());
+        } else {
+            return (T) Double.valueOf(value.toString());
+        }
+    }
+}
