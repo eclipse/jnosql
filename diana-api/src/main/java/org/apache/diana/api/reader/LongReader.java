a6909bd23584c6e1250754153ff152dfc7970cdc
Otavio <otaviojava@java.net>
2016-07-20 21:57:48 -0300

    Creates Long reader

---
+++ diana-api/src/main/java/org/apache/diana/api/reader/LongReader.java
@@ -0,0 +1,31 @@
+package org.apache.diana.api.reader;
+
+
+import org.apache.diana.api.ReaderField;
+
+/**
+ * Class to reads and converts to {@link Long}, first it verify if is Double if yes return itself then verifies if is
+ * {@link Number} and use {@link Number#longValue()} otherwise convert to {@link String} and then {@link Long}
+ *
+ * @author Otávio Santana
+ */
+final class LongReader implements ReaderField {
+
+    @Override
+    public boolean isCompatible(Class clazz) {
+        return Long.class.equals(clazz);
+    }
+
+    @Override
+    public <T> T read(Class<T> clazz, Object value) {
+
+        if (Long.class.isInstance(value)) {
+            return (T) value;
+        }
+        if (Number.class.isInstance(value)) {
+            return (T) Long.valueOf(Long.class.cast(value).longValue());
+        } else {
+            return (T) Long.valueOf(value.toString());
+        }
+    }
+}
