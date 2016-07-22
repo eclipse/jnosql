8e2bfed0e8fa697afc0fdcf0f8f488877627cbf1
Otavio <otaviojava@java.net>
2016-07-22 17:20:40 -0300

    Creates WriteFieldDecorator

---
+++ diana-api/src/main/java/org/apache/diana/api/writer/WriterFieldDecorator.java
@@ -0,0 +1,44 @@
+package org.apache.diana.api.writer;
+
+import org.apache.diana.api.WriterField;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+
+/**
+ * Decorators of all {@link WriterField} supported by Diana
+ *
+ * @author Otávio Santana
+ * @see WriterField
+ */
+public final class WriterFieldDecorator implements WriterField {
+
+    private static WriterFieldDecorator INSTANCE = new WriterFieldDecorator();
+
+    private final List<WriterField> writers = new ArrayList<>();
+
+    {
+        ServiceLoader.load(WriterField.class).forEach(writers::add);
+    }
+
+    public static WriterFieldDecorator getInstance() {
+        return INSTANCE;
+    }
+
+    private WriterFieldDecorator() {
+    }
+
+    @Override
+    public boolean isCompatible(Class clazz) {
+        return writers.stream().anyMatch(writerField -> writerField.isCompatible(clazz));
+    }
+
+    @Override
+    public Object write(Object object) {
+        Class clazz = object.getClass();
+        WriterField writerField = writers.stream().filter(r -> r.isCompatible(clazz)).findFirst().orElseThrow(
+                () -> new UnsupportedOperationException("The type " + clazz + " is not supported yet"));
+        return writerField.write(object);
+    }
+}
