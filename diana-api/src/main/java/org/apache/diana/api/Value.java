fcc90f9321c4487fd42c9f504505c65fa4f7178d
Otavio <otaviojava@java.net>
2016-07-21 21:27:42 -0300

    ADds value documentation

--- diana-api/src/main/java/org/apache/diana/api/Value.java
+++ diana-api/src/main/java/org/apache/diana/api/Value.java
@@ -9,29 +9,84 @@ import java.util.stream.Stream;
 
 public interface Value extends Serializable {
 
-
+    /**
+     * Returns the real value without conversion.
+     *
+     * @return the instance inside {@link Value}
+     */
     Object get();
 
     /**
-     * Converts Value to specified class
+     * Converts {@link Value#get()} to specified class
+     *
      * @param clazz the new class
-     * @param <T> the new instance type
+     * @param <T>   the new instance type
      * @return a new instance converted to informed class
-     * @throws NullPointerException when the class is null
+     * @throws NullPointerException          when the class is null
      * @throws UnsupportedOperationException when the type is unsupported
      * @see ReaderField
      */
-    <T> T get(Class<T> clazz)throws  NullPointerException, UnsupportedOperationException;
+    <T> T get(Class<T> clazz) throws NullPointerException, UnsupportedOperationException;
 
-    <T> List<T> getList(Class<T> clazz);
 
-    <T> Stream<T> getStream(Class<T> clazz);
+    /**
+     * Converts the {@link Value#get()} in a {@link List},
+     * if the value is not a Iterable extension it will be converted to a list with just one element.
+     *
+     * @param clazz type to be converted
+     * @param <T>   - type
+     * @return a instance list
+     * @throws NullPointerException          - when clazz is null
+     * @throws UnsupportedOperationException when the type is unsupported
+     */
+    <T> List<T> getList(Class<T> clazz) throws NullPointerException, UnsupportedOperationException;
 
-    <T> Set<T> getSet(Class<T> clazz);
+    /**
+     * Converts {@link Value#get()} in a {@link Set} type of class,
+     * if the value is not a Iterable extension it will be converted to a Set with just one element.
+     * type of class
+     *
+     * @param clazz type to be converted
+     * @param <T>   - type
+     * @return a instance of {@link Set}
+     * @throws NullPointerException          - when clazz is null
+     * @throws UnsupportedOperationException when the type is unsupported
+     */
+    <T> Set<T> getSet(Class<T> clazz) throws NullPointerException, UnsupportedOperationException;
 
-    <K, V > Map<K, V> getSet(Class<K> keyClass, Class<V> valueClass);
 
-    static Value of(Object value) {
+    /**
+     * Converts {@link Value#get()} to a {@link Map}
+     *
+     * @param keyClass   - the key class
+     * @param valueClass - the value class
+     * @param <K>        - the key type
+     * @param <V>        - the value type
+     * @return a {@link Map} instance
+     * @throws NullPointerException          when clazz is null
+     * @throws UnsupportedOperationException when the type is unsupported
+     */
+    <K, V> Map<K, V> getMap(Class<K> keyClass, Class<V> valueClass) throws NullPointerException, UnsupportedOperationException;
+
+    /**
+     * Converts {@link Value#get()} in a {@link Stream} type of class,
+     * @param clazz the class
+     * @param <T> the type
+     * @return a {@link Stream} instance
+     */
+    default <T> Stream<T> getStream(Class<T> clazz) {
+        return getList(clazz).stream();
+    }
+
+
+    /**
+     * Creates a new {@link Value} instance
+     *
+     * @param value - the information to {@link Value}
+     * @return a {@link Value} instance within a value informed
+     * @throws NullPointerException when the parameter is null
+     */
+    static Value of(Object value) throws NullPointerException {
         return DefaultValue.of(value);
     }
 
