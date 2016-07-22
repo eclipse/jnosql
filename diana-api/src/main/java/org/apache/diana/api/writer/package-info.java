68fa2eaa5e3b666d445b9762d5146533ce84b936
Otavio <otaviojava@java.net>
2016-07-22 16:56:55 -0300

    Adds writer package

---
+++ diana-api/src/main/java/org/apache/diana/api/writer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains all WriterField supported by API by default.
+ *@see org.apache.diana.api.WriterField
+ * @author Otávio Santana
+ */
+package org.apache.diana.api.writer;
