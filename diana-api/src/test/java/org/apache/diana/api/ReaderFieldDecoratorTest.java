446f3ae121f50041aa9e9a81f42537cf25fad730
Otavio <otaviojava@java.net>
2016-07-22 17:26:52 -0300

    Fixes ReaderFieldDecoratorTest tests

--- diana-api/src/test/java/org/apache/diana/api/ReaderFieldDecoratorTest.java
+++ diana-api/src/test/java/org/apache/diana/api/ReaderFieldDecoratorTest.java
@@ -15,29 +15,29 @@ public class ReaderFieldDecoratorTest {
 
     @Test
     public void shouldConvert() {
-        Number convert = serviceLoader.convert(Number.class, "10D");
+        Number convert = serviceLoader.read(Number.class, "10D");
         assertEquals(Double.valueOf(10D), convert);
     }
 
     @Test
     public void shoulCastObject() {
-        Bean name = serviceLoader.convert(Bean.class, new Bean("name"));
+        Bean name = serviceLoader.read(Bean.class, new Bean("name"));
         Assert.assertEquals(name.getClass(), Bean.class);
     }
 
     @Test(expected = UnsupportedOperationException.class)
     public void shoulReturnErrorWhenTypeIsNotSupported() {
-        Bean name = serviceLoader.convert(Bean.class, "name");
+        Bean name = serviceLoader.read(Bean.class, "name");
     }
 
     @Test
     public void shouldReturnIfIsCompatible() {
-        assertTrue(serviceLoader.hasSupport(Integer.class));
+        assertTrue(serviceLoader.isCompatible(Integer.class));
     }
 
     @Test
     public void shouldReturnIfIsNotCompatible() {
-        assertFalse(serviceLoader.hasSupport(Bean.class));
+        assertFalse(serviceLoader.isCompatible(Bean.class));
     }
 
 
