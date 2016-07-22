a5a33b26e32e3894516c986c11afbaf056a99397
Otavio <otaviojava@java.net>
2016-07-22 19:45:06 -0300

    Updates ColumnFamily documentation

--- diana-api/src/main/java/org/apache/diana/api/column/ColumnFamilyManager.java
+++ diana-api/src/main/java/org/apache/diana/api/column/ColumnFamilyManager.java
@@ -13,35 +13,112 @@ import java.util.function.Consumer;
 public interface ColumnFamilyManager extends AutoCloseable {
 
     /**
-     * Save a Column family
-     * @param columnFamilyEntity column family to be saved
+     * Saves a Column family entity
+     * @param entity column family to be saved
+     * @return the entity saved
      */
-    void save(ColumnFamilyEntity columnFamilyEntity);
+    ColumnFamilyEntity save(ColumnFamilyEntity entity);
 
-    void saveAsync(ColumnFamilyEntity columnFamilyEntity) throws ExecuteAsyncQueryException, UnsupportedOperationException;
+    /**
+     * Saves an entity asynchronously
+     * @param entity entity to be saved
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to save asynchronous
+     */
+    void saveAsync(ColumnFamilyEntity entity) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
-    void saveAsync(ColumnFamilyEntity columnFamilyEntity, Consumer<Void> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
+    /**
+     * Saves an entity asynchronously
+     * @param entity entity to be saved
+     * @param callBack the callback, when the process is finished will call this instance returning the saved entity within parameters
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to save asynchronous
+     */
+    void saveAsync(ColumnFamilyEntity entity, Consumer<ColumnFamilyEntity> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
+    /**
+     * Updates a Column family entity
+     * @param entity column family to be saved
+     * @return the entity updated
+     */
+    ColumnFamilyEntity update(ColumnFamilyEntity entity);
+    /**
+     * Updates an entity asynchronously
+     * @param entity entity to be saved
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to update asynchronous
+     */
+    void updateAsync(ColumnFamilyEntity entity) throws ExecuteAsyncQueryException, UnsupportedOperationException;
+    /**
+     * Updates an entity asynchronously
+     * @param entity entity to be saved
+     * @param callBack the callback, when the process is finished will call this instance returning the updated entity within parameters
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to update asynchronous
+     */
+    void updateAsync(ColumnFamilyEntity entity, Consumer<ColumnFamilyEntity> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
-    void update(ColumnFamilyEntity columnFamilyEntity);
-
-    void updateAsync(ColumnFamilyEntity columnFamilyEntity) throws ExecuteAsyncQueryException, UnsupportedOperationException;
-
-    void updateAsync(ColumnFamilyEntity columnFamilyEntity, Consumer<Void> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
-
+    /**
+     * Deleted an entity
+     * @param query query to delete an entity
+     */
     void delete(ColumnQuery query);
 
+    /**
+     * deleted an entity asynchronously
+     * @param query query to delete an entity
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to delete asynchronous
+     */
     void deleteAsync(ColumnQuery query) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
+    /**
+     * deleted an entity asynchronously
+     * @param query query to delete an entity
+     * @param callBack the callback, when the process is finished will call this instance returning the null within parameters
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to delete asynchronous
+     */
     void deleteAsync(ColumnQuery query, Consumer<Void> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
+    /**
+     * Finds {@link ColumnFamilyEntity} from query
+     * @param query - query to figure out entities
+     * @return entities found by query
+     */
     List<ColumnFamilyEntity> find(ColumnQuery query);
 
+    /**
+     * Finds {@link ColumnFamilyEntity} from query asynchronously
+     * @param query query to find entities
+     * @param callBack the callback, when the process is finished will call this instance returning the result of query within parameters
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to save asynchronous
+     */
     void findAsync(ColumnQuery query, Consumer<List<ColumnFamilyEntity>> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
+    /**
+     * Executes a native query from database, this query may be difference between kind of database.
+     * @param query query to be executed
+     * @return the result of query
+     * @throws UnsupportedOperationException when the database does not have support to run native query
+     */
     List<ColumnFamilyEntity> nativeQuery(String query) throws UnsupportedOperationException;
 
+    /**
+     * Executes a native query from database, this query may be difference between kind of database and run it asynchronously.
+     * @param query query to be executed
+     * @param callBack the callback, when the process is finished will call this instance returning the result of query within parameters
+     * @throws ExecuteAsyncQueryException when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to run native query async.
+     */
     void nativeQueryAsync(String query, Consumer<List<ColumnFamilyEntity>> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
+    /**
+     * Creates a {@link PreparedStatement} from a native query
+     * @param query a query to be executed
+     * @return a {@link PreparedStatement}
+     * @throws UnsupportedOperationException
+     */
     PreparedStatement nativeQueryPrepare(String query) throws UnsupportedOperationException;
 
 }
--- diana-api/src/main/java/org/apache/diana/api/column/PreparedStatement.java
+++ diana-api/src/main/java/org/apache/diana/api/column/PreparedStatement.java
@@ -6,11 +6,36 @@ import org.apache.diana.api.ExecuteAsyncQueryException;
 import java.util.List;
 import java.util.function.Consumer;
 
+
+/**
+ * Represents a prepared statement, a query with bound variables that has been
+ * prepared (pre-parsed) by the database.
+ *
+ * @author Otávio Santana
+ */
 public interface PreparedStatement extends AutoCloseable {
 
+    /**
+     * Executes the query
+     *
+     * @return the result
+     */
     List<ColumnFamilyEntity> executeQuery();
 
+    /**
+     * Executes the query asynchronously
+     *
+     * @param callBack the callback, when the process is finished will call this instance returning the result of query within parameters
+     * @throws ExecuteAsyncQueryException    when there is a async error
+     * @throws UnsupportedOperationException when the database does not have support to run async.
+     */
     void executeQueryAsync(Consumer<List<ColumnFamilyEntity>> callBack) throws ExecuteAsyncQueryException, UnsupportedOperationException;
 
+    /**
+     * Bind the properties within query
+     *
+     * @param values the values to be put in the query
+     * @return a  {@link PreparedStatement} binded with the parameters to execute a query
+     */
     PreparedStatement bind(Object... values);
 }
