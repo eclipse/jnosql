f694025d763b0c7494df43edd04ebb26024c50c1
Otavio <otaviojava@java.net>
2016-07-23 05:48:04 -0300

    Adds DocumentConfiguration

---
+++ diana-api/src/main/java/org/apache/diana/api/document/DocumentConfiguration.java
@@ -0,0 +1,26 @@
+package org.apache.diana.api.document;
+
+import java.util.Map;
+
+/**
+ * The diana configuration to create a {@link DocumentCollectionManagerFactory}
+ *
+ * @author Otávio Santana
+ */
+public interface DocumentConfiguration {
+
+    /**
+     * Reads configuration from Map and creates a {@link DocumentCollectionManagerFactory} instance.
+     *
+     * @param configurations
+     * @return a {@link DocumentCollectionManagerFactory} instance
+     */
+    DocumentCollectionManagerFactory getManagerFactory(Map<String, String> configurations);
+
+    /**
+     * Reads configuration either from default configuration or a file defined by NoSQL provider and then creates a {@link DocumentCollectionManagerFactory} instance.
+     *
+     * @return
+     */
+    DocumentCollectionManagerFactory getManagerFactory();
+}
