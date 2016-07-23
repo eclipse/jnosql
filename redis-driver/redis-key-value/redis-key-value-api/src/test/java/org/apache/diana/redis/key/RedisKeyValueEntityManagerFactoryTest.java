0bf6b13b427ce425378e6d289f15fc3e5d6bdd80
Otavio <otaviojava@java.net>
2016-07-23 07:10:11 -0300

    Creates Redis RedisKeyValueEntityManagerFactoryTest

---
+++ redis-driver/redis-key-value/redis-key-value-api/src/test/java/org/apache/diana/redis/key/RedisKeyValueEntityManagerFactoryTest.java
@@ -0,0 +1,58 @@
+package org.apache.diana.redis.key;
+
+import org.apache.diana.api.key.KeyValueConfiguration;
+import org.apache.diana.api.key.KeyValueEntityManager;
+import org.apache.diana.api.key.KeyValueEntityManagerFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import static org.junit.Assert.assertNotNull;
+
+
+public class RedisKeyValueEntityManagerFactoryTest {
+
+    public static final String BUCKET_NAME = "bucketName";
+    private KeyValueEntityManagerFactory managerFactory;
+
+    @Before
+    public void setUp() {
+        KeyValueConfiguration configuration = new RedisConfiguration();
+        managerFactory = configuration.getManagerFactory();
+    }
+
+    @Test
+    public void shouldCreateKeyValueEntityManager(){
+        KeyValueEntityManager keyValueEntityManager = managerFactory.getKeyValueEntityManager(BUCKET_NAME);
+        assertNotNull(keyValueEntityManager);
+    }
+
+    @Test
+    public void shouldCreateMap(){
+        Map<String, String> map = managerFactory.getMap(BUCKET_NAME, String.class, String.class);
+        assertNotNull(map);
+    }
+
+    @Test
+    public void shouldCreateSet(){
+        Set<String> set = managerFactory.getSet(BUCKET_NAME, String.class);
+        assertNotNull(set);
+    }
+
+    @Test
+    public void shouldCreateList(){
+        List<String> list = managerFactory.getList(BUCKET_NAME, String.class);
+        assertNotNull(list);
+    }
+
+    @Test
+    public void shouldCreateQueue(){
+        Queue<String> queue = managerFactory.getQueue(BUCKET_NAME, String.class);
+        assertNotNull(queue);
+    }
+
+}
