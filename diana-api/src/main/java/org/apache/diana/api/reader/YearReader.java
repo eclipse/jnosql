f4d5a8636b45470f03dd47263f68fe0ec41f0904
Marcelo de Souza <marcelo.vieira@involves.com.br>
2016-07-28 09:11:33 -0300

    Changing the Calendar Reader class to add suggested adjustments and create YearReader with the unit test.

--- diana-api/src/main/java/org/apache/diana/api/reader/YearReader.java
+++ diana-api/src/main/java/org/apache/diana/api/reader/YearReader.java
@@ -1,7 +1,30 @@
 package org.apache.diana.api.reader;
 
+import org.apache.diana.api.ReaderField;
+
+import java.time.Year;
+import java.util.Calendar;
+
 /**
- * Created by marcelo on 28/07/16.
+ * Class to reads and converts to {@link Year}, first it verify if is Year if yes return itself otherwise convert to {@link String}
+ *
+ * @author Marcelo de Souza
  */
-public class YearReader {
+public final class YearReader  implements ReaderField {
+
+    @Override
+    public boolean isCompatible(Class clazz) {
+        return Year.class.equals(clazz);
+    }
+
+    @Override
+    public <T> T read(Class<T> clazz, Object value) {
+
+        if (Year.class.isInstance(value)) {
+            return (T) value;
+        }
+
+        Year year = Year.parse(value.toString());
+        return (T) year;
+    }
 }
