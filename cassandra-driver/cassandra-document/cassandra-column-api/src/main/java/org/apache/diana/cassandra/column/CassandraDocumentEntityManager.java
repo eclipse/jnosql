2ddea7dc515f8dea5590d99b1251fe01a73dbd36
Daniel Cunha <danielsoro@apache.org>
2016-07-30 14:55:31 -0300

    DIANA-31 Configure checkstyle

--- cassandra-driver/cassandra-document/cassandra-column-api/src/main/java/org/apache/diana/cassandra/column/CassandraDocumentEntityManager.java
+++ cassandra-driver/cassandra-document/cassandra-column-api/src/main/java/org/apache/diana/cassandra/column/CassandraDocumentEntityManager.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.diana.cassandra.column;
@@ -108,13 +110,15 @@ class CassandraDocumentEntityManager implements ColumnFamilyManager {
     public List<ColumnFamilyEntity> find(ColumnQuery query) {
         BuiltStatement select = QueryUtils.add(query, keyspace);
         ResultSet resultSet = session.execute(select);
-        return resultSet.all().stream().map(row -> CassandraConverter.toDocumentEntity(row)).collect(Collectors.toList());
+        return resultSet.all().stream().map(row -> CassandraConverter.toDocumentEntity(row))
+                .collect(Collectors.toList());
     }
 
 
 
     @Override
-    public void findAsync(ColumnQuery query, Consumer<List<ColumnFamilyEntity>> consumer) throws ExecuteAsyncQueryException, UnsupportedOperationException {
+    public void findAsync(ColumnQuery query, Consumer<List<ColumnFamilyEntity>> consumer)
+            throws ExecuteAsyncQueryException, UnsupportedOperationException {
         BuiltStatement select = QueryUtils.add(query, keyspace);
         ResultSetFuture resultSet = session.executeAsync(select);
         CassandraReturnQueryAsync executeAsync = new CassandraReturnQueryAsync(resultSet, consumer);
@@ -124,11 +128,13 @@ class CassandraDocumentEntityManager implements ColumnFamilyManager {
     @Override
     public List<ColumnFamilyEntity> nativeQuery(String query) {
         ResultSet resultSet = session.execute(query);
-        return resultSet.all().stream().map(row -> CassandraConverter.toDocumentEntity(row)).collect(Collectors.toList());
+        return resultSet.all().stream().map(row -> CassandraConverter.toDocumentEntity(row))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void nativeQueryAsync(String query, Consumer<List<ColumnFamilyEntity>> consumer) throws ExecuteAsyncQueryException {
+    public void nativeQueryAsync(String query, Consumer<List<ColumnFamilyEntity>> consumer)
+            throws ExecuteAsyncQueryException {
         ResultSetFuture resultSet = session.executeAsync(query);
         CassandraReturnQueryAsync executeAsync = new CassandraReturnQueryAsync(resultSet, consumer);
         resultSet.addListener(executeAsync, executor);
--- cassandra-driver/cassandra-document/cassandra-column-api/src/main/java/org/apache/diana/cassandra/column/QueryUtils.java
+++ cassandra-driver/cassandra-document/cassandra-column-api/src/main/java/org/apache/diana/cassandra/column/QueryUtils.java
@@ -1,25 +1,32 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.diana.cassandra.column;
 
 
-import com.datastax.driver.core.querybuilder.*;
-import com.google.common.collect.Iterables;
+import com.datastax.driver.core.querybuilder.BuiltStatement;
+import com.datastax.driver.core.querybuilder.Clause;
+import com.datastax.driver.core.querybuilder.Delete;
+import com.datastax.driver.core.querybuilder.Insert;
+import com.datastax.driver.core.querybuilder.Ordering;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
 import org.apache.diana.api.Condition;
 import org.apache.diana.api.Sort;
 import org.apache.diana.api.column.Column;
@@ -107,7 +114,8 @@ final class QueryUtils {
             case AND:
             case OR:
             default:
-                throw new UnsupportedOperationException("The columnCondition " + condition + " is not supported in cassandra column driver");
+                throw new UnsupportedOperationException("The columnCondition " + condition +
+                        " is not supported in cassandra column driver");
         }
     }
 
