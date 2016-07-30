dfcffdfcb99dea433d9a4e5ac3267cd0c5d0a76e
Daniel Cunha <danielsoro@apache.org>
2016-07-30 14:42:46 -0300

    DIANA-31 Configure checkstyle

--- hazelcast-driver/redis-key-value/hazelcast-key-value-api/src/main/java/org/apache/diana/hazelcast/key/HazelCastKeyValueConfiguration.java
+++ hazelcast-driver/redis-key-value/hazelcast-key-value-api/src/main/java/org/apache/diana/hazelcast/key/HazelCastKeyValueConfiguration.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.diana.hazelcast.key;
@@ -38,7 +40,8 @@ public class HazelCastKeyValueConfiguration implements KeyValueConfiguration {
     @Override
     public BucketManagerFactory getManagerFactory(Map<String, String> configurations) {
 
-        List<String> servers = configurations.keySet().stream().filter(s -> s.startsWith("hazelcast-hoster-")).collect(Collectors.toList());
+        List<String> servers = configurations.keySet().stream().filter(s -> s.startsWith("hazelcast-hoster-"))
+                .collect(Collectors.toList());
 
         Config config = new Config(configurations.getOrDefault("hazelcast-instanceName", "hazelcast-instanceName"));
         HazelcastInstance hazelcastInstance = Hazelcast.getOrCreateHazelcastInstance(config);
@@ -49,9 +52,11 @@ public class HazelCastKeyValueConfiguration implements KeyValueConfiguration {
     public BucketManagerFactory getManagerFactory() {
         try {
             Properties properties = new Properties();
-            InputStream stream = HazelCastKeyValueConfiguration.class.getClassLoader().getResourceAsStream(HAZELCAST_FILE_CONFIGURATION);
+            InputStream stream = HazelCastKeyValueConfiguration.class.getClassLoader()
+                    .getResourceAsStream(HAZELCAST_FILE_CONFIGURATION);
             properties.load(stream);
-            Map<String, String> collect = properties.keySet().stream().collect(Collectors.toMap(Object::toString, s -> properties.get(s).toString()));
+            Map<String, String> collect = properties.keySet().stream()
+                    .collect(Collectors.toMap(Object::toString, s -> properties.get(s).toString()));
             return getManagerFactory(collect);
         } catch (IOException e) {
             throw new RuntimeException(e);
