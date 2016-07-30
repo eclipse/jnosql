9a4c5a9518cb8eb6e007ac51cb85a2aa80387d81
Daniel Cunha <danielsoro@apache.org>
2016-07-30 14:51:33 -0300

    DIANA-31 Configure checkstyle

--- mongodb-driver/mongodb-document/mongodb-document-api/src/main/java/org/apache/diana/mongodb/document/DocumentQueryConversor.java
+++ mongodb-driver/mongodb-document/mongodb-document-api/src/main/java/org/apache/diana/mongodb/document/DocumentQueryConversor.java
@@ -1,18 +1,20 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.apache.diana.mongodb.document;
@@ -27,6 +29,9 @@ import org.bson.conversions.Bson;
 import java.util.List;
 
 final class DocumentQueryConversor {
+
+    private DocumentQueryConversor() {
+    }
 
     public static Bson convert(DocumentCondition condition) {
         Document document = condition.getDocument();
@@ -48,12 +53,15 @@ final class DocumentQueryConversor {
                 return Filters.regex(document.getName(), value.toString());
             case AND:
                 List<Document> andList = condition.getDocument().getValue().getList(Document.class);
-                return Filters.and(andList.stream().map(d -> new BasicDBObject(d.getName(), d.getValue().get())).toArray(BasicDBObject[]::new));
+                return Filters.and(andList.stream()
+                        .map(d -> new BasicDBObject(d.getName(), d.getValue().get())).toArray(BasicDBObject[]::new));
             case OR:
                 List<Document> orList = condition.getDocument().getValue().getList(Document.class);
-                return Filters.or(orList.stream().map(d -> new BasicDBObject(d.getName(), d.getValue().get())).toArray(BasicDBObject[]::new));
+                return Filters.or(orList.stream()
+                        .map(d -> new BasicDBObject(d.getName(), d.getValue().get())).toArray(BasicDBObject[]::new));
             default:
-                throw new UnsupportedOperationException("The condition " + condition.getCondition()+ " is not supported from mongoDB diana driver" );
+                throw new UnsupportedOperationException("The condition " + condition.getCondition()
+                        + " is not supported from mongoDB diana driver" );
         }
     }
 }
--- mongodb-driver/mongodb-document/mongodb-document-api/src/main/java/org/apache/diana/mongodb/document/MongoDBDocumentCollectionManagerFactory.java
+++ mongodb-driver/mongodb-document/mongodb-document-api/src/main/java/org/apache/diana/mongodb/document/MongoDBDocumentCollectionManagerFactory.java
@@ -20,7 +20,6 @@
 package org.apache.diana.mongodb.document;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import org.apache.diana.api.document.DocumentCollectionManager;
 import org.apache.diana.api.document.DocumentCollectionManagerFactory;
 
@@ -29,18 +28,20 @@ class MongoDBDocumentCollectionManagerFactory implements DocumentCollectionManag
     private final MongoClient mongoClient;
     private final com.mongodb.async.client.MongoClient asyncMongoDatabase;
 
-    MongoDBDocumentCollectionManagerFactory(MongoClient mongoClient, com.mongodb.async.client.MongoClient asyncMongoDatabase) {
+    MongoDBDocumentCollectionManagerFactory(MongoClient mongoClient,
+                                            com.mongodb.async.client.MongoClient asyncMongoDatabase) {
         this.mongoClient = mongoClient;
         this.asyncMongoDatabase = asyncMongoDatabase;
     }
 
     @Override
     public DocumentCollectionManager getDocumentEntityManager(String database) {
-        return new MongoDBDocumentCollectionManager(mongoClient.getDatabase(database), asyncMongoDatabase.getDatabase(database));
+        return new MongoDBDocumentCollectionManager(mongoClient.getDatabase(database),
+                asyncMongoDatabase.getDatabase(database));
     }
 
     @Override
-    public void close()  {
+    public void close() {
         mongoClient.close();
     }
 
