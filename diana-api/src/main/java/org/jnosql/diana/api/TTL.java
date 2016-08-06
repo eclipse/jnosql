26d5043352d638ab22d298a603767508bf1ffba7
Otavio <otaviojava@java.net>
2016-08-06 07:22:17 -0300

    Adds constructors in TTL

--- diana-api/src/main/java/org/jnosql/diana/api/TTL.java
+++ diana-api/src/main/java/org/jnosql/diana/api/TTL.java
@@ -1,6 +1,7 @@
 package org.jnosql.diana.api;
 
 import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Entity that defines time to live an entity
@@ -8,6 +9,77 @@ import java.io.Serializable;
  * @author Otávio Santana
  */
 public interface TTL extends Serializable {
+
+    /**
+     * Returns TTL in nanoseconds
+     *
+     * @param duration the duration
+     * @return a TTL instance in nanoseconds
+     */
+    static TTL ofNanos(long duration) {
+        return new DefaultTTL(duration, TimeUnit.NANOSECONDS);
+    }
+
+    /**
+     * Returns TTL in microseconds
+     *
+     * @param duration the duration
+     * @return a TTL instance in microseconds
+     */
+    static TTL ofMicros(long duration) {
+        return new DefaultTTL(duration, TimeUnit.MICROSECONDS);
+    }
+
+    /**
+     * Returns TTL in milliseconds
+     *
+     * @param duration the duration
+     * @return a TTL instance in milliseconds
+     */
+    static TTL ofMillis(long duration) {
+        return new DefaultTTL(duration, TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * Returns TTL in seconds
+     *
+     * @param duration the duration
+     * @return a TTL instance in seconds
+     */
+    static TTL ofSeconds(long duration) {
+        return new DefaultTTL(duration, TimeUnit.SECONDS);
+    }
+
+    /**
+     * Returns TTL in minutes
+     *
+     * @param duration the duration
+     * @return a TTL instance in minutes
+     */
+    static TTL ofMinutes(long duration) {
+        return new DefaultTTL(duration, TimeUnit.MINUTES);
+    }
+
+    /**
+     * Returns TTL in hours
+     *
+     * @param duration the duration
+     * @return a TTL instance in hours
+     */
+    static TTL ofHours(long duration) {
+        return new DefaultTTL(duration, TimeUnit.HOURS);
+    }
+
+    /**
+     * Returns TTL in days
+     *
+     * @param duration the duration
+     * @return a TTL instance in days
+     */
+    static TTL ofDays(long duration) {
+        return new DefaultTTL(duration, TimeUnit.DAYS);
+    }
+
 
     /**
      * Returns TTL converted in nanoseconds
