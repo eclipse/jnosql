3df4644a388dfdc254c419f3cde4a365fec9b956
Otavio <otaviojava@java.net>
2016-08-21 12:09:15 -0300

    Adds new NonUniqueResult exception

---
+++ diana-api/src/main/java/org/jnosql/diana/api/NonUniqueResultException.java
@@ -0,0 +1,36 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.diana.api;
+
+
+/**
+ * Thrown by the persistence provider an exception when is executed
+ * on a query and there is more than one result from the query.
+ */
+public class NonUniqueResultException extends RuntimeException {
+
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public NonUniqueResultException(String message) {
+        super(message);
+    }
+}
