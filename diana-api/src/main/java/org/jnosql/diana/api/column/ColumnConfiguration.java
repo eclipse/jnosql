f1755b273e2de7c8d030fa6bda47a617a8b9aa3b
Otavio <otaviojava@java.net>
2016-09-05 20:18:04 -0300

    Removes creation from map in Configuration

--- diana-api/src/main/java/org/jnosql/diana/api/column/ColumnConfiguration.java
+++ diana-api/src/main/java/org/jnosql/diana/api/column/ColumnConfiguration.java
@@ -27,14 +27,6 @@ import java.util.Map;
 public interface ColumnConfiguration {
 
     /**
-     * Reads configuration from Map and creates a {@link ColumnFamilyManagerFactory} instance.
-     *
-     * @param configurations the configuration from {@link Map}
-     * @return a {@link ColumnFamilyManagerFactory} instance
-     */
-    ColumnFamilyManagerFactory getManagerFactory(Map<String, String> configurations);
-
-    /**
      * Reads configuration either from default configuration or a file defined by NoSQL
      * provider and then creates a {@link ColumnFamilyManagerFactory} instance.
      *
