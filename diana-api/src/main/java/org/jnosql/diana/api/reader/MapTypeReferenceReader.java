9ae54767b283a7a9444b594bec4c50c3b000db88
Otavio Santana <otaviojava@java.net>
2016-10-14 18:22:08 -0300

    Adds implementation to MapTypeRererence

---
+++ diana-api/src/main/java/org/jnosql/diana/api/reader/MapTypeReferenceReader.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.diana.api.reader;
+
+
+import org.jnosql.diana.api.TypeReference;
+import org.jnosql.diana.api.TypeReferenceReader;
+import org.jnosql.diana.api.ValueReader;
+import org.jnosql.diana.api.ValueReaderDecorator;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.singleton;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.StreamSupport.stream;
+
+/**
+ * The {@link TypeReferenceReader} to {@link Map}
+ */
+public class MapTypeReferenceReader implements TypeReferenceReader {
+
+    private static final transient ValueReader SERVICE_PROVIDER = ValueReaderDecorator.getInstance();
+
+    @Override
+    public <T> boolean isCompatible(TypeReference<T> typeReference) {
+        Type type = typeReference.getType();
+        if (ParameterizedType.class.isInstance(type)) {
+            ParameterizedType parameterizedType = ParameterizedType.class.cast(type);
+
+            return Map.class.equals((Class<?>) parameterizedType.getRawType()) &&
+                    Class.class.isInstance(parameterizedType.getActualTypeArguments()[0])
+                    && Class.class.isInstance(parameterizedType.getActualTypeArguments()[1]);
+        }
+        return false;
+    }
+
+    @Override
+    public <T> T convert(Type type, Object value) {
+        ParameterizedType parameterizedType = ParameterizedType.class.cast(type);
+        Class<?> keyType = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+        Class<?> valueType = (Class<?>) parameterizedType.getActualTypeArguments()[1];
+        return (T) getMap(keyType, valueType, value);
+
+    }
+
+    private <K, V> Map<K, V> getMap(Class<K> keyClass, Class<V> valueClass, Object value) {
+
+        if (Map.class.isInstance(value)) {
+            Map mapValue = Map.class.cast(value);
+            return (Map<K, V>) mapValue.keySet().stream()
+                    .collect(Collectors.toMap(mapKeyElement(keyClass), mapValueElement(valueClass, mapValue)));
+        }
+        throw new UnsupportedOperationException("There is not supported convert" + value + " a not Map type.");
+    }
+
+    private <K> Function mapKeyElement(Class<K> keyClass) {
+        return (keyElement) -> {
+            if (SERVICE_PROVIDER.isCompatible(keyClass)) {
+                return SERVICE_PROVIDER.read(keyClass, keyElement);
+            }
+            return keyElement;
+        };
+    }
+
+    private <V> Function mapValueElement(Class<V> valueClass, Map mapValue) {
+        return (keyElement) -> {
+            Object valueElement = mapValue.get(keyElement);
+            if (SERVICE_PROVIDER.isCompatible(valueClass)) {
+                return SERVICE_PROVIDER.read(valueClass, valueElement);
+            }
+            return valueElement;
+        };
+    }
+
+
+}
---
+++ diana-api/src/test/java/org/jnosql/diana/api/reader/MapTypeReferenceReaderTest.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.diana.api.reader;
+
+import org.jnosql.diana.api.TypeReference;
+import org.jnosql.diana.api.TypeReferenceReader;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class MapTypeReferenceReaderTest {
+
+    private TypeReferenceReader referenceReader = new MapTypeReferenceReader();
+
+
+    @Test
+    public void shouldBeCompatible() {
+
+        assertTrue(referenceReader.isCompatible(new TypeReference<Map<String, String>>(){}));
+        assertTrue(referenceReader.isCompatible(new TypeReference<Map<Long, Integer>>(){}));
+
+    }
+
+
+    @Test
+    public void shouldNotBeCompatible() {
+        assertFalse(referenceReader.isCompatible(new TypeReference<ArrayList<BigDecimal>>(){}));
+        assertFalse(referenceReader.isCompatible(new TypeReference<String>(){}));
+        assertFalse(referenceReader.isCompatible(new TypeReference<Set<String>>(){}));
+        assertFalse(referenceReader.isCompatible(new TypeReference<List<List<String>>>(){}));
+        assertFalse(referenceReader.isCompatible(new TypeReference<Queue<String>>(){}));
+        assertFalse(referenceReader.isCompatible(new TypeReference<Map<Integer, List<String>>>(){}));
+    }
+
+
+    @Test
+    public void shouldConvert() {
+        assertEquals(singletonMap("123", "123"), referenceReader.convert(new TypeReference<Map<String, String>>(){}.getType(), singletonMap(123, 123L)));
+        assertEquals(singletonMap(123L, 123), referenceReader.convert(new TypeReference<Map<Long, Integer>>(){}.getType(), singletonMap("123", "123")));
+    }
+
+}
