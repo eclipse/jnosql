ed433aeb19a950edf19d5479a06608fff5c7de56
Otavio Santana <otaviojava@java.net>
2016-10-17 21:34:07 -0200

    Adds optional resource

---
+++ diana-api/src/main/java/org/jnosql/diana/api/reader/OptionalTypeReferenceReader.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.diana.api.reader;
+
+import org.jnosql.diana.api.TypeReferenceReader;
+import org.jnosql.diana.api.TypeSupplier;
+import org.jnosql.diana.api.ValueReader;
+import org.jnosql.diana.api.ValueReaderDecorator;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Optional;
+
+/**
+ * The {@link TypeReferenceReader} to {@link java.util.Optional}
+ */
+@SuppressWarnings("unchecked")
+public class OptionalTypeReferenceReader implements TypeReferenceReader {
+
+    private static final transient ValueReader SERVICE_PROVIDER = ValueReaderDecorator.getInstance();
+
+    @Override
+    public <T> boolean isCompatible(TypeSupplier<T> typeReference) {
+        Type type = typeReference.get();
+        if (ParameterizedType.class.isInstance(type)) {
+            ParameterizedType parameterizedType = ParameterizedType.class.cast(type);
+
+            return Optional.class.equals(parameterizedType.getRawType()) &&
+                    Class.class.isInstance(parameterizedType.getActualTypeArguments()[0]);
+        }
+        return false;
+    }
+
+    @Override
+    public <T> T convert(TypeSupplier<T> typeReference, Object value) {
+
+        Type type = typeReference.get();
+        ParameterizedType parameterizedType = ParameterizedType.class.cast(type);
+        Class<?> classType = (Class<?>) parameterizedType.getActualTypeArguments()[0];
+        return (T) Optional.ofNullable(SERVICE_PROVIDER.read(classType, value));
+    }
+}
