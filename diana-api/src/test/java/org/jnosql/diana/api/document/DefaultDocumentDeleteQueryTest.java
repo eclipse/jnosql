cc5234476d5a8e86fddbbdc27014ab108c942f87
Otavio Santana <otaviojava@java.net>
2017-02-05 09:32:18 -0200

    Adds tests on documentdelete query

---
+++ diana-api/src/test/java/org/jnosql/diana/api/document/DefaultDocumentDeleteQueryTest.java
@@ -0,0 +1,93 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jnosql.diana.api.document;
+
+import org.junit.Test;
+
+import static java.util.Collections.singletonList;
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+
+public class DefaultDocumentDeleteQueryTest {
+
+    @Test(expected = NullPointerException.class)
+    public void shouldReturnNPEWhenColumnIsNull() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery.of(null, condition);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldReturnNPEWhenConditionIsNull() {
+        DefaultDocumentDeleteQuery.of("collection", null);
+    }
+
+    @Test
+    public void shouldCreateInstance() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        assertEquals("query", query.getCollection());
+        assertEquals(condition, query.getCondition());
+        assertTrue(query.getColumns().isEmpty());
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldGetErrorWhenAddColumnIsNull() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        query.add(null);
+    }
+
+    @Test
+    public void shouldAddColumn() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        query.add("column");
+        assertThat(query.getColumns(), contains("column"));
+    }
+
+    @Test
+    public void shouldRemoveColumn() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        query.add("column");
+        query.remove("column");
+        assertTrue(query.getColumns().isEmpty());
+    }
+
+
+    @Test
+    public void shouldAddAllColumns() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        query.addAll(singletonList("column"));
+        assertThat(query.getColumns(), contains("column"));
+    }
+
+    @Test
+    public void shouldRemoveAllColumns() {
+        DocumentCondition condition = DocumentCondition.eq(Document.of("name", "Ada"));
+        DefaultDocumentDeleteQuery query = DefaultDocumentDeleteQuery.of("query", condition);
+        query.add("column");
+        query.removeAll(singletonList("column"));
+        assertTrue(query.getColumns().isEmpty());
+    }
+}
