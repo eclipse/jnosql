2622f5a24bec4dca1d9d8eaa14eed384d0a55427
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-16 07:15:27 -0300

    Adds delete query builder

---
+++ src/main/java/org/jnosql/diana/api/document/query/DefaultDeleteQueryBuilder.java
@@ -0,0 +1,74 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api.document.query;
+
+import org.jnosql.diana.api.document.DocumentCondition;
+import org.jnosql.diana.api.document.DocumentDeleteQuery;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * The default implementation to Delete query
+ */
+class DefaultDeleteQueryBuilder implements DocumentDelete, DocumentDeleteFrom, DocumentDeleteWhere {
+
+    private String documentCollection;
+
+    private DocumentCondition condition;
+
+    private final List<String> documents;
+
+    DefaultDeleteQueryBuilder(List<String> documents) {
+        this.documents = documents;
+    }
+
+    @Override
+    public DocumentDeleteFrom from(String documentCollection) throws NullPointerException {
+        requireNonNull(documentCollection, "documentCollection is required");
+        this.documentCollection = documentCollection;
+        return this;
+    }
+
+    @Override
+    public DocumentDeleteWhere where(DocumentCondition condition) throws NullPointerException {
+        requireNonNull(condition, "condition is required");
+        this.condition = condition;
+        return this;
+    }
+
+    @Override
+    public DocumentDeleteWhere and(DocumentCondition condition) throws NullPointerException {
+        requireNonNull(condition, "condition is required");
+        this.condition = this.condition.and(condition);
+        return this;
+    }
+
+    @Override
+    public DocumentDeleteWhere or(DocumentCondition condition) throws NullPointerException {
+        requireNonNull(condition, "condition is required");
+        this.condition = this.condition.or(condition);
+        return this;
+    }
+
+
+    @Override
+    public DocumentDeleteQuery build() {
+        return null;
+    }
+}
