0c74e7f18a92047e3bfc4d746203863eaa3429b8
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-15 23:22:30 -0300

    Adds column where

--- src/main/java/org/jnosql/diana/api/column/query/ColumnWhere.java
+++ src/main/java/org/jnosql/diana/api/column/query/ColumnWhere.java
@@ -1,7 +1,81 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
 package org.jnosql.diana.api.column.query;
 
+
+import org.jnosql.diana.api.Sort;
+import org.jnosql.diana.api.column.ColumnCondition;
+import org.jnosql.diana.api.column.ColumnQuery;
+
 /**
- * Created by otaviojava on 7/15/17.
+ * The Column Where whose define the condition in the query.
  */
 public interface ColumnWhere {
+
+
+    /**
+     * Appends a new condition in the select using {@link ColumnCondition#and(ColumnCondition)}
+     *
+     * @param condition a condition to be added
+     * @return the same {@link ColumnWhere} with the condition appended
+     * @throws NullPointerException when condition is null
+     */
+    ColumnWhere and(ColumnCondition condition) throws NullPointerException;
+
+    /**
+     * Appends a new condition in the select using {@link ColumnCondition#or(ColumnCondition)}
+     *
+     * @param condition a condition to be added
+     * @return the same {@link ColumnWhere} with the condition appended
+     * @throws NullPointerException when condition is null
+     */
+    ColumnWhere or(ColumnCondition condition) throws NullPointerException;
+
+    /**
+     * Defines the position of the first result to retrieve.
+     *
+     * @param start the first result to retrive
+     * @return a query with first result defined
+     */
+    ColumnStart start(long start);
+
+
+    /**
+     * Defines the maximum number of results to retrieve.
+     *
+     * @param limit the limit
+     * @return a query with the limit defined
+     */
+    ColumnLimit limit(long limit);
+
+    /**
+     * Add the order how the result will returned
+     *
+     * @param sort the order
+     * @return a query with the sort defined
+     * @throws NullPointerException when sort is null
+     */
+    ColumnOrder orderBy(Sort sort) throws NullPointerException;
+
+    /**
+     * Creates a new instance of {@link ColumnQuery}
+     *
+     * @return a new {@link ColumnQuery} instance
+     */
+    ColumnQuery build();
+
 }
