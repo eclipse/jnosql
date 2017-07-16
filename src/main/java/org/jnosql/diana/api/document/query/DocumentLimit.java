c4939212a177edd0b44cb981a24722014381c85d
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-16 02:16:58 -0300

    Adds document limit

--- src/main/java/org/jnosql/diana/api/document/query/DocumentLimit.java
+++ src/main/java/org/jnosql/diana/api/document/query/DocumentLimit.java
@@ -1,7 +1,41 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
 package org.jnosql.diana.api.document.query;
 
+
+import org.jnosql.diana.api.document.DocumentQuery;
+
 /**
- * Created by otaviojava on 7/16/17.
+ * The Document Order whose define the the maximum number of results to retrieve.
  */
 public interface DocumentLimit {
+
+    /**
+     * Defines the position of the first result to retrieve.
+     *
+     * @param start the first result to retrive
+     * @return a query with first result defined
+     */
+    DocumentStart start(long start);
+
+    /**
+     * Creates a new instance of {@link DocumentQuery}
+     *
+     * @return a new {@link DocumentQuery} instance
+     */
+    DocumentQuery build();
 }
