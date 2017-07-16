09f2ad3258fc4b3e5507821ec10ab54b16344d4b
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-16 01:24:46 -0300

    Adds delete from column

--- src/main/java/org/jnosql/diana/api/column/query/ColumnDeleteFrom.java
+++ src/main/java/org/jnosql/diana/api/column/query/ColumnDeleteFrom.java
@@ -1,7 +1,45 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
 package org.jnosql.diana.api.column.query;
 
+
+import org.jnosql.diana.api.column.ColumnCondition;
+import org.jnosql.diana.api.column.ColumnDeleteQuery;
+
+
 /**
- * Created by otaviojava on 7/16/17.
+ * The Column Delete Query
  */
 public interface ColumnDeleteFrom {
+
+
+    /**
+     * Defines a new condition in the query
+     *
+     * @param condition the condition in the where
+     * @return a new {@link ColumnWhere}
+     * @throws NullPointerException when condition is null
+     */
+    ColumnDeleteWhere where(ColumnCondition condition) throws NullPointerException;
+
+    /**
+     * Creates a new instance of {@link ColumnDeleteQuery}
+     *
+     * @return a new {@link ColumnDeleteQuery} instance
+     */
+    ColumnDeleteQuery build();
 }
