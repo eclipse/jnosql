00c2fd90bdbd79078ea71e3494f55da246bc7c38
Otavio Santana <otaviopolianasantana@gmail.com>
2017-07-16 01:15:52 -0300

    Adds tests at columnQuery

---
+++ src/test/java/org/jnosql/diana/api/column/query/DefaultColumnQueryTest.java
@@ -0,0 +1,54 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api.column.query;
+
+import org.jnosql.diana.api.column.ColumnQuery;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.jnosql.diana.api.column.query.ColumnQueryBuilder.select;
+import static org.junit.Assert.assertTrue;
+
+
+public class DefaultColumnQueryTest {
+
+    private ColumnQuery query;
+
+
+    @Before
+    public void setUp() {
+        query = select().from("columnFamily").build();
+    }
+
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldNotRemoveColumns() {
+        List<String> columns = query.getColumns();
+        assertTrue(columns.isEmpty());
+        columns.clear();
+    }
+
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldNotRemoveSort() {
+        List<String> columns = query.getColumns();
+        assertTrue(columns.isEmpty());
+        columns.clear();
+    }
+}
