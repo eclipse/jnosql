773ba4c9220136caf5aea7021e1907fa60468a42
Otavio Santana <otaviopolianasantana@gmail.com>
2017-11-03 07:58:07 -0200

    Implementes delete query

--- diana-column/src/main/java/org/jnosql/diana/api/column/query/DefaultDeleteQueryBuilder.java
+++ diana-column/src/main/java/org/jnosql/diana/api/column/query/DefaultDeleteQueryBuilder.java
@@ -17,23 +17,34 @@
 package org.jnosql.diana.api.column.query;
 
 
+import org.jnosql.diana.api.column.Column;
 import org.jnosql.diana.api.column.ColumnCondition;
 import org.jnosql.diana.api.column.ColumnDeleteQuery;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static java.util.Objects.nonNull;
 import static java.util.Objects.requireNonNull;
 
 /**
  * The default implementation to Delete query
  */
-class DefaultDeleteQueryBuilder implements ColumnDelete, ColumnDeleteFrom, ColumnDeleteWhere {
+class DefaultDeleteQueryBuilder implements ColumnDelete, ColumnDeleteFrom,
+        ColumnDeleteWhere, ColumnDeleteWhereName, ColumnDeleteNotCondition {
 
     private String columnFamily;
 
     private ColumnCondition condition;
 
     private final List<String> columns;
+
+    private String name;
+
+    private boolean negate;
+
+    private boolean and;
+
 
     DefaultDeleteQueryBuilder(List<String> columns) {
         this.columns = columns;
@@ -54,6 +65,13 @@ class DefaultDeleteQueryBuilder implements ColumnDelete, ColumnDeleteFrom, Colum
     }
 
     @Override
+    public ColumnDeleteWhereName where(String name) throws NullPointerException {
+        requireNonNull(name, "name is required");
+        this.name = name;
+        return this;
+    }
+
+    @Override
     public ColumnDeleteWhere and(ColumnCondition condition) throws NullPointerException {
         requireNonNull(condition, "condition is required");
         this.condition = this.condition.and(condition);
@@ -67,6 +85,87 @@ class DefaultDeleteQueryBuilder implements ColumnDelete, ColumnDeleteFrom, Colum
         return this;
     }
 
+
+    @Override
+    public ColumnDeleteNotCondition not() {
+        this.negate = true;
+        return this;
+    }
+
+    @Override
+    public <T> ColumnDeleteWhere eq(T value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.eq(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere like(String value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.like(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere gt(Number value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.gt(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere gte(Number value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.gte(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere lt(Number value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.lt(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere lte(Number value) throws NullPointerException {
+        requireNonNull(value, "value is required");
+        ColumnCondition newCondition = ColumnCondition.lte(Column.of(name, value));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public ColumnDeleteWhere between(Number valueA, Number valueB) throws NullPointerException {
+        requireNonNull(valueA, "valueA is required");
+        requireNonNull(valueB, "valueB is required");
+        ColumnCondition newCondition = ColumnCondition.between(Column.of(name, asList(valueA, valueB)));
+        return appendCondition(newCondition);
+    }
+
+    @Override
+    public <T> ColumnDeleteWhere in(Iterable<T> values) throws NullPointerException {
+        requireNonNull(values, "values is required");
+        ColumnCondition newCondition = ColumnCondition.in(Column.of(name, values));
+        return this;
+    }
+
+    private DefaultDeleteQueryBuilder appendCondition(ColumnCondition newCondition) {
+        if (negate) {
+            newCondition = newCondition.negate();
+        }
+        if (nonNull(condition)) {
+            if (and) {
+                this.condition = condition.and(newCondition);
+            } else {
+                this.condition = condition.or(newCondition);
+            }
+        } else {
+            this.condition = newCondition;
+        }
+        this.negate = false;
+        this.name = null;
+        return this;
+    }
 
     @Override
     public ColumnDeleteQuery build() {
