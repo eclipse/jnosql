d79b300bf6223684bb24ab2120d0934069eec31d
Otavio Santana <otaviopolianasantana@gmail.com>
2017-11-03 06:45:45 -0200

    Adds tests scenarios

--- diana-column/src/test/java/org/jnosql/diana/api/column/query/DefaultSelectQueryBuilderTest.java
+++ diana-column/src/test/java/org/jnosql/diana/api/column/query/DefaultSelectQueryBuilderTest.java
@@ -16,11 +16,17 @@
  */
 package org.jnosql.diana.api.column.query;
 
+import org.hamcrest.Matchers;
+import org.jnosql.diana.api.Condition;
 import org.jnosql.diana.api.Sort;
+import org.jnosql.diana.api.TypeReference;
 import org.jnosql.diana.api.column.Column;
 import org.jnosql.diana.api.column.ColumnCondition;
 import org.jnosql.diana.api.column.ColumnQuery;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -151,4 +157,117 @@ public class DefaultSelectQueryBuilderTest {
         assertEquals(10L, query.getFirstResult());
     }
 
+    @Test
+    public void shouldSelectWhereNameEq() {
+        String columnFamily = "columnFamily";
+        String name = "Ada Lovelace";
+        ColumnQuery query = select().from(columnFamily).where("name").eq(name).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.EQUALS, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(name, column.get());
+
+    }
+
+    @Test
+    public void shouldSelectWhereNameLike() {
+        String columnFamily = "columnFamily";
+        String name = "Ada Lovelace";
+        ColumnQuery query = select().from(columnFamily).where("name").like(name).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.LIKE, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(name, column.get());
+    }
+
+    @Test
+    public void shouldSelectWhereNameGt() {
+        String columnFamily = "columnFamily";
+        Number value = 10;
+        ColumnQuery query = select().from(columnFamily).where("name").gt(value).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.GREATER_THAN, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(value, column.get());
+    }
+
+    @Test
+    public void shouldSelectWhereNameGte() {
+        String columnFamily = "columnFamily";
+        Number value = 10;
+        ColumnQuery query = select().from(columnFamily).where("name").gte(value).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.GREATER_EQUALS_THAN, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(value, column.get());
+    }
+
+    @Test
+    public void shouldSelectWhereNameLt() {
+        String columnFamily = "columnFamily";
+        Number value = 10;
+        ColumnQuery query = select().from(columnFamily).where("name").lt(value).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.LESSER_THAN, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(value, column.get());
+    }
+
+    @Test
+    public void shouldSelectWhereNameLte() {
+        String columnFamily = "columnFamily";
+        Number value = 10;
+        ColumnQuery query = select().from(columnFamily).where("name").lte(value).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.LESSER_EQUALS_THAN, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        assertEquals(value, column.get());
+    }
+
+    @Test
+    public void shouldSelectWhereNameBetween() {
+        String columnFamily = "columnFamily";
+        Number valueA = 10;
+        Number valueB = 20;
+        ColumnQuery query = select().from(columnFamily).where("name").between(valueA, valueB).build();
+        ColumnCondition columnCondition = query.getCondition().get();
+
+        Column column = columnCondition.getColumn();
+
+        assertTrue(query.getColumns().isEmpty());
+        assertEquals(columnFamily, query.getColumnFamily());
+        assertEquals(Condition.BETWEEN, columnCondition.getCondition());
+        assertEquals("name", column.getName());
+        Assert.assertThat(column.get(new TypeReference<List<Number>>() {}), Matchers.contains(10, 20));
+    }
 }
