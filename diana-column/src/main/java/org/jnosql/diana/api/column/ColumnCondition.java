c1bd09d05d3db357245b15695f826b6e6015f057
Otavio Santana <otaviopolianasantana@gmail.com>
2018-02-09 20:20:02 -0200

    fixes import

--- diana-column/src/main/java/org/jnosql/diana/api/column/ColumnCondition.java
+++ diana-column/src/main/java/org/jnosql/diana/api/column/ColumnCondition.java
@@ -23,7 +23,6 @@ import org.jnosql.diana.api.Condition;
 import static org.jnosql.diana.api.Condition.EQUALS;
 import static org.jnosql.diana.api.Condition.GREATER_EQUALS_THAN;
 import static org.jnosql.diana.api.Condition.GREATER_THAN;
-import static org.jnosql.diana.api.Condition.IN;
 import static org.jnosql.diana.api.Condition.LESSER_EQUALS_THAN;
 import static org.jnosql.diana.api.Condition.LESSER_THAN;
 import static org.jnosql.diana.api.Condition.LIKE;
