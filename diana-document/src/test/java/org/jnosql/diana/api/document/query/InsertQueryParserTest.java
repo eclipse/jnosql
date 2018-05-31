1546112e3d50a40e2ddb04659da329b9748d0085
Otavio Santana <otaviopolianasantana@gmail.com>
2018-05-31 14:10:06 -0300

    fixes parser

---
+++ diana-document/src/test/java/org/jnosql/diana/api/document/query/InsertQueryParserTest.java
@@ -0,0 +1,177 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api.document.query;
+
+import org.jnosql.diana.api.document.Document;
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentEntity;
+import org.jnosql.query.QueryException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class InsertQueryParserTest {
+
+    private InsertQueryParser parser = new InsertQueryParser();
+
+    private DocumentCollectionManager documentCollection = Mockito.mock(DocumentCollectionManager.class);
+
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\")"})
+    public void shouldReturnParserQuery(String query) {
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture());
+        DocumentEntity entity = captor.getValue();
+
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (age = 30, name = \"Artemis\")"})
+    public void shouldReturnParserQuery1(String query) {
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture());
+        DocumentEntity entity = captor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Artemis"), entity.find("name").get());
+        assertEquals(Document.of("age", 30L), entity.find("age").get());
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 day"})
+    public void shouldReturnParserQuery2(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofDays(10L), duration);
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 hour"})
+    public void shouldReturnParserQuery3(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofHours(10L), duration);
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 minute"})
+    public void shouldReturnParserQuery4(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofMinutes(10L), duration);
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 second"})
+    public void shouldReturnParserQuery5(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofSeconds(10L), duration);
+    }
+
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 millisecond"})
+    public void shouldReturnParserQuery6(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofMillis(10L), duration);
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\") 10 nanosecond"})
+    public void shouldReturnParserQuery7(String query) {
+
+        ArgumentCaptor<DocumentEntity> captor = ArgumentCaptor.forClass(DocumentEntity.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        parser.query(query, documentCollection);
+        Mockito.verify(documentCollection).insert(captor.capture(), durationCaptor.capture());
+        DocumentEntity entity = captor.getValue();
+        Duration duration = durationCaptor.getValue();
+
+        assertEquals("God", entity.getName());
+        assertEquals(Document.of("name", "Diana"), entity.find("name").get());
+        assertEquals(Duration.ofNanos(10L), duration);
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = @name)"})
+    public void shouldReturnParserQuery8(String query) {
+
+        assertThrows(QueryException.class, () -> {
+            parser.query(query, documentCollection);
+        });
+    }
+}
