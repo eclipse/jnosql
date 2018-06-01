0faa89d2864f4c62436140e813353dacf3781977
Otavio Santana <otaviopolianasantana@gmail.com>
2018-06-01 08:00:14 -0300

    adds tests

---
+++ diana-column/src/test/java/org/jnosql/diana/api/column/query/DefaultColumnQueryParserTest.java
@@ -0,0 +1,193 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api.column.query;
+
+import org.jnosql.diana.api.column.Column;
+import org.jnosql.diana.api.column.ColumnCondition;
+import org.jnosql.diana.api.column.ColumnDeleteQuery;
+import org.jnosql.diana.api.column.ColumnEntity;
+import org.jnosql.diana.api.column.ColumnFamilyManager;
+import org.jnosql.diana.api.column.ColumnPreparedStatement;
+import org.jnosql.diana.api.column.ColumnQuery;
+import org.jnosql.diana.api.column.ColumnQueryParser;
+import org.jnosql.query.QueryException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import static org.jnosql.diana.api.Condition.EQUALS;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DefaultColumnQueryParserTest {
+    
+    private ColumnQueryParser parser = new DefaultColumnQueryParser();
+
+
+    private ColumnFamilyManager manager = Mockito.mock(ColumnFamilyManager.class);
+
+    @Test
+    public void shouldReturnNPEWhenThereIsNullParameter() {
+
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            parser.query(null, manager);
+        });
+
+        Assertions.assertThrows(NullPointerException.class, () -> {
+            parser.query("select * from God", null);
+        });
+    }
+
+    @Test
+    public void shouldReturnErrorWhenHasInvalidQuery() {
+
+        Assertions.assertThrows(QueryException.class, () -> {
+            parser.query("inva", manager);
+        });
+
+        Assertions.assertThrows(QueryException.class, () -> {
+            parser.query("invalid", manager);
+        });
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"select * from God"})
+    public void shouldReturnParserQuery(String query) {
+        ArgumentCaptor<ColumnQuery> captor = ArgumentCaptor.forClass(ColumnQuery.class);
+        parser.query(query, manager);
+        Mockito.verify(manager).select(captor.capture());
+        ColumnQuery columnQuery = captor.getValue();
+
+        assertTrue(columnQuery.getColumns().isEmpty());
+        assertTrue(columnQuery.getSorts().isEmpty());
+        assertEquals(0L, columnQuery.getLimit());
+        assertEquals(0L, columnQuery.getSkip());
+        assertEquals("God", columnQuery.getColumnFamily());
+        assertFalse(columnQuery.getCondition().isPresent());
+
+    }
+
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"delete from God"})
+    public void shouldReturnParserQuery1(String query) {
+        ArgumentCaptor<ColumnDeleteQuery> captor = ArgumentCaptor.forClass(ColumnDeleteQuery.class);
+        parser.query(query, manager);
+        Mockito.verify(manager).delete(captor.capture());
+        ColumnDeleteQuery columnDeleteQuery = captor.getValue();
+
+        assertTrue(columnDeleteQuery.getColumns().isEmpty());
+        assertEquals("God", columnDeleteQuery.getColumnFamily());
+        assertFalse(columnDeleteQuery.getCondition().isPresent());
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = \"Diana\")"})
+    public void shouldReturnParserQuery2(String query) {
+        ArgumentCaptor<ColumnEntity> captor = ArgumentCaptor.forClass(ColumnEntity.class);
+        parser.query(query, manager);
+        Mockito.verify(manager).insert(captor.capture());
+        ColumnEntity entity = captor.getValue();
+
+
+        assertEquals("God", entity.getName());
+        assertEquals(Column.of("name", "Diana"), entity.find("name").get());
+    }
+
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"update God (name = \"Diana\")"})
+    public void shouldReturnParserQuery3(String query) {
+        ArgumentCaptor<ColumnEntity> captor = ArgumentCaptor.forClass(ColumnEntity.class);
+        parser.query(query, manager);
+        Mockito.verify(manager).update(captor.capture());
+        ColumnEntity entity = captor.getValue();
+
+
+        assertEquals("God", entity.getName());
+        assertEquals(Column.of("name", "Diana"), entity.find("name").get());
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"delete from God where age = @age"})
+    public void shouldExecutePrepareStatment(String query) {
+        ArgumentCaptor<ColumnDeleteQuery> captor = ArgumentCaptor.forClass(ColumnDeleteQuery.class);
+
+        ColumnPreparedStatement prepare = parser.prepare(query, manager);
+        prepare.bind("age", 12);
+        prepare.getResultList();
+        Mockito.verify(manager).delete(captor.capture());
+        ColumnDeleteQuery columnDeleteQuery = captor.getValue();
+        ColumnCondition columnCondition = columnDeleteQuery.getCondition().get();
+        Column column = columnCondition.getColumn();
+        assertEquals(EQUALS, columnCondition.getCondition());
+        assertEquals("age", column.getName());
+        assertEquals(12, column.get());
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"insert God (name = @name)"})
+    public void shouldExecutePrepareStatment1(String query) {
+        ArgumentCaptor<ColumnEntity> captor = ArgumentCaptor.forClass(ColumnEntity.class);
+        ColumnPreparedStatement prepare = parser.prepare(query, manager);
+        prepare.bind("name", "Diana");
+        prepare.getResultList();
+        Mockito.verify(manager).insert(captor.capture());
+        ColumnEntity entity = captor.getValue();
+        assertEquals("God", entity.getName());
+        assertEquals(Column.of("name", "Diana"), entity.find("name").get());
+
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"select  * from God where age = @age"})
+    public void shouldExecutePrepareStatment2(String query) {
+        ArgumentCaptor<ColumnQuery> captor = ArgumentCaptor.forClass(ColumnQuery.class);
+
+        ColumnPreparedStatement prepare = parser.prepare(query, manager);
+        prepare.bind("age", 12);
+        prepare.getResultList();
+        Mockito.verify(manager).select(captor.capture());
+        ColumnQuery columnQuery = captor.getValue();
+        ColumnCondition columnCondition = columnQuery.getCondition().get();
+        Column column = columnCondition.getColumn();
+        assertEquals(EQUALS, columnCondition.getCondition());
+        assertEquals("age", column.getName());
+        assertEquals(12, column.get());
+    }
+
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = {"update God (name = @name)"})
+    public void shouldExecutePrepareStatment3(String query) {
+        ArgumentCaptor<ColumnEntity> captor = ArgumentCaptor.forClass(ColumnEntity.class);
+        ColumnPreparedStatement prepare = parser.prepare(query, manager);
+        prepare.bind("name", "Diana");
+        prepare.getResultList();
+        Mockito.verify(manager).update(captor.capture());
+        ColumnEntity entity = captor.getValue();
+        assertEquals("God", entity.getName());
+        assertEquals(Column.of("name", "Diana"), entity.find("name").get());
+
+    }
+
+}
