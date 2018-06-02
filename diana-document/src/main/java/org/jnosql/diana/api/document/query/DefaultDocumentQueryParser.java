634c0c34f876f96b56d752c5d09dd914c5acb74b
Otavio Santana <otaviopolianasantana@gmail.com>
2018-06-02 14:12:37 -0300

    defines observer

--- diana-document/src/main/java/org/jnosql/diana/api/document/query/DefaultDocumentQueryParser.java
+++ diana-document/src/main/java/org/jnosql/diana/api/document/query/DefaultDocumentQueryParser.java
@@ -18,6 +18,7 @@ package org.jnosql.diana.api.document.query;
 
 import org.jnosql.diana.api.document.DocumentCollectionManager;
 import org.jnosql.diana.api.document.DocumentEntity;
+import org.jnosql.diana.api.document.DocumentObserverParser;
 import org.jnosql.diana.api.document.DocumentPreparedStatement;
 import org.jnosql.diana.api.document.DocumentQueryParser;
 import org.jnosql.query.QueryException;
@@ -33,46 +34,50 @@ public final class DefaultDocumentQueryParser implements DocumentQueryParser {
     private final UpdateQueryParser update = new UpdateQueryParser();
 
     @Override
-    public List<DocumentEntity> query(String query, DocumentCollectionManager collectionManager) {
-        validation(query, collectionManager);
+    public List<DocumentEntity> query(String query, DocumentCollectionManager collectionManager,
+                                      DocumentObserverParser observer) {
+        validation(query, collectionManager, observer);
         String command = query.substring(0, 6);
         switch (command) {
             case "select":
-                return select.query(query, collectionManager, this);
+                return select.query(query, collectionManager, observer);
             case "delete":
-                return delete.query(query, collectionManager, this);
+                return delete.query(query, collectionManager, observer);
             case "insert":
-                return insert.query(query, collectionManager, this);
+                return insert.query(query, collectionManager, observer);
             case "update":
-                return update.query(query, collectionManager, this);
+                return update.query(query, collectionManager, observer);
             default:
                 throw new QueryException(String.format("The command was not recognized at the query %s ", query));
         }
     }
 
     @Override
-    public DocumentPreparedStatement prepare(String query, DocumentCollectionManager collectionManager) {
-        validation(query, collectionManager);
+    public DocumentPreparedStatement prepare(String query, DocumentCollectionManager collectionManager,
+                                             DocumentObserverParser observer) {
+
+        validation(query, collectionManager, observer);
         String command = query.substring(0, 6);
 
         switch (command) {
             case "select":
-                return select.prepare(query, collectionManager, this);
+                return select.prepare(query, collectionManager, observer);
             case "delete":
-                return delete.prepare(query, collectionManager, this);
+                return delete.prepare(query, collectionManager, observer);
             case "insert":
-                return insert.prepare(query, collectionManager, this);
+                return insert.prepare(query, collectionManager, observer);
             case "update":
-                return update.prepare(query, collectionManager, this);
+                return update.prepare(query, collectionManager, observer);
             default:
                 throw new QueryException(String.format("The command was not recognized at the query %s ", query));
         }
     }
 
 
-    private void validation(String query, DocumentCollectionManager collectionManager) {
+    private void validation(String query, DocumentCollectionManager collectionManager, DocumentObserverParser observer) {
         Objects.requireNonNull(query, "query is required");
         Objects.requireNonNull(collectionManager, "collectionManager is required");
+        Objects.requireNonNull(observer, "observer is required");
         if (query.length() < 6) {
             throw new QueryException(String.format("The query %s is invalid", query));
         }
