6973821e97fa2d74ed50936c80c0d5e70508141e
Otavio Santana <otaviopolianasantana@gmail.com>
2018-06-02 14:32:29 -0300

    fixes parsr

--- diana-column/src/main/java/org/jnosql/diana/api/column/query/DefaultColumnQueryParser.java
+++ diana-column/src/main/java/org/jnosql/diana/api/column/query/DefaultColumnQueryParser.java
@@ -18,6 +18,7 @@ package org.jnosql.diana.api.column.query;
 
 import org.jnosql.diana.api.column.ColumnEntity;
 import org.jnosql.diana.api.column.ColumnFamilyManager;
+import org.jnosql.diana.api.column.ColumnObserverParser;
 import org.jnosql.diana.api.column.ColumnPreparedStatement;
 import org.jnosql.diana.api.column.ColumnQueryParser;
 import org.jnosql.query.QueryException;
@@ -33,46 +34,47 @@ public class DefaultColumnQueryParser implements ColumnQueryParser {
     private final UpdateQueryParser update = new UpdateQueryParser();
 
     @Override
-    public List<ColumnEntity> query(String query, ColumnFamilyManager manager) {
-        validation(query, manager);
+    public List<ColumnEntity> query(String query, ColumnFamilyManager manager, ColumnObserverParser observer) {
+        validation(query, manager, observer);
         String command = query.substring(0, 6);
         switch (command) {
             case "select":
-                return select.query(query, manager, this);
+                return select.query(query, manager, observer);
             case "delete":
-                return delete.query(query, manager, this);
+                return delete.query(query, manager, observer);
             case "insert":
-                return insert.query(query, manager, this);
+                return insert.query(query, manager, observer);
             case "update":
-                return update.query(query, manager, this);
+                return update.query(query, manager, observer);
             default:
                 throw new QueryException(String.format("The command was not recognized at the query %s ", query));
         }
     }
 
     @Override
-    public ColumnPreparedStatement prepare(String query, ColumnFamilyManager manager) {
-        validation(query, manager);
+    public ColumnPreparedStatement prepare(String query, ColumnFamilyManager manager, ColumnObserverParser observer) {
+        validation(query, manager, observer);
         String command = query.substring(0, 6);
 
         switch (command) {
             case "select":
-                return select.prepare(query, manager, this);
+                return select.prepare(query, manager, observer);
             case "delete":
-                return delete.prepare(query, manager, this);
+                return delete.prepare(query, manager, observer);
             case "insert":
-                return insert.prepare(query, manager, this);
+                return insert.prepare(query, manager, observer);
             case "update":
-                return update.prepare(query, manager, this);
+                return update.prepare(query, manager, observer);
             default:
                 throw new QueryException(String.format("The command was not recognized at the query %s ", query));
         }
     }
 
 
-    private void validation(String query, ColumnFamilyManager manager) {
+    private void validation(String query, ColumnFamilyManager manager, ColumnObserverParser observer) {
         Objects.requireNonNull(query, "query is required");
         Objects.requireNonNull(manager, "manager is required");
+        Objects.requireNonNull(observer, "manager is observer");
         if (query.length() < 6) {
             throw new QueryException(String.format("The query %s is invalid", query));
         }
