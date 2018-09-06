1c6dae3feb25bd9c6ab0dd31afea6fa11c72e030
Otavio Santana <otaviopolianasantana@gmail.com>
2018-09-06 15:26:19 -0300

    defines column delete query

---
+++ diana-column/src/main/java/org/jnosql/diana/api/column/query/DeleteQueryConverter.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api.column.query;
+
+import org.jnosql.diana.api.column.ColumnObserverParser;
+import org.jnosql.query.DeleteQuery;
+import org.jnosql.query.SelectQuery;
+
+import java.util.function.BiFunction;
+
+/**
+ * A converter that converts {@link SelectQuery} to {@link ColumnSelectQuery}
+ */
+public interface DeleteQueryConverter extends BiFunction<DeleteQuery, ColumnObserverParser, ColumnSelectQuery> {
+
+    /**
+     * A {@link DeleteQueryConverter} instance
+     *
+     * @return A {@link DeleteQueryConverter} instance
+     */
+    static DeleteQueryConverter get() {
+        return SelectQueryConverterFactory.INSTANCE;
+    }
+}
