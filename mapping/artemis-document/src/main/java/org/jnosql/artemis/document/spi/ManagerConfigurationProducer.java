a982a265088b7ad12819916d393f349b73072899
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-01 14:31:19 -0200

    implements solution to document

--- mapping/artemis-document/src/main/java/org/jnosql/artemis/document/spi/ManagerConfigurationProducer.java
+++ mapping/artemis-document/src/main/java/org/jnosql/artemis/document/spi/ManagerConfigurationProducer.java
@@ -14,5 +14,63 @@
  */
 package org.jnosql.artemis.document.spi;
 
+import org.jnosql.artemis.ConfigurationException;
+import org.jnosql.artemis.ConfigurationUnit;
+import org.jnosql.artemis.document.DocumentTemplate;
+import org.jnosql.artemis.document.DocumentTemplateAsync;
+import org.jnosql.artemis.document.DocumentTemplateAsyncProducer;
+import org.jnosql.artemis.document.DocumentTemplateProducer;
+import org.jnosql.artemis.util.StringUtils;
+import org.jnosql.diana.api.document.DocumentCollectionManager;
+import org.jnosql.diana.api.document.DocumentCollectionManagerAsync;
+import org.jnosql.diana.api.document.DocumentCollectionManagerAsyncFactory;
+import org.jnosql.diana.api.document.DocumentCollectionManagerFactory;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+import static org.jnosql.artemis.util.ConfigurationUnitUtils.getConfigurationUnit;
+
+/**
+ * It creates both a {@link DocumentCollectionManager} and a {@link DocumentCollectionManagerAsync} from a ConfigurationUnit annotation.
+ */
+@ApplicationScoped
 class ManagerConfigurationProducer {
+
+    @Inject
+    private DocumentCollectionConfigurationProducer configurationProducer;
+
+    @Inject
+    private DocumentTemplateProducer producer;
+
+    @Inject
+    private DocumentTemplateAsyncProducer asyncProducer;
+
+
+    @ConfigurationUnit
+    @Produces
+    public DocumentCollectionManager get(InjectionPoint injectionPoint) {
+        DocumentCollectionManagerFactory<?> managerFactory = configurationProducer.getDocumentConfiguration(injectionPoint);
+        ConfigurationUnit annotation = getConfigurationUnit(injectionPoint, injectionPoint.getAnnotated());
+        String database = annotation.database();
+        if(StringUtils.isBlank(database)){
+            throw new ConfigurationException("To create a DocumentTemplate from a ConfigurationUnit the database field is required");
+        }
+        return managerFactory.get(database);
+    }
+
+    @ConfigurationUnit
+    @Produces
+    public DocumentCollectionManagerAsync getAsync(InjectionPoint injectionPoint) {
+        DocumentCollectionManagerAsyncFactory<?> managerFactory = configurationProducer.getDocumentManagerAsync(injectionPoint);
+        ConfigurationUnit annotation = getConfigurationUnit(injectionPoint, injectionPoint.getAnnotated());
+        String database = annotation.database();
+        if(StringUtils.isBlank(database)){
+            throw new ConfigurationException("To create a DocumentTemplateAsync from a ConfigurationUnit the database field is required");
+        }
+        return managerFactory.getAsync(database);
+    }
+
 }
