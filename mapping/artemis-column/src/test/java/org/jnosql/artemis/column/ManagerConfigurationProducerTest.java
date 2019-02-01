92da35ad039cc3def63ce048d5713c7aee9b681d
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-01 15:00:01 -0200

    creates test to manager configuration producer

---
+++ mapping/artemis-column/src/test/java/org/jnosql/artemis/column/ManagerConfigurationProducerTest.java
@@ -0,0 +1,47 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.column;
+
+import org.jnosql.artemis.ConfigurationUnit;
+import org.jnosql.diana.api.column.ColumnFamilyManager;
+import org.jnosql.diana.api.column.ColumnFamilyManagerAsync;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+class ManagerConfigurationProducerTest {
+
+
+
+    @Inject
+    @ConfigurationUnit(fileName = "document.json", name = "name", database = "database")
+    private ColumnFamilyManager manager;
+
+    @Inject
+    @ConfigurationUnit(fileName = "document.json", name = "name", database = "database")
+    private ColumnFamilyManagerAsync managerAsync;
+
+
+    @Test
+    public void shouldTestManager() {
+        Assertions.assertNotNull(manager);
+    }
+
+    @Test
+    public void shouldTestManagerAsync() {
+        Assertions.assertNotNull(manager);
+    }
+}
