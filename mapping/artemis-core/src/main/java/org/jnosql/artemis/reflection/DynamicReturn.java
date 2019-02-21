2d6757472dc3c7754b4d0868dd631f26914984ec
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-21 19:47:48 -0300

    crates dynamic return

--- mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/DynamicReturn.java
+++ mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/DynamicReturn.java
@@ -1,4 +1,18 @@
 package org.jnosql.artemis.reflection;
 
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Optional;
+
 public interface DynamicReturn {
+
+    <T> Class<T> typeClass();
+
+    Method getMethod();
+
+    <T> Optional<T> singleResult();
+
+    <T> List<T> list();
+
+
 }
