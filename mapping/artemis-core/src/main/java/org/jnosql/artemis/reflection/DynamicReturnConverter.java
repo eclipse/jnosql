27c44a7006dc07bbf96dbfd297a927273e5c1682
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-21 19:45:31 -0300

    updates header

--- mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/DynamicReturnConverter.java
+++ mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/DynamicReturnConverter.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017 Otávio Santana and others
+ *  Copyright (c) 2019 Otávio Santana and others
  *   All rights reserved. This program and the accompanying materials
  *   are made available under the terms of the Eclipse Public License v1.0
  *   and Apache License v2.0 which accompanies this distribution.
