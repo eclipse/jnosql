c24a8c50d2ef16f93b866116ac4c216503f9deb7
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-22 05:46:07 -0300

    creates DynamicQueryMethodReturn

--- mapping/artemis-column/src/main/java/org/jnosql/artemis/column/query/DynamicQueryMethodReturn.java
+++ mapping/artemis-column/src/main/java/org/jnosql/artemis/column/query/DynamicQueryMethodReturn.java
@@ -14,5 +14,101 @@
  */
 package org.jnosql.artemis.column.query;
 
-public class DynamicQueryMethodReturn {
+import org.jnosql.artemis.PreparedStatement;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+
+public final class DynamicQueryMethodReturn {
+
+
+    private final Method method;
+    private final Object[] args;
+    private final Class<?> typeClass;
+    private final java.util.function.Function<String, List<?>> queryConverter;
+    private final Function<String, PreparedStatement> prepareConverter;
+
+    private DynamicQueryMethodReturn(Method method, Object[] args, Class<?> typeClass, Function<String, List<?>> queryConverter,
+                                     Function<String, PreparedStatement> prepareConverter) {
+        this.method = method;
+        this.args = args;
+        this.typeClass = typeClass;
+        this.queryConverter = queryConverter;
+        this.prepareConverter = prepareConverter;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Object[] getArgs() {
+        return args;
+    }
+
+    public Class<?> getTypeClass() {
+        return typeClass;
+    }
+
+    public Function<String, List<?>> getQueryConverter() {
+        return queryConverter;
+    }
+
+    public Function<String, PreparedStatement> getPrepareConverter() {
+        return prepareConverter;
+    }
+
+
+    public static class DynamicQueryMethodReturnBuilder {
+
+        private Method method;
+
+        private Object[] args;
+
+        private Class<?> typeClass;
+
+        private java.util.function.Function<String, List<?>> queryConverter;
+
+        private Function<String, PreparedStatement> prepareConverter;
+
+        private DynamicQueryMethodReturnBuilder() {
+        }
+
+        public DynamicQueryMethodReturnBuilder withMethod(Method method) {
+            this.method = method;
+            return this;
+        }
+
+        public DynamicQueryMethodReturnBuilder withArgs(Object[] args) {
+            this.args = args;
+            return this;
+        }
+
+        public DynamicQueryMethodReturnBuilder withTypeClass(Class<?> typeClass) {
+            this.typeClass = typeClass;
+            return this;
+        }
+
+        public DynamicQueryMethodReturnBuilder withQueryConverter(Function<String, List<?>> queryConverter) {
+            this.queryConverter = queryConverter;
+            return this;
+        }
+
+        public DynamicQueryMethodReturnBuilder withPrepareConverter(Function<String, PreparedStatement> prepareConverter) {
+            this.prepareConverter = prepareConverter;
+            return this;
+        }
+        public DynamicQueryMethodReturn build() {
+            Objects.requireNonNull(method, "method is required");
+            Objects.requireNonNull(args, "args is required");
+            Objects.requireNonNull(typeClass, "typeClass is required");
+            Objects.requireNonNull(queryConverter, "queryConverter is required");
+            Objects.requireNonNull(prepareConverter, "prepareConverter is required");
+
+            return new DynamicQueryMethodReturn(method, args, typeClass, queryConverter, prepareConverter);
+        }
+    }
+
+
 }
