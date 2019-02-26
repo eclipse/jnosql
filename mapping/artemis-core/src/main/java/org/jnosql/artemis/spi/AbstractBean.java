c82c61cf5e3700b7d35e927fca591852522cb8b1
Otavio Santana <otaviopolianasantana@gmail.com>
2019-02-25 21:58:38 -0300

    creates the AbstractBean

---
+++ mapping/artemis-core/src/main/java/org/jnosql/artemis/spi/AbstractBean.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2017 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.spi;
+
+import org.jnosql.artemis.DatabaseQualifier;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.PassivationCapable;
+import java.lang.annotation.Annotation;
+import java.util.Collections;
+import java.util.Set;
+
+
+public abstract class AbstractBean<T> implements Bean<T>, PassivationCapable {
+
+
+    private final BeanManager beanManager;
+
+    protected AbstractBean(BeanManager beanManager) {
+        this.beanManager = beanManager;
+    }
+
+
+    protected  <T> T getInstance(Class<T> clazz) {
+        Bean<T> bean = (Bean<T>) beanManager.getBeans(clazz).iterator().next();
+        CreationalContext<T> ctx = beanManager.createCreationalContext(bean);
+        return (T) beanManager.getReference(bean, clazz, ctx);
+    }
+
+    protected <T> T getInstance(Class<T> clazz, String name) {
+        Bean bean = beanManager.getBeans(clazz, DatabaseQualifier.ofDocument(name)).iterator().next();
+        CreationalContext ctx = beanManager.createCreationalContext(bean);
+        return (T) beanManager.getReference(bean, clazz, ctx);
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isNullable() {
+        return false;
+    }
+
+    @Override
+    public Class<? extends Annotation> getScope() {
+        return ApplicationScoped.class;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Set<Class<? extends Annotation>> getStereotypes() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public boolean isAlternative() {
+        return false;
+    }
+
+}
