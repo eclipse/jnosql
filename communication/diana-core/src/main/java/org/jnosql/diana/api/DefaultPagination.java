fd557be7363364af71a8725e4b2495a23875ec90
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-08 05:28:34 -0300

    creates a default pagination implementation

---
+++ communication/diana-core/src/main/java/org/jnosql/diana/api/DefaultPagination.java
@@ -0,0 +1,82 @@
+/*
+ *
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
+package org.jnosql.diana.api;
+
+import java.util.Objects;
+
+/**
+ * The default implementation of {@link Pagination}
+ */
+final class DefaultPagination implements Pagination {
+
+    private final long page;
+
+    private final long size;
+
+    DefaultPagination(long page, long size) {
+        this.page = page;
+        this.size = size;
+    }
+
+    @Override
+    public long getPageNumber() {
+        return page;
+    }
+
+    @Override
+    public long getPageSize() {
+        return size;
+    }
+
+    @Override
+    public long getLimit() {
+        return 0;
+    }
+
+    @Override
+    public long getSkip() {
+        return 0;
+    }
+
+    @Override
+    public Pagination next() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        DefaultPagination that = (DefaultPagination) o;
+        return page == that.page &&
+                size == that.size;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(page, size);
+    }
+
+    @Override
+    public String toString() {
+        return "page " + page + " of " + size;
+    }
+}
