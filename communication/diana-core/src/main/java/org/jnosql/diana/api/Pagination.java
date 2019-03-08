3344dec9d98371c0feeb0e59625c145033a3c8a7
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-08 04:37:42 -0300

    improves documentaiton

--- communication/diana-core/src/main/java/org/jnosql/diana/api/Pagination.java
+++ communication/diana-core/src/main/java/org/jnosql/diana/api/Pagination.java
@@ -1,6 +1,6 @@
 /*
  *
- *  Copyright (c) 2017 Otávio Santana and others
+ *  Copyright (c) 2019 Otávio Santana and others
  *   All rights reserved. This program and the accompanying materials
  *   are made available under the terms of the Eclipse Public License v1.0
  *   and Apache License v2.0 which accompanies this distribution.
@@ -16,5 +16,38 @@
  */
 package org.jnosql.diana.api;
 
+/**
+ * Pagination is the process of separating print or digital content into discrete pages.
+ * This instance represents this pagination process.
+ */
 public interface Pagination {
+
+    /**
+     * Returns the page to be returned.
+     *
+     * @return the page to be returned.
+     */
+    int getPageNumber();
+
+    /**
+     * Returns the number of items to be returned.
+     *
+     * @return the number of items of that page
+     */
+    int getPageSize();
+
+    /**
+     * Returns the offset to be taken according to the underlying page and page size.
+     *
+     * @return the offset to be taken
+     */
+    long getOffset();
+
+
+    /**
+     * Returns the {@link Pagination} requesting the next {@link Pagination}.
+     *
+     * @return
+     */
+    Pagination next();
 }
