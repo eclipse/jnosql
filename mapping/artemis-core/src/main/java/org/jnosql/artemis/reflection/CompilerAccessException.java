07541e4534c5775ced240558ce92908737185bd4
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-09 06:38:52 -0300

    creates compiler access exception

---
+++ mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/CompilerAccessException.java
@@ -0,0 +1,33 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis.reflection;
+
+import org.jnosql.diana.api.JNoSQLException;
+
+/**
+ * Eclipse JNoSQL tries to optimize access at Getter, Setter,
+ * and Constructors to write/read fields at Class. This Exception launch when there is an issue when trying to compile.
+ */
+class CompilerAccessException extends JNoSQLException {
+
+    /**
+     * Constructs a new runtime exception with the specified detail message.
+     *
+     * @param message the message
+     */
+    CompilerAccessException(String message) {
+        super(message);
+    }
+}
