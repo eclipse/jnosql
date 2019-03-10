8f5b54f4700ad3c4abce810abd87ccaf4921e3cc
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-10 19:22:31 -0300

    creates next scenario

--- communication/diana-core/src/test/java/org/jnosql/diana/api/DefaultPaginationTest.java
+++ communication/diana-core/src/test/java/org/jnosql/diana/api/DefaultPaginationTest.java
@@ -26,22 +26,22 @@ class DefaultPaginationTest {
 
     @Test
     public void shouldReturnErrorWhenPageIsZero() {
-        Assertions.assertThrows(IllegalArgumentException.class,() -> Pagination.page(0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Pagination.page(0));
     }
 
     @Test
     public void shouldReturnErrorWhenPageIsNegative() {
-        Assertions.assertThrows(IllegalArgumentException.class,() -> Pagination.page(-1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Pagination.page(-1));
     }
 
     @Test
     public void shouldReturnErrorWhenSizeIsZero() {
-        Assertions.assertThrows(IllegalArgumentException.class,() -> Pagination.page(2).of(0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Pagination.page(2).of(0));
     }
 
     @Test
     public void shouldReturnErrorWhenSizeIsNegative() {
-        Assertions.assertThrows(IllegalArgumentException.class,() -> Pagination.page(2).of(-1));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Pagination.page(2).of(-1));
     }
 
 
@@ -52,6 +52,34 @@ class DefaultPaginationTest {
         Assertions.assertEquals(2L, pagination.getPageSize());
         Assertions.assertEquals(2L, pagination.getLimit());
         Assertions.assertEquals(0L, pagination.getSkip());
+    }
 
+
+    @Test
+    public void shouldNext() {
+        Pagination pagination = Pagination.page(1).of(2);
+
+        checkPagination(pagination, 1, 0, 2, 2);
+        Pagination next = pagination.next();
+
+        assertNotNull(next);
+        checkPagination(pagination, 2, 2, 2, 2 );
+
+        next = pagination.next();
+        checkPagination(pagination, 3, 4, 2, 2 );
+
+        next = pagination.next();
+        checkPagination(pagination, 4, 6, 2, 2 );
+
+        next = pagination.next();
+        checkPagination(pagination, 5, 8, 2, 2 );
+
+    }
+
+    private void checkPagination(Pagination pagination, long pageNumber, long skip, long limit, long size) {
+        Assertions.assertEquals(pageNumber, pagination.getPageNumber());
+        Assertions.assertEquals(skip, pagination.getSkip());
+        Assertions.assertEquals(limit, pagination.getLimit());
+        Assertions.assertEquals(size, pagination.getPageSize());
     }
 }
