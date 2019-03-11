51181f57c8fb3a103f3ddfe7a3d8789d7ab2d022
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-11 16:51:21 -0300

    fixes column condition

--- communication/diana-column/src/main/java/org/jnosql/diana/api/column/DefaultColumnCondition.java
+++ communication/diana-column/src/main/java/org/jnosql/diana/api/column/DefaultColumnCondition.java
@@ -20,6 +20,7 @@ package org.jnosql.diana.api.column;
 
 import org.jnosql.diana.api.Condition;
 import org.jnosql.diana.api.TypeReference;
+import org.jnosql.diana.api.Value;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -74,12 +75,12 @@ final class DefaultColumnCondition implements ColumnCondition {
 
     static DefaultColumnCondition in(Column column) {
         Objects.requireNonNull(column, "column is required");
-        checkInClause(column.get());
+        checkInClause(column.getValue());
         return new DefaultColumnCondition(column, IN);
     }
 
-    private static void checkInClause(Object value) {
-        if (!Iterable.class.isInstance(value)) {
+    private static void checkInClause(Value value) {
+        if (!value.isInstanceOf(Iterable.class)) {
             throw new IllegalArgumentException("On Columncondition#in you must use an iterable" +
                     " instead of class: " + value.getClass().getName());
         }
@@ -170,7 +171,7 @@ final class DefaultColumnCondition implements ColumnCondition {
 
     @Override
     public String toString() {
-        return  "DefaultColumnCondition{" + "column=" + column +
+        return "DefaultColumnCondition{" + "column=" + column +
                 ", condition=" + condition +
                 '}';
     }
