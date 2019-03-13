17e33de59ae273e772a84831fd740d7447adc806
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-13 06:40:25 -0300

    creates column pagination test

--- mapping/artemis-column/src/test/java/org/jnosql/artemis/column/DefaultColumnQueryPaginationTest.java
+++ mapping/artemis-column/src/test/java/org/jnosql/artemis/column/DefaultColumnQueryPaginationTest.java
@@ -16,8 +16,46 @@
  */
 package org.jnosql.artemis.column;
 
+import org.jnosql.artemis.Pagination;
+import org.jnosql.artemis.column.query.ColumnQueryMapperBuilder;
+import org.jnosql.diana.api.column.ColumnQuery;
+import org.jnosql.diana.api.column.query.ColumnQueryBuilder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultColumnQueryPaginationTest {
+
+
+    @Test
+    public void shouldReturnNPEWhenQueryIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                ColumnQueryPagination.of(null, Pagination.page(1).of(2)));
+    }
+
+    @Test
+    public void shouldReturnNPEWhenPaginationIsNull() {
+        Assertions.assertThrows(NullPointerException.class, () ->
+                ColumnQueryPagination.of(ColumnQueryBuilder.select().from("column").build(), null));
+    }
+
+    @Test
+    public void shouldCreateColumnQueryPagination() {
+        ColumnQuery query = ColumnQueryBuilder.select().from("column").build();
+        Pagination pagination = Pagination.page(1).of(2);
+        ColumnQueryPagination queryPagination = ColumnQueryPagination.of(query, pagination);
+
+        Assertions.assertNotNull(queryPagination);
+
+        Assertions.assertEquals(query.getColumnFamily(), queryPagination.getColumnFamily());
+        Assertions.assertEquals(query.getColumns(), queryPagination.getColumns());
+        Assertions.assertEquals(query.getLimit(), queryPagination.getLimit());
+        Assertions.assertEquals(query.getSkip(), queryPagination.getSkip());
+        Assertions.assertEquals(query.getSorts(), queryPagination.getSorts());
+        Assertions.assertEquals(query.getCondition().orElse(null), queryPagination.getCondition().orElse(null));
+    }
+
+
 
 }
