0f669ff07eb62e9b00458655337eb4d64ccc1e30
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-15 05:10:05 -0300

    creates test to default dynamic test

--- mapping/artemis-core/src/test/java/org/jnosql/artemis/reflection/DefaultDynamicExecutorQueryConverterTest.java
+++ mapping/artemis-core/src/test/java/org/jnosql/artemis/reflection/DefaultDynamicExecutorQueryConverterTest.java
@@ -14,13 +14,19 @@
  */
 package org.jnosql.artemis.reflection;
 
+import org.jnosql.artemis.DynamicQueryException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
@@ -64,8 +70,125 @@ class DefaultDynamicExecutorQueryConverterTest {
         Assertions.assertNull(person);
     }
 
+    @Test
+    public void shouldReturnOptional() {
 
-    private static class Person {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.of(ada))
+                .withClassSource(Person.class)
+                .withList(() -> Collections.emptyList())
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        Optional<Person> person = converter.toOptional(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertTrue(person.isPresent());
+        Assertions.assertEquals(ada, person.get());
+    }
+
+    @Test
+    public void shouldReturnList() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        List<Person> person = converter.toList(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertFalse(person.isEmpty());
+        Assertions.assertEquals(ada, person.get(0));
+    }
+
+    @Test
+    public void shouldReturnSet() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        Set<Person> person = converter.toSet(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertFalse(person.isEmpty());
+        Assertions.assertEquals(ada, person.stream().findFirst().get());
+    }
+
+
+    @Test
+    public void shouldReturnLinkedList() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        LinkedList<Person> person = converter.toLinkedList(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertFalse(person.isEmpty());
+        Assertions.assertEquals(ada, person.stream().findFirst().get());
+    }
+
+    @Test
+    public void shouldReturnTreeSet() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        TreeSet<Person> person = converter.toTreeSet(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertFalse(person.isEmpty());
+        Assertions.assertEquals(ada, person.stream().findFirst().get());
+    }
+
+    @Test
+    public void shouldReturnStream() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        Stream<Person> person = converter.toStream(dynamic);
+        Assertions.assertNotNull(person);
+        Assertions.assertEquals(ada, person.findFirst().get());
+    }
+
+
+    @Test
+    public void shouldReturnDefault() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        Object person = converter.toDefault(dynamic);
+        Assertions.assertNotNull(person);
+    }
+
+    @Test
+    public void shouldReturnErrorWhenTrytoUsePage() {
+        Person ada = new Person("Ada");
+        dynamic = DynamicReturn.builder()
+                .withSingleResult(() -> Optional.empty())
+                .withClassSource(Person.class)
+                .withList(() -> Collections.singletonList(ada))
+                .withMethodSource(Person.class.getDeclaredMethods()[0])
+                .build();
+        Assertions.assertThrows(DynamicQueryException.class, () -> converter.toPage(dynamic));
+    }
+
+
+    private static class Person implements Comparable<Person> {
 
         private String name;
 
@@ -98,6 +221,11 @@ class DefaultDynamicExecutorQueryConverterTest {
             return "Person{" +
                     "name='" + name + '\'' +
                     '}';
+        }
+
+        @Override
+        public int compareTo(Person o) {
+            return name.compareTo(o.name);
         }
     }
 }
