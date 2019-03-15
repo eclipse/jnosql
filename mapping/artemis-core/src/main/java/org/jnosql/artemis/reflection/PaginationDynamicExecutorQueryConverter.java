36e7eebeebad1faf4d7b4b3f41ab97343a440c4d
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-15 20:55:37 -0300

    removes unnecessary imports

--- mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/PaginationDynamicExecutorQueryConverter.java
+++ mapping/artemis-core/src/main/java/org/jnosql/artemis/reflection/PaginationDynamicExecutorQueryConverter.java
@@ -14,9 +14,7 @@
  */
 package org.jnosql.artemis.reflection;
 
-import org.jnosql.artemis.DynamicQueryException;
 import org.jnosql.artemis.Page;
-import org.jnosql.artemis.Pagination;
 
 import java.util.HashSet;
 import java.util.LinkedList;
