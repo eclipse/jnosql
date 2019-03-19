1c5ba019f3409eaff67495b564640f17f6c4260f
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-19 07:13:59 -0300

    creates default sort tests

---
+++ mapping/artemis-core/src/test/java/org/jnosql/artemis/DefaultSortsTest.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.jnosql.artemis;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jnosql.diana.api.Sort;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DefaultSortsTest {
+
+    @Test
+    public void shouldCreateNewSortInstance() {
+        Sorts sorts = Sorts.sorts();
+        assertTrue(sorts.getSorts().isEmpty());
+    }
+
+    @Test
+    public void shouldAsc() {
+        Sorts sorts = Sorts.sorts().asc("name");
+        assertThat(sorts.getSorts(), Matchers.contains(Sort.asc("name")));
+    }
+
+    @Test
+    public void shouldDesc() {
+        Sorts sorts = Sorts.sorts().desc("name");
+        assertThat(sorts.getSorts(), Matchers.contains(Sort.desc("name")));
+    }
+
+    @Test
+    public void shouldAdd() {
+        Sort sort = Sort.desc("name");
+        Sorts sorts = Sorts.sorts().add(sort);
+        assertThat(sorts.getSorts(), Matchers.contains(sort));
+    }
+
+    @Test
+    public void shouldRemove() {
+        Sort sort = Sort.desc("name");
+        Sorts sorts = Sorts.sorts().add(sort).desc("name");
+        sorts.remove(sort);
+        assertTrue(sorts.getSorts().isEmpty());
+    }
+
+    @Test
+    public void shouldReturnErrorWhenUsesNullParameters(){
+        Assertions.assertThrows(NullPointerException.class, () -> Sorts.sorts().asc(null));
+        Assertions.assertThrows(NullPointerException.class, () -> Sorts.sorts().desc(null));
+        Assertions.assertThrows(NullPointerException.class, () -> Sorts.sorts().add(null));
+        Assertions.assertThrows(NullPointerException.class, () -> Sorts.sorts().remove(null));
+    }
+}
