5846c6294d333860c2b546f67da7ce533fd7d8b0
Otavio Santana <otaviopolianasantana@gmail.com>
2019-03-19 08:15:04 -0300

    removes import at SortTest

--- communication/diana-core/src/test/java/org/jnosql/diana/api/SortTest.java
+++ communication/diana-core/src/test/java/org/jnosql/diana/api/SortTest.java
@@ -19,8 +19,6 @@ package org.jnosql.diana.api;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SortTest {
 
 
