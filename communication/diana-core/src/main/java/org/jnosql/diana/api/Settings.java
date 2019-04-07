6105b9def06bd4baaddb84b69e40a8dc833bb925
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-07 08:04:02 -0300

    improves documentation

--- communication/diana-core/src/main/java/org/jnosql/diana/api/Settings.java
+++ communication/diana-core/src/main/java/org/jnosql/diana/api/Settings.java
@@ -29,20 +29,71 @@ import static java.util.Objects.requireNonNull;
  */
 public interface Settings {
 
+    /**
+     * Returns a Set view of the keys contained in this map. The set is backed by the map, so changes to the map are
+     * reflected in the set, and vice-versa. If the map is modified while an iteration over
+     * the set is in progress (except through the iterator's own remove operation), the results of the iteration are undefined.
+     * The set supports element removal, which removes the corresponding mapping from the map, via the Iterator.remove,
+     * Set.remove, removeAll, retainAll, and clear operations. It does not support the add or addAll operations.
+     *
+     * @return a set view of the keys contained in this map
+     */
     Set<String> keySet();
 
+    /**
+     * Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
+     *
+     * @param key the key whose associated value is to be returned
+     * @return the value to which the specified key is mapped, or null if this map contains no mapping for the key
+     * @throws NullPointerException when key is null
+     */
     Object get(String key);
 
+    /**
+     * Returns the value to which the specified key is mapped, or null if this map contains no mapping for the key.
+     *
+     * @param key  the key whose associated value is to be returned
+     * @param type the type be used as {@link Value#get(Class)}
+     * @param <T>  the type value
+     * @return the value to which the specified key is mapped, or null if this map contains no mapping for the key
+     * @throws NullPointerException when there are null parameters
+     */
     <T> T get(String key, Class<T> type);
 
+    /**
+     * Returns the value to which the specified key is mapped, or defaultValue if this map contains no mapping for the key.
+     *
+     * @param key          the key whose associated value is to be returned
+     * @param defaultValue the default mapping of the key
+     * @param <T>          the type value
+     * @return the value to which the specified key is mapped, or defaultValue if this map contains no mapping for the key
+     */
     <T> T getOrDefault(Object key, T defaultValue);
 
+    /**
+     * @return Returns true if this map contains no key-value mappings.
+     */
     boolean isEmpty();
 
+    /**
+     * @return Returns the number of key-value mappings in this map.
+     */
     int size();
 
+    /**
+     * Returns true if this map contains a mapping for the specified key.
+     *
+     * @param key key whose presence in this map is to be tested
+     * @return true if this map contains a mapping for the specified key
+     * @throws NullPointerException when key is null
+     */
     boolean containsKey(String key);
 
+    /**
+     * Returns a Set view of the mappings contained in this map.
+     *
+     * @return a set view of the mappings contained in this map
+     */
     Set<Map.Entry<String, Object>> entrySet();
 
     /**
