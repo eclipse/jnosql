dedf5d7223c09ebafd223d13c4008228bc6437cc
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-19 06:10:00 -0300

    improves documentation at settins priority

--- communication/diana-core/src/main/java/org/jnosql/diana/api/SettingsPriority.java
+++ communication/diana-core/src/main/java/org/jnosql/diana/api/SettingsPriority.java
@@ -16,11 +16,49 @@
  */
 package org.jnosql.diana.api;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 /**
  * A utilitarian class that defines the priority to settings. It follows using Eclipse MicroProfile Configuration and JSR 382.
  * {@link System#getProperties()}, {@link System#getenv()} and {@link Settings}
  */
 public final class SettingsPriority {
     private SettingsPriority() {
+    }
+
+
+    /**
+     * Creates a {@link Settings} appending {@link System#getProperties()}, {@link System#getenv()}
+     * with priority
+     *
+     * @param settings the settings
+     * @return a new {@link Settings} instance
+     * @throws NullPointerException when settins is null
+     */
+    public static Settings get(Map<String, Object> settings) {
+        Objects.requireNonNull(settings, "settings is required");
+        return getSettings(settings);
+    }
+
+    /**
+     * Creates a {@link Settings} appending {@link System#getProperties()}, {@link System#getenv()}
+     * with priority
+     *
+     * @param settings the settings
+     * @return a new {@link Settings} instance
+     * @throws NullPointerException when settins is null
+     */
+    public static Settings get(Settings settings) {
+        Objects.requireNonNull(settings, "settings is required");
+        return getSettings(settings.toMap());
+    }
+
+    private static Settings getSettings(Map<String, Object> settings) {
+        Map<String, Object> configurations = new HashMap<>(settings);
+        settings.putAll(System.getenv());
+        System.getProperties().forEach((k, v) -> configurations.put(k.toString(), v));
+        return Settings.of(configurations);
     }
 }
