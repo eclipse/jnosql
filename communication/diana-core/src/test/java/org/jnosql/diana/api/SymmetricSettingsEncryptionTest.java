21167e5a94f94f0d10a8cf5d9fec9b9a798e081a
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-20 01:26:05 -0300

    fixes imports

--- communication/diana-core/src/test/java/org/jnosql/diana/api/SymmetricSettingsEncryptionTest.java
+++ communication/diana-core/src/test/java/org/jnosql/diana/api/SymmetricSettingsEncryptionTest.java
@@ -16,10 +16,6 @@
  */
 package org.jnosql.diana.api;
 
-import org.jnosql.diana.api.EncryptionException;
-import org.jnosql.diana.api.Settings;
-import org.jnosql.diana.api.SettingsEncryption;
-import org.jnosql.diana.api.SymmetricSettingsEncryption;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
