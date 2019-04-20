ae772460c73c6303256ba04e0e5a42cd314f0aba
Otavio Santana <otaviopolianasantana@gmail.com>
2019-04-20 17:25:00 -0300

    creates commons configuration to databases

--- communication/diana-core/src/main/java/org/jnosql/diana/api/Configurations.java
+++ communication/diana-core/src/main/java/org/jnosql/diana/api/Configurations.java
@@ -1,4 +1,38 @@
+/*
+ *
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ *
+ */
 package org.jnosql.diana.api;
 
-public class Configurations {
+import java.util.function.Supplier;
+
+public enum  Configurations implements Supplier<String> {
+    USER("jakarta.nosql.user"),
+    PASSWORD("jakarta.nosql.password"),
+    HOST("jakarta.nosql.host"),
+    QUERY("jakarta.nosql.query");
+
+    private final String configuration;
+
+    Configurations(String configuration) {
+        this.configuration = configuration;
+    }
+
+
+    @Override
+    public String get() {
+        return configuration;
+    }
 }
