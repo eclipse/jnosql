ca309c665594c209dfc30ce99ac9308c96422b24
Otavio Santana <otaviopolianasantana@gmail.com>
2019-10-01 06:22:47 -0500

    moves the code to column

---
+++ artemis/artemis-column/src/main/java/org/eclipse/jnosql/artemis/column/configuration/ColumnTemplateConverter.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.column.configuration;
+
+import jakarta.nosql.column.ColumnFamilyManager;
+import jakarta.nosql.mapping.column.ColumnTemplate;
+import jakarta.nosql.mapping.column.ColumnTemplateProducer;
+import org.eclipse.jnosql.artemis.configuration.SettingsConverter;
+import org.eclipse.jnosql.artemis.util.BeanManagers;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.Converter;
+
+/**
+ * Converter the {@link String} to {@link ColumnTemplate} it will use the {@link SettingsConverter} and
+ * find by the provider that should be an implementation of {@link jakarta.nosql.column.ColumnConfiguration}
+ */
+public class ColumnTemplateConverter implements Converter<ColumnTemplate> {
+
+    @Override
+    public ColumnTemplate convert(String value) {
+        Config config = BeanManagers.getInstance(Config.class);
+        final ColumnFamilyManager manager = config.getValue(value, ColumnFamilyManager.class);
+        ColumnTemplateProducer producer = BeanManagers.getInstance(ColumnTemplateProducer.class);
+
+        return producer.get(manager);
+    }
+}
---
+++ artemis/artemis-column/src/test/java/org/eclipse/jnosql/artemis/column/configuration/ColumnFamilyManagerAsynConverterTest.java
@@ -0,0 +1,98 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.column.configuration;
+
+import jakarta.nosql.column.ColumnFamilyManagerAsync;
+import org.eclipse.jnosql.artemis.CDIExtension;
+import org.eclipse.jnosql.artemis.column.configuration.ColumnConfigurationAsyncMock.ColumnFamilyManagerAsyncMock;
+import org.eclipse.jnosql.artemis.configuration.ConfigurationException;
+import org.eclipse.microprofile.config.Config;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.inject.Inject;
+import java.util.NoSuchElementException;
+import java.util.UUID;
+
+@ExtendWith(CDIExtension.class)
+class ColumnFamilyManagerAsynConverterTest {
+
+
+    @Inject
+    private Config config;
+
+    @Test
+    public void shouldReturnErrorWhenThereIsNoProvider() {
+        final String prefix = UUID.randomUUID().toString();
+        System.setProperty(prefix, prefix);
+        System.setProperty(prefix + ".settings.key", "value");
+        System.setProperty(prefix + ".settings.key2", "value2");
+        Assertions.assertThrows(NoSuchElementException.class, () -> config.getValue(prefix, ColumnFamilyManagerAsync.class) );
+
+        System.clearProperty(prefix);
+        System.clearProperty(prefix + ".settings.key");
+        System.clearProperty(prefix + ".settings.key2");
+    }
+
+    @Test
+    public void shouldReturnErrorWhenThereIsInvalidProvider() {
+        final String prefix = UUID.randomUUID().toString();
+        System.setProperty(prefix, prefix);
+        System.setProperty(prefix + ".settings.key", "value");
+        System.setProperty(prefix + ".settings.key2", "value2");
+        System.setProperty(prefix + ".provider", "java.lang.String");
+        Assertions.assertThrows(ConfigurationException.class, () -> config.getValue(prefix, ColumnFamilyManagerAsync.class) );
+
+        System.clearProperty(prefix);
+        System.clearProperty(prefix + ".settings.key");
+        System.clearProperty(prefix + ".settings.key2");
+        System.clearProperty(prefix + ".provider");
+    }
+
+    @Test
+    public void shouldReturnErrorWhenTableIsMissing() {
+        final String prefix = UUID.randomUUID().toString();
+        System.setProperty(prefix, prefix);
+        System.setProperty(prefix + ".settings.key", "value");
+        System.setProperty(prefix + ".settings.key2", "value2");
+        System.setProperty(prefix + ".provider", ColumnConfigurationAsyncMock.class.getName());
+        Assertions.assertThrows(NoSuchElementException.class, () -> config.getValue(prefix, ColumnFamilyManagerAsync.class) );
+
+        System.clearProperty(prefix);
+        System.clearProperty(prefix + ".settings.key");
+        System.clearProperty(prefix + ".settings.key2");
+        System.clearProperty(prefix + ".provider");
+    }
+
+    @Test
+    public void shouldReturnManager() {
+        final String prefix = UUID.randomUUID().toString();
+        System.setProperty(prefix, prefix);
+        System.setProperty(prefix + ".settings.key", "value");
+        System.setProperty(prefix + ".settings.key2", "value2");
+        System.setProperty(prefix + ".provider", ColumnConfigurationAsyncMock.class.getName());
+        System.setProperty(prefix + ".database", "database");
+        final ColumnFamilyManagerAsync manager = config.getValue(prefix, ColumnFamilyManagerAsync.class);
+        final ColumnFamilyManagerAsyncMock managerMock = ColumnFamilyManagerAsyncMock.class.cast(manager);
+        Assertions.assertEquals("database", managerMock.getDatabase());
+        System.clearProperty(prefix);
+        System.clearProperty(prefix + ".settings.key");
+        System.clearProperty(prefix + ".settings.key2");
+        System.clearProperty(prefix + ".provider");
+        System.clearProperty(prefix + ".database");
+    }
+
+}
