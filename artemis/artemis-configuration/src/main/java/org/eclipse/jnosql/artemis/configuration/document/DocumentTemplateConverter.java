4d25c789f48b4357b34f3140030fb0d3ca1eb75d
Otavio Santana <otaviopolianasantana@gmail.com>
2019-09-30 22:27:40 -0500

    creates template to converter

---
+++ artemis/artemis-configuration/src/main/java/org/eclipse/jnosql/artemis/configuration/document/DocumentTemplateConverter.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2019 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.artemis.configuration.document;
+
+import jakarta.nosql.document.DocumentCollectionManager;
+import jakarta.nosql.mapping.document.DocumentTemplate;
+import jakarta.nosql.mapping.document.DocumentTemplateProducer;
+import org.eclipse.jnosql.artemis.configuration.SettingsConverter;
+import org.eclipse.jnosql.artemis.util.BeanManagers;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.Converter;
+
+/**
+ * Converter the {@link String} to {@link DocumentTemplate} it will use the {@link SettingsConverter} and
+ * find by the provider that should be an implementation of {@link jakarta.nosql.column.ColumnConfiguration}
+ */
+public class DocumentTemplateConverter implements Converter<DocumentTemplate> {
+
+    @Override
+    public DocumentTemplate convert(String value) {
+        Config config = BeanManagers.getInstance(Config.class);
+        final DocumentCollectionManager manager = config.getValue(value, DocumentCollectionManager.class);
+        DocumentTemplateProducer producer = BeanManagers.getInstance(DocumentTemplateProducer.class);
+
+        return producer.get(manager);
+    }
+}
