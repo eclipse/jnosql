7bb66bafdaa04b63d5a70f4e3ff1237d78aac981
Otavio Santana <otaviopolianasantana@gmail.com>
2019-10-01 20:03:13 -0500

    creates graph converter

--- artemis/artemis-graph/src/main/java/org/eclipse/jnosql/artemis/graph/configuration/GraphConverter.java
+++ artemis/artemis-graph/src/main/java/org/eclipse/jnosql/artemis/graph/configuration/GraphConverter.java
@@ -12,37 +12,36 @@
  *
  *   Otavio Santana
  */
-package org.eclipse.jnosql.artemis.document.configuration;
+package org.eclipse.jnosql.artemis.graph.configuration;
 
 import jakarta.nosql.Settings;
-import jakarta.nosql.document.DocumentCollectionManagerFactory;
-import jakarta.nosql.document.DocumentConfiguration;
 import jakarta.nosql.mapping.reflection.Reflections;
+import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.eclipse.jnosql.artemis.configuration.ConfigurationException;
 import org.eclipse.jnosql.artemis.configuration.SettingsConverter;
+import org.eclipse.jnosql.artemis.graph.GraphConfiguration;
 import org.eclipse.jnosql.artemis.util.BeanManagers;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.Converter;
 
 /**
- * Converter the {@link String} to {@link DocumentCollectionManagerFactory} it will use the {@link SettingsConverter} and
- * find by the provider that should be an implementation of {@link DocumentConfiguration}
+ * Converter the {@link String} to {@link Graph}
  */
-public class DocumentCollectionManagerFactoryConverter implements Converter<DocumentCollectionManagerFactory> {
+public class GraphConverter implements Converter<Graph> {
 
     @Override
-    public DocumentCollectionManagerFactory convert(String value) {
+    public Graph convert(String value) {
         final SettingsConverter settingsConverter = BeanManagers.getInstance(SettingsConverter.class);
         Config config = BeanManagers.getInstance(Config.class);
         final Settings settings = settingsConverter.convert(value);
-        String provider = value + ".provider";
+        final String provider = value + ".provider";
         final Class<?> configurationClass = config.getValue(provider, Class.class);
-        if (DocumentConfiguration.class.isAssignableFrom(configurationClass)) {
+        if (GraphConfiguration.class.isAssignableFrom(configurationClass)) {
             final Reflections reflections = BeanManagers.getInstance(Reflections.class);
-            final DocumentConfiguration configuration = (DocumentConfiguration) reflections.newInstance(configurationClass);
-            return configuration.get(settings);
+            final GraphConfiguration configuration = (GraphConfiguration) reflections.newInstance(configurationClass);
+            return configuration.apply(settings);
 
         }
-        throw new ConfigurationException("The class " + configurationClass + " is not valid to " + DocumentConfiguration.class);
+        throw new ConfigurationException("The class " + configurationClass + " is not valid to " + GraphConfiguration.class);
     }
 }
