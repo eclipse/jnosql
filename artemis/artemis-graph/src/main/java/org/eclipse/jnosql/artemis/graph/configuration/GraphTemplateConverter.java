5e04a2fdfe3eb199fd7d8cfa663cd28f86c79d4f
Otavio Santana <otaviopolianasantana@gmail.com>
2019-10-01 20:30:36 -0500

    creates graph template conveter

--- artemis/artemis-graph/src/main/java/org/eclipse/jnosql/artemis/graph/configuration/GraphTemplateConverter.java
+++ artemis/artemis-graph/src/main/java/org/eclipse/jnosql/artemis/graph/configuration/GraphTemplateConverter.java
@@ -1,12 +1,25 @@
 package org.eclipse.jnosql.artemis.graph.configuration;
 
+import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.eclipse.jnosql.artemis.configuration.SettingsConverter;
 import org.eclipse.jnosql.artemis.graph.GraphTemplate;
+import org.eclipse.jnosql.artemis.graph.GraphTemplateProducer;
+import org.eclipse.jnosql.artemis.util.BeanManagers;
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.Converter;
 
+/**
+ * Converter the {@link String} to {@link GraphTemplate} it will use the {@link SettingsConverter} and
+ * find by the provider that should be an implementation of {@link org.eclipse.jnosql.artemis.graph.GraphConfiguration}
+ */
 public class GraphTemplateConverter implements Converter<GraphTemplate> {
 
     @Override
     public GraphTemplate convert(String value) {
-        return null;
+
+        Config config = BeanManagers.getInstance(Config.class);
+        final Graph manager = config.getValue(value, Graph.class);
+        GraphTemplateProducer producer = BeanManagers.getInstance(GraphTemplateProducer.class);
+        return producer.get(manager);
     }
 }
