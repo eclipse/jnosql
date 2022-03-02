10041bde2002a1519e03594e470937c5a5f14fbf
Otavio Santana <otaviopolianasantana@gmail.com>
2022-03-02 19:52:02 +0000

    Create city entity to test proposal
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping/mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/City.java
+++ mapping/mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/City.java
@@ -1,2 +1,18 @@
-package org.eclipse.jnosql.mapping.document.entities;public class City {
+/*
+ *  Copyright (c) 2021 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.document.entities;
+
+public class City {
 }
