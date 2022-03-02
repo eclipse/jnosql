81cd67b12d27981f025405c36979bcf910731019
Otavio Santana <otaviopolianasantana@gmail.com>
2022-03-02 19:53:06 +0000

    Improve methods in the citizen
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping/mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/Citizen.java
+++ mapping/mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/Citizen.java
@@ -14,5 +14,80 @@
  */
 package org.eclipse.jnosql.mapping.document.entities;
 
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+
+import java.util.Objects;
+
+@Entity
 public class Citizen {
+
+    @Id
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column
+    private City city;
+    Citizen() {
+    }
+
+    private Citizen(String id, String name, City city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Citizen citizen = (Citizen) o;
+        return Objects.equals(id, citizen.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Citizen{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", city=" + city +
+                '}';
+    }
+
+    public static Citizen of(String id, String name, City city){
+        Objects.requireNonNull(id, "id is required");
+        Objects.requireNonNull(name, "name is required");
+        Objects.requireNonNull(city, "city is required");
+        return new Citizen(id, name, city);
+    }
+
+    public static Citizen of(String id, String name){
+        Objects.requireNonNull(id, "id is required");
+        Objects.requireNonNull(name, "name is required");
+        return new Citizen(id, name, null);
+    }
 }
