61a078d2bf676efe7d4493f851c577941f9b4e09
Otavio Santana <otaviopolianasantana@gmail.com>
2022-03-02 22:05:04 +0000

    Create sample models application
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- mapping/mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/entities/City.java
+++ mapping/mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/entities/City.java
@@ -1,2 +1,63 @@
-package org.eclipse.jnosql.mapping.column.entities;public class City {
+/*
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.column.entities;
+
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+
+import java.util.Objects;
+
+@Entity
+public class City {
+
+    @Column
+    private String id;
+
+    @Column
+    private String name;
+
+    @Deprecated
+    public City() {
+    }
+
+
+    private City(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public static City of(String id, String name) {
+        Objects.requireNonNull(id, "id is required");
+        Objects.requireNonNull(name, "name is required");
+        return new City(id, name);
+    }
 }
