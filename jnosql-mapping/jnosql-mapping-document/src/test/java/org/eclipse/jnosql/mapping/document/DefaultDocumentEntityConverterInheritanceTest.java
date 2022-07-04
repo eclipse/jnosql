0c3ac3204c0e1a3b049a2451e9f732a5bddac4be
Otavio Santana <otaviopolianasantana@gmail.com>
2022-07-04 09:49:22 +0100

    Create cover tests to inheritance
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/DefaultDocumentEntityConverterInheritanceTest.java
@@ -0,0 +1,190 @@
+/*
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.document;
+
+import jakarta.nosql.document.DocumentEntity;
+import jakarta.nosql.mapping.document.DocumentEntityConverter;
+import jakarta.nosql.tck.entities.inheritance.EmailNotification;
+import jakarta.nosql.tck.entities.inheritance.LargeProject;
+import jakarta.nosql.tck.entities.inheritance.Project;
+import jakarta.nosql.tck.entities.inheritance.SmallProject;
+import jakarta.nosql.tck.entities.inheritance.SmsNotification;
+import jakarta.nosql.tck.entities.inheritance.SocialMediaNotification;
+import jakarta.nosql.tck.test.CDIExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@CDIExtension
+class DefaultDocumentEntityConverterInheritanceTest {
+
+
+    @Inject
+    private DocumentEntityConverter converter;
+
+    @Test
+    public void shouldConvertProjectToSmallProject() {
+        DocumentEntity entity = DocumentEntity.of("Project");
+        entity.add("name", "Small Project");
+        entity.add("investor", "Otavio Santana");
+        entity.add("size", "Small");
+        Project project = this.converter.toEntity(entity);
+        assertEquals("Small Project", project.getName());
+        assertEquals(SmallProject.class, project.getClass());
+        SmallProject smallProject = SmallProject.class.cast(project);
+        assertEquals("Otavio Santana", smallProject.getInvestor());
+    }
+
+    @Test
+    public void shouldConvertProjectToLargeProject() {
+        DocumentEntity entity = DocumentEntity.of("Project");
+        entity.add("name", "Large Project");
+        entity.add("budget", BigDecimal.TEN);
+        entity.add("size", "Large");
+        Project project = this.converter.toEntity(entity);
+        assertEquals("Large Project", project.getName());
+        assertEquals(LargeProject.class, project.getClass());
+        LargeProject smallProject = LargeProject.class.cast(project);
+        assertEquals(BigDecimal.TEN, smallProject.getBudget());
+    }
+
+    @Test
+    public void shouldConvertLargeProjectToDocumentEntity() {
+        LargeProject project = new LargeProject();
+        project.setName("Large Project");
+        project.setBudget(BigDecimal.TEN);
+        DocumentEntity entity = this.converter.toDocument(project);
+        assertNotNull(entity);
+        assertEquals("Project", entity.getName());
+        assertEquals(project.getName(), entity.find("name", String.class).get());
+        assertEquals(project.getBudget(), entity.find("budget", BigDecimal.class).get());
+        assertEquals("Large", entity.find("type", String.class).get());
+    }
+
+    @Test
+    public void shouldConvertSmallProjectToDocumentEntity() {
+        SmallProject project = new SmallProject();
+        project.setName("Small Project");
+        project.setInvestor("Otavio Santana");
+        DocumentEntity entity = this.converter.toDocument(project);
+        assertNotNull(entity);
+        assertEquals("Project", entity.getName());
+        assertEquals(project.getName(), entity.find("name", String.class).get());
+        assertEquals(project.getInvestor(), entity.find("investor", String.class).get());
+        assertEquals("Small", entity.find("type", String.class).get());
+    }
+
+    @Test
+    public void shouldConvertDocumentEntityToSocialMedia(){
+        LocalDate date = LocalDate.now();
+        DocumentEntity entity = DocumentEntity.of("Notification");
+        entity.add("_id", 100L);
+        entity.add("name", "Social Media");
+        entity.add("nickname", "otaviojava");
+        entity.add("createdOn",date);
+        entity.add("type", SocialMediaNotification.class.getSimpleName());
+        SocialMediaNotification notification = this.converter.toEntity(entity);
+        assertEquals(100L, notification.getId());
+        assertEquals("Social Media", notification.getName());
+        assertEquals("otaviojava", notification.getNickname());
+        assertEquals(date, notification.getCreatedOn());
+    }
+
+    @Test
+    public void shouldConvertDocumentEntityToSms(){
+        LocalDate date = LocalDate.now();
+        DocumentEntity entity = DocumentEntity.of("Notification");
+        entity.add("_id", 100L);
+        entity.add("name", "SMS Notification");
+        entity.add("phone", "+351987654123");
+        entity.add("createdOn", date);
+        entity.add("type", "SMS");
+        SmsNotification notification = this.converter.toEntity(entity);
+        Assertions.assertEquals(100L, notification.getId());
+        Assertions.assertEquals("SMS Notification", notification.getName());
+        Assertions.assertEquals("351987654123", notification.getPhone());
+        assertEquals(date, notification.getCreatedOn());
+    }
+
+    @Test
+    public void shouldConvertDocumentEntityToEmail(){
+        LocalDate date = LocalDate.now();
+        DocumentEntity entity = DocumentEntity.of("Notification");
+        entity.add("_id", 100L);
+        entity.add("name", "Email Notification");
+        entity.add("phone", "otavio@otavio.test");
+        entity.add("createdOn", date);
+        entity.add("type", "Email");
+        EmailNotification notification = this.converter.toEntity(entity);
+        Assertions.assertEquals(100L, notification.getId());
+        Assertions.assertEquals("SMS Notification", notification.getName());
+        Assertions.assertEquals("otavio@otavio.test", notification.getEmail());
+        assertEquals(date, notification.getCreatedOn());
+    }
+
+    @Test
+    public void shouldConvertSocialMediaToDocumentEntity(){
+        SocialMediaNotification notification = new SocialMediaNotification();
+        notification.setId(100L);
+        notification.setName("Social Media");
+        notification.setCreatedOn(LocalDate.now());
+        notification.setNickname("otaviojava");
+        DocumentEntity entity = this.converter.toDocument(notification);
+        assertNotNull(entity);
+        assertEquals("Notification", entity.getName());
+        assertEquals(notification.getId(), entity.find("_id", Long.class).get());
+        assertEquals(notification.getName(), entity.find("name", String.class).get());
+        assertEquals(notification.getNickname(), entity.find("nickname", String.class).get());
+        assertEquals(notification.getCreatedOn(), entity.find("createdOn", LocalDate.class).get());
+    }
+
+    @Test
+    public void shouldConvertSmsToDocumentEntity(){
+        SmsNotification notification = new SmsNotification();
+        notification.setId(100L);
+        notification.setName("SMS");
+        notification.setCreatedOn(LocalDate.now());
+        notification.setPhone("+351123456987");
+        DocumentEntity entity = this.converter.toDocument(notification);
+        assertNotNull(entity);
+        assertEquals("Notification", entity.getName());
+        assertEquals(notification.getId(), entity.find("_id", Long.class).get());
+        assertEquals(notification.getName(), entity.find("name", String.class).get());
+        assertEquals(notification.getPhone(), entity.find("phone", String.class).get());
+        assertEquals(notification.getCreatedOn(), entity.find("createdOn", LocalDate.class).get());
+    }
+
+    @Test
+    public void shouldConvertEmailToDocumentEntity(){
+        EmailNotification notification = new EmailNotification();
+        notification.setId(100L);
+        notification.setName("Email Media");
+        notification.setCreatedOn(LocalDate.now());
+        notification.setEmail("otavio@otavio.test.com");
+        DocumentEntity entity = this.converter.toDocument(notification);
+        assertNotNull(entity);
+        assertEquals("Notification", entity.getName());
+        assertEquals(notification.getId(), entity.find("_id", Long.class).get());
+        assertEquals(notification.getName(), entity.find("name", String.class).get());
+        assertEquals(notification.getEmail(), entity.find("phone", String.class).get());
+        assertEquals(notification.getCreatedOn(), entity.find("createdOn", LocalDate.class).get());
+    }
+}
