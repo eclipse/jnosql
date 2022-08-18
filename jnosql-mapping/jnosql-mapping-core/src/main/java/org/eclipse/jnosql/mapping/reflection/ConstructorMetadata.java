1e4b8dd6c4c65030681e1c302d722f77c32a527b
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-18 08:34:30 +0100

    Create ConstructorMetadata
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/ConstructorMetadata.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/ConstructorMetadata.java
@@ -1,2 +1,69 @@
-package org.eclipse.jnosql.mapping.reflection;public interface ConstructorMetadata {
+/*
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.reflection;
+
+import java.lang.reflect.Constructor;
+import java.sql.ParameterMetaData;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * This class has information about {@link java.lang.reflect.Constructor}
+ */
+public final class ConstructorMetadata {
+
+
+    private final Constructor<?> constructor;
+    private final List<ParameterMetaData> parameters;
+
+    ConstructorMetadata(Constructor<?> constructor, List<ParameterMetaData> parameters) {
+        this.constructor = constructor;
+        this.parameters = parameters;
+    }
+
+    public List<ParameterMetaData> getParameters() {
+        return Collections.unmodifiableList(parameters);
+    }
+
+    public Constructor<?> getConstructor() {
+        return constructor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ConstructorMetadata that = (ConstructorMetadata) o;
+        return Objects.equals(constructor, that.constructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(constructor);
+    }
+
+    @Override
+    public String toString() {
+        return "ConstructorMetadata{" +
+                "constructor=" + constructor +
+                ", parameters=" + parameters +
+                '}';
+    }
 }
