8bb1a7e64d6ae7557d4d967fccd25142726ca19c
Otavio Santana <otaviopolianasantana@gmail.com>
2022-08-29 10:41:19 +0100

    Create observer to new event
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-validation/src/test/java/org/eclipse/jnosql/mapping/validation/ConstructorValidationTest.java
+++ jnosql-mapping/jnosql-mapping-validation/src/test/java/org/eclipse/jnosql/mapping/validation/ConstructorValidationTest.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2017 Otávio Santana and others
+ *  Copyright (c) 2022 Otávio Santana and others
  *   All rights reserved. This program and the accompanying materials
  *   are made available under the terms of the Eclipse Public License v1.0
  *   and Apache License v2.0 which accompanies this distribution.
@@ -14,25 +14,8 @@
  */
 package org.eclipse.jnosql.mapping.validation;
 
+import org.eclipse.jnosql.mapping.test.CDIExtension;
 
-import jakarta.nosql.mapping.EntityPrePersist;
-import org.eclipse.jnosql.mapping.ConstructorEvent;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
-
-@ApplicationScoped
-class EntityObserver {
-
-    @Inject
-    private MappingValidator validator;
-
-    void validate(@Observes EntityPrePersist entity) {
-        this.validator.validate(entity.getValue());
-    }
-
-    void validate(@Observes ConstructorEvent event) {
-        this.validator.validate(event);
-    }
+@CDIExtension
+public class ConstructorValidationTest {
 }
