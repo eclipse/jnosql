cfd25ddf9fde962efaa9521fe1b027e103e1623b
Otavio Santana <otaviopolianasantana@gmail.com>
2022-09-03 07:22:37 +0100

    create test to column template test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-column/src/test/java/org/eclipse/jnosql/mapping/column/ColumnTemplateTest.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) 2022 Otávio Santana and others
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.column;
+
+import jakarta.nosql.mapping.Template;
+import jakarta.nosql.tck.test.CDIExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+@CDIExtension
+class ColumnTemplateTest {
+
+    @Inject
+    private Template template;
+    
+    @Test
+    public void shouldInjectTemplate() {
+        Assertions.assertNotNull(template);
+    }
+}
