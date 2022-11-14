c11b23697caae9c414bc915817451edd80bd8679
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-14 17:48:27 +0000

    Add mapping configuation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/config/MappingConfigurations.java
@@ -0,0 +1,61 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.config;
+
+import java.util.function.Supplier;
+
+/**
+ * The properties around Eclipse JNoSQL mapping for define create manager instances.
+ */
+public enum MappingConfigurations implements Supplier<String> {
+    /**
+     * Define the KeyValueConfiguration that creates a BucketManager instance.
+     * It is necessary when there is more than one implementation; otherwise,  it will find automatically.
+     */
+    KEY_VALUE_PROVIDER("jnosql.keyvalue.provider"),
+    /**
+     *Define the key-value database name.
+     */
+    KEY_VALUE_DATABASE("jnosql.keyvalue.database"),
+    /**
+     * Define the DocumentConfiguration that creates a DocumentCollectionManager instance.
+     * It is necessary when there is more than one implementation; otherwise,  it will find automatically.
+     */
+    DOCUMENT_PROVIDER("jnosql.document.provider"),
+    /**
+     *Define the document database name.
+     */
+    DOCUMENT_DATABASE("jnosql.document.database"),
+    /**
+     * Define the ColumnConfiguration that creates a ColumnFamilyManager instance.
+     * It is necessary when there is more than one implementation; otherwise,  it will find automatically.
+     */
+    COLUMN_PROVIDER("jnosql.column.provider"),
+    /**
+     *Define the column database name.
+     */
+    COLUMN_DATABASE("jnosql.column.database");
+
+    private final String value;
+
+    MappingConfigurations(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String get() {
+        return value;
+    }
+}
