47c7bcc9b5379d37f81bf605290c72f0bc87f519
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-14 18:55:27 +0000

    add test to supplier
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-key-value/src/test/java/org/eclipse/jnosql/mapping/keyvalue/configuration/BucketManagerSupplierTest.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (c) 2022 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.keyvalue.configuration;
+
+import jakarta.nosql.keyvalue.BucketManager;
+import jakarta.nosql.tck.test.CDIExtension;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.eclipse.jnosql.mapping.config.MappingConfigurations.KEY_VALUE_DATABASE;
+import static org.eclipse.jnosql.mapping.config.MappingConfigurations.KEY_VALUE_PROVIDER;
+import static org.junit.jupiter.api.Assertions.*;
+
+@CDIExtension
+class BucketManagerSupplierTest {
+
+    @Inject
+    private BucketManagerSupplier supplier;
+
+    @Test
+    public void shouldGetBucketManager() {
+        System.setProperty(KEY_VALUE_PROVIDER.get(), KeyValueConfigurationMock.class.getName());
+        System.setProperty(KEY_VALUE_DATABASE.get(), "database");
+        BucketManager manager = supplier.get();
+        Assertions.assertNotNull(manager);
+
+        System.clearProperty(KEY_VALUE_PROVIDER.get());
+        System.clearProperty(KEY_VALUE_DATABASE.get());
+    }
+
+}
