a8c818f4c66a5449df6c568b89d1cca45c6d7a06
Otavio Santana <otaviopolianasantana@gmail.com>
2022-11-21 10:49:53 +0000

    refactoring scanning using scan
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/EntityMetadataExtension.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/EntityMetadataExtension.java
@@ -15,17 +15,17 @@
 package org.eclipse.jnosql.mapping.reflection;
 
 
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import jakarta.nosql.mapping.Embeddable;
 import jakarta.nosql.mapping.Entity;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.enterprise.inject.spi.AfterBeanDiscovery;
 import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.inject.spi.ProcessAnnotatedType;
-import javax.enterprise.inject.spi.WithAnnotations;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
@@ -43,43 +43,32 @@ public class EntityMetadataExtension implements Extension {
 
     private final Map<Class<?>, EntityMetadata> classes = new ConcurrentHashMap<>();
 
-    private final ClassConverter classConverter;
+    private final ClassConverter converter;
 
     {
-        classConverter = new ClassConverter(ClassOperationFactory.INSTANCE.getReflections());
+        converter = new ClassConverter(ClassOperationFactory.INSTANCE.getReflections());
     }
 
-    /**
-     * Event observer
-     *
-     * @param target the target
-     * @param <T>    the type
-     */
-    public <T> void loadEntity(@Observes @WithAnnotations({Entity.class, Embeddable.class}) final ProcessAnnotatedType<T> target) {
-
-        AnnotatedType<T> annotatedType = target.getAnnotatedType();
-        if (annotatedType.isAnnotationPresent(Entity.class)) {
-            Class<T> javaClass = target.getAnnotatedType().getJavaClass();
-            EntityMetadata entityMetadata = classConverter.create(javaClass);
-            if (entityMetadata.hasEntityName()) {
-                mappings.put(entityMetadata.getName(), entityMetadata);
-            }
-            classes.put(javaClass, entityMetadata);
-        } else if (isSubElement(annotatedType)) {
-            Class<T> javaClass = target.getAnnotatedType().getJavaClass();
-            EntityMetadata entityMetadata = classConverter.create(javaClass);
-            classes.put(javaClass, entityMetadata);
-        }
-
-    }
 
     public void afterBeanDiscovery(@Observes BeforeBeanDiscovery event) {
         LOGGER.fine("Starting the scanning process for Entity and Embeddable annotations: ");
 
-    }
-
-    private <T> boolean isSubElement(AnnotatedType<T> annotatedType) {
-        return annotatedType.isAnnotationPresent(Embeddable.class);
+        try (ScanResult result = new ClassGraph().enableAllInfo().scan()) {
+            ClassInfoList entities = result.getClassesWithAnnotation(Entity.class);
+            for (Class<?> entity : entities.loadClasses()) {
+                EntityMetadata entityMetadata = converter.create(entity);
+                if (entityMetadata.hasEntityName()) {
+                    mappings.put(entityMetadata.getName(), entityMetadata);
+                }
+                classes.put(entity, entityMetadata);
+            }
+            ClassInfoList embeddables = result.getClassesWithAnnotation(Embeddable.class);
+            for (Class<?> embeddable : embeddables.loadClasses()) {
+                EntityMetadata entityMetadata = converter.create(embeddable);
+                classes.put(embeddable, entityMetadata);
+            }
+        }
+        LOGGER.fine("Finishing the scanning with total of " + classes.size() + " scanned.");
     }
 
 
@@ -103,7 +92,7 @@ public class EntityMetadataExtension implements Extension {
 
     @Override
     public String toString() {
-        return "EntityMetadataExtension{" + "classConverter=" + classConverter +
+        return "EntityMetadataExtension{" + "classConverter=" + converter +
                 ", mappings-size=" + mappings.size() +
                 ", classes=" + classes +
                 '}';
