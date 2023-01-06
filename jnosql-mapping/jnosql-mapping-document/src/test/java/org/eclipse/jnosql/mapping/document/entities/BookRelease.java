22e25d7f231d6a403d7e64d6a9be7bea74851586
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-06 18:44:14 -0300

    Update and define as final book release
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/BookRelease.java
+++ jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/BookRelease.java
@@ -14,5 +14,77 @@
  */
 package org.eclipse.jnosql.mapping.document.entities;
 
+
+import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.Entity;
+import jakarta.nosql.mapping.Id;
+
+import java.time.Year;
+import java.util.Objects;
+
+@Entity
 public class BookRelease {
+
+    @Id
+    private final String id;
+    @Column("name")
+    private final String name;
+    @Column("author")
+    private final String author;
+    @Column("year")
+    private final Year year;
+
+    public BookRelease(@Id String id, @Column("name") String name,
+                       @Column("author") String author,
+                       @Column("year") Year year) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BookRelease that = (BookRelease) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "BookRelease{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
