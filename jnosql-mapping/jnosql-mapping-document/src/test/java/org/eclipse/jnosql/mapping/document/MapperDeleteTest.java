5a61d290494d6f4c8ec70704d75a8b30a247cd14
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-09 20:14:49 -0300

    update mapper delete test
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/MapperDeleteTest.java
+++ jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/MapperDeleteTest.java
@@ -14,11 +14,222 @@
  */
 package org.eclipse.jnosql.mapping.document;
 
+import jakarta.enterprise.inject.Instance;
+import jakarta.inject.Inject;
+import jakarta.nosql.document.DocumentDeleteQuery;
+import jakarta.nosql.document.DocumentManager;
+import jakarta.nosql.document.DocumentQuery;
+import jakarta.nosql.mapping.Converters;
+import jakarta.nosql.mapping.QueryMapper;
+import jakarta.nosql.mapping.document.DocumentEntityConverter;
+import jakarta.nosql.mapping.document.DocumentEventPersistManager;
+import jakarta.nosql.tck.entities.Address;
+import jakarta.nosql.tck.entities.Money;
+import jakarta.nosql.tck.entities.Person;
+import jakarta.nosql.tck.entities.Worker;
 import jakarta.nosql.tck.test.CDIExtension;
+import org.eclipse.jnosql.mapping.reflection.EntitiesMetadata;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.math.BigDecimal;
+
+import static jakarta.nosql.document.DocumentDeleteQuery.delete;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @CDIExtension
 public class MapperDeleteTest {
 
+    @Inject
+    private DocumentEntityConverter converter;
 
+    @Inject
+    private EntitiesMetadata entities;
+
+    @Inject
+    private Converters converters;
+
+    private DocumentManager managerMock;
+
+    private DefaultDocumentTemplate template;
+
+
+    private ArgumentCaptor<DocumentDeleteQuery> captor;
+
+    @BeforeEach
+    public void setUp() {
+        managerMock = Mockito.mock(DocumentManager.class);
+        DocumentEventPersistManager persistManager = Mockito.mock(DocumentEventPersistManager.class);
+        Instance<DocumentManager> instance = Mockito.mock(Instance.class);
+        this.captor = ArgumentCaptor.forClass(DocumentDeleteQuery.class);
+        when(instance.get()).thenReturn(managerMock);
+        DefaultDocumentWorkflow workflow = new DefaultDocumentWorkflow(persistManager, converter);
+        this.template = new DefaultDocumentTemplate(converter, instance, workflow,
+                persistManager, entities, converters);
+    }
+
+    @Test
+    public void shouldReturnDeleteFrom() {
+        template.delete(Person.class).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").build();
+        assertEquals(queryExpected, query);
+    }
+
+
+    @Test
+    public void shouldSelectWhereNameEq() {
+        template.delete(Person.class).where("name").eq("Ada").execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("name").eq("Ada").build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameLike() {
+        template.delete(Person.class).where("name").like("Ada").execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("name").like("Ada").build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameGt() {
+        template.delete(Person.class).where("id").gt(10).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id").gt(10L).build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameGte() {
+        template.delete(Person.class).where("id").gte(10).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id").gte(10L).build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameLt() {
+        template.delete(Person.class).where("id").lt(10).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id").lt(10L).build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameLte() {
+        template.delete(Person.class).where("id").lte(10).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id").lte(10L).build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameBetween() {
+        template.delete(Person.class).where("id")
+                .between(10, 20).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id")
+                .between(10L, 20L).build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameNot() {
+        template.delete(Person.class).where("name").not().like("Ada").execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("name").not().like("Ada").build();
+        assertEquals(queryExpected, query);
+    }
+
+
+    @Test
+    public void shouldSelectWhereNameAnd() {
+        template.delete(Person.class).where("age").between(10, 20)
+                .and("name").eq("Ada").execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("age")
+                .between(10, 20)
+                .and("name").eq("Ada").build();
+
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldSelectWhereNameOr() {
+        template.delete(Person.class).where("id").between(10, 20)
+                .or("name").eq("Ada").execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id")
+                .between(10L, 20L)
+                .or("name").eq("Ada").build();
+
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldConvertField() {
+        template.delete(Person.class).where("id").eq("20")
+                .execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Person").where("_id").eq(20L)
+                .build();
+
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldUseAttributeConverter() {
+        template.delete(Worker.class).where("salary")
+                .eq(new Money("USD", BigDecimal.TEN)).execute();
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Worker").where("money")
+                .eq("USD 10").build();
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldQueryByEmbeddable() {
+        template.delete(Worker.class).where("job.city").eq("Salvador")
+                .execute();
+
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+        DocumentDeleteQuery queryExpected = delete().from("Worker").where("city").eq("Salvador")
+                .build();
+
+        assertEquals(queryExpected, query);
+    }
+
+    @Test
+    public void shouldQueryBySubEntity() {
+        template.delete(Address.class).where("zipCode.zip").eq("01312321")
+                .execute();
+
+        Mockito.verify(managerMock).delete(captor.capture());
+        DocumentDeleteQuery query = captor.getValue();
+
+        DocumentDeleteQuery queryExpected = delete().from("Address").where("zipCode.zip").eq("01312321")
+                .build();
+
+        assertEquals(queryExpected, query);
+    }
 
 }
