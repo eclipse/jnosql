3a98ff0c0b6bbd3d87f692a4e67ad9e965a095a4
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-10 07:36:32 -0300

    implement graph mapper delete
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GraphMapperDelete.java
+++ jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GraphMapperDelete.java
@@ -14,5 +14,108 @@
  */
 package org.eclipse.jnosql.mapping.graph;
 
-final class GraphMapperDelete {
+import jakarta.nosql.mapping.Converters;
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
+import org.apache.tinkerpop.gremlin.structure.Vertex;
+import org.eclipse.jnosql.mapping.reflection.EntityMetadata;
+import jakarta.nosql.mapping.QueryMapper.MapperDeleteFrom;
+import jakarta.nosql.mapping.QueryMapper.MapperDeleteNameCondition;
+import jakarta.nosql.mapping.QueryMapper.MapperDeleteNotCondition;
+import jakarta.nosql.mapping.QueryMapper.MapperDeleteWhere;
+
+import static java.util.Objects.requireNonNull;
+
+final class GraphMapperDelete extends AbstractMapperQuery implements MapperDeleteFrom,
+        MapperDeleteWhere, MapperDeleteNameCondition, MapperDeleteNotCondition {
+    GraphMapperDelete(EntityMetadata mapping, Converters converters, GraphTraversal<Vertex, Vertex> traversal,
+                      GraphConverter converter) {
+        super(mapping, converters, traversal, converter);
+    }
+
+    @Override
+    public MapperDeleteNameCondition where(String name) {
+        requireNonNull(name, "name is required");
+        this.name = name;
+        return this;
+    }
+
+
+    @Override
+    public MapperDeleteNameCondition and(String name) {
+        requireNonNull(name, "name is required");
+        this.name = name;
+        this.and = true;
+        return this;
+    }
+
+    @Override
+    public MapperDeleteNameCondition or(String name) {
+        requireNonNull(name, "name is required");
+        this.name = name;
+        this.and = false;
+        return this;
+    }
+
+
+    @Override
+    public MapperDeleteNotCondition not() {
+        this.negate = true;
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere eq(T value) {
+        eqImpl(value);
+        return this;
+    }
+
+    @Override
+    public MapperDeleteWhere like(String value) {
+        likeImpl(value);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere gt(T value) {
+        gtImpl(value);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere gte(T value) {
+        gteImpl(value);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere lt(T value) {
+        ltImpl(value);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere lte(T value) {
+        lteImpl(value);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere between(T valueA, T valueB) {
+        betweenImpl(valueA, valueB);
+        return this;
+    }
+
+    @Override
+    public <T> MapperDeleteWhere in(Iterable<T> values) {
+        inImpl(values);
+        return this;
+    }
+
+    @Override
+    public void execute() {
+        if (condition != null) {
+            traversal.filter(condition);
+        }
+        traversal.toStream().forEach(Vertex::remove);
+    }
 }
