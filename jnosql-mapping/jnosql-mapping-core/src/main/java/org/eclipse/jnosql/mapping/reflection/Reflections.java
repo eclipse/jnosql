3f10a8eb7e14e31a27676300d26e7e843a17e5fd
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-11 09:30:09 -0300

    simplify reflections removing converts Reflection to class
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/Reflections.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/reflection/Reflections.java
@@ -14,19 +14,40 @@
  */
 package org.eclipse.jnosql.mapping.reflection;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.nosql.mapping.Column;
+import jakarta.nosql.mapping.DiscriminatorColumn;
+import jakarta.nosql.mapping.DiscriminatorValue;
 import jakarta.nosql.mapping.Entity;
 import jakarta.nosql.mapping.Id;
+import jakarta.nosql.mapping.Inheritance;
+import jakarta.nosql.mapping.MappedSuperclass;
+import org.eclipse.jnosql.mapping.util.StringUtils;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Utilitarian class to reflection
  */
-public interface Reflections {
+@ApplicationScoped
+public class Reflections {
+
+    private static final Logger LOGGER = Logger.getLogger(Reflections.class.getName());
 
     /**
      * Return The Object from the Column.
@@ -35,7 +56,16 @@ public interface Reflections {
      * @param field  the field to return object
      * @return - the field value in Object
      */
-    Object getValue(Object object, Field field);
+    public Object getValue(Object object, Field field) {
+
+        try {
+            return field.get(object);
+        } catch (Exception exception) {
+            LOGGER.log(Level.FINEST, "There is an issue with returning value from this field.", exception);
+        }
+        return null;
+    }
+
 
     /**
      * Set the field in the Object.
@@ -45,7 +75,17 @@ public interface Reflections {
      * @param value  the value to object
      * @return - if the operation was execute with success
      */
-    boolean setValue(Object object, Field field, Object value);
+    public boolean setValue(Object object, Field field, Object value) {
+        try {
+
+            field.set(object, value);
+
+        } catch (Exception exception) {
+            LOGGER.log(Level.FINEST, "There is an issue with setting value from this field.", exception);
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Create new instance of this class.
@@ -54,7 +94,14 @@ public interface Reflections {
      * @param <T>         the instance type
      * @return the new instance that class
      */
-    <T> T newInstance(Constructor<T> constructor);
+    public <T> T newInstance(Constructor<T> constructor) {
+        try {
+            return constructor.newInstance();
+        } catch (Exception exception) {
+            LOGGER.log(Level.FINEST, "There is an issue to creating an entity from this constructor", exception);
+            return null;
+        }
+    }
 
 
     /**
@@ -64,7 +111,15 @@ public interface Reflections {
      * @param <T>   the instance type
      * @return the new instance that class
      */
-    <T> T newInstance(Class<T> type);
+    public <T> T newInstance(Class<T> type) {
+        try {
+            Constructor<T> constructor = getConstructor(type);
+            return newInstance(constructor);
+        } catch (Exception exception) {
+            LOGGER.log(Level.FINEST, "There is an issue to creating an entity from this constructor", exception);
+            return null;
+        }
+    }
 
     /**
      * Find the Column from the name field.
@@ -73,7 +128,14 @@ public interface Reflections {
      * @param type  the class
      * @return the field from the name
      */
-    Field getField(String string, Class<?> type);
+    public Field getField(String string, Class<?> type) {
+        for (Field field : type.getDeclaredFields()) {
+            if (field.getName().equals(string)) {
+                return field;
+            }
+        }
+        return null;
+    }
 
     /**
      * returns the generic type of field.
@@ -81,7 +143,11 @@ public interface Reflections {
      * @param field the field
      * @return a generic type
      */
-    Class<?> getGenericType(Field field);
+    public Class<?> getGenericType(Field field) {
+        ParameterizedType genericType = (ParameterizedType) field.getGenericType();
+        return (Class<?>) genericType.getActualTypeArguments()[0];
+
+    }
 
     /**
      * Make the given field accessible, explicitly setting it accessible
@@ -91,7 +157,13 @@ public interface Reflections {
      *
      * @param field field the field to make accessible
      */
-    void makeAccessible(Field field);
+    public void makeAccessible(Field field) {
+        if ((!Modifier.isPublic(field.getModifiers()) || !Modifier
+                .isPublic(field.getDeclaringClass().getModifiers()))
+                && !field.isAccessible()) {
+            field.setAccessible(true);
+        }
+    }
 
     /**
      * Make the given a constructor class accessible, explicitly setting it accessible
@@ -104,7 +176,40 @@ public interface Reflections {
      * @return the constructor class
      * @throws ConstructorException when the constructor has public and default
      */
-    <T> Constructor<T> getConstructor(Class<T> type);
+    public <T> Constructor<T> getConstructor(Class<T> type) {
+        final Predicate<Constructor<?>> defaultConstructorPredicate = c -> c.getParameterCount() == 0;
+        final Predicate<Constructor<?>> customConstructorPredicate = c -> {
+            for (Parameter parameter : c.getParameters()) {
+                if (parameter.getAnnotation(Id.class) != null || parameter.getAnnotation(Column.class) != null) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        List<Constructor<?>> constructors = Stream.
+                of(type.getDeclaredConstructors())
+                .filter(defaultConstructorPredicate.or(customConstructorPredicate))
+                .collect(toList());
+
+
+        if (constructors.isEmpty()) {
+            throw new ConstructorException(type);
+        }
+
+        Optional<Constructor<?>> publicConstructor = constructors
+                .stream()
+                .sorted(ConstructorComparable.INSTANCE)
+                .filter(c -> Modifier.isPublic(c.getModifiers()))
+                .findFirst();
+        if (publicConstructor.isPresent()) {
+            return (Constructor<T>) publicConstructor.get();
+        }
+
+        Constructor<?> constructor = constructors.get(0);
+        constructor.setAccessible(true);
+        return (Constructor<T>) constructor;
+    }
 
     /**
      * Returns the name of the entity. So it tries to read the {@link Entity} otherwise
@@ -114,7 +219,14 @@ public interface Reflections {
      * @return the {@link Entity} when is not blank otherwise {@link Class#getSimpleName()}
      * @throws NullPointerException when entity is null
      */
-    String getEntityName(Class<?> entity);
+    String getEntityName(Class<?> entity) {
+        requireNonNull(entity, "class entity is required");
+
+        if (isInheritance(entity)) {
+            return readEntity(entity.getSuperclass());
+        }
+        return readEntity(entity);
+    }
 
     /**
      * Returns the fields from the entity class
@@ -124,7 +236,22 @@ public interface Reflections {
      * {@link Id}
      * @throws NullPointerException when class entity is null
      */
-    List<Field> getFields(Class<?> type);
+    public List<Field> getFields(Class<?> type) {
+        requireNonNull(type, "class entity is required");
+
+        List<Field> fields = new ArrayList<>();
+
+        if (isMappedSuperclass(type)) {
+            fields.addAll(getFields(type.getSuperclass()));
+        }
+        Predicate<Field> hasColumnAnnotation = f -> f.getAnnotation(Column.class) != null;
+        Predicate<Field> hasIdAnnotation = f -> f.getAnnotation(Id.class) != null;
+
+        Stream.of(type.getDeclaredFields())
+                .filter(hasColumnAnnotation.or(hasIdAnnotation))
+                .forEach(fields::add);
+        return fields;
+    }
 
     /**
      * Checks if the class is annotated with {@link jakarta.nosql.mapping.MappedSuperclass} or
@@ -134,7 +261,12 @@ public interface Reflections {
      * @return if the class is annotated
      * @throws NullPointerException when type is null
      */
-    boolean isMappedSuperclass(Class<?> type);
+    public boolean isMappedSuperclass(Class<?> type) {
+        requireNonNull(type, "class entity is required");
+        Class<?> superclass = type.getSuperclass();
+        return superclass.getAnnotation(MappedSuperclass.class) != null
+                || superclass.getAnnotation(Inheritance.class) != null;
+    }
 
     /**
      * Checks if the field is annotated with {@link Column}
@@ -143,7 +275,10 @@ public interface Reflections {
      * @return if the field is annotated with {@link Column}
      * @throws NullPointerException when the field is null
      */
-    boolean isIdField(Field field);
+    public boolean isIdField(Field field) {
+        requireNonNull(field, "field is required");
+        return field.getAnnotation(Id.class) != null;
+    }
 
     /**
      * Gets the name from the field, so it reads the {@link Column#value()}
@@ -153,7 +288,13 @@ public interface Reflections {
      * @return the column name
      * @throws NullPointerException when the field is null
      */
-    String getColumnName(Field field);
+    public String getColumnName(Field field) {
+        requireNonNull(field, "field is required");
+        return Optional.ofNullable(field.getAnnotation(Column.class))
+                .map(Column::value)
+                .filter(StringUtils::isNotBlank)
+                .orElse(field.getName());
+    }
 
     /**
      * Gets the id name, so it reads the {@link Id#value()} otherwise {@link Field#getName()}
@@ -162,7 +303,13 @@ public interface Reflections {
      * @return the column name
      * @throws NullPointerException when the field is null
      */
-    String getIdName(Field field);
+    public String getIdName(Field field) {
+        requireNonNull(field, "field is required");
+        return Optional.ofNullable(field.getAnnotation(Id.class))
+                .map(Id::value)
+                .filter(StringUtils::isNotBlank)
+                .orElse(field.getName());
+    }
 
     /**
      * Reads the type annotation and checks if the inheritance has an
@@ -174,7 +321,22 @@ public interface Reflections {
      * @return the {@link InheritanceMetadata} or {@link Optional#empty()}
      * @throws NullPointerException when type is null
      */
-    Optional<InheritanceMetadata> getInheritance(Class<?> type);
+    public Optional<InheritanceMetadata> getInheritance(Class<?> type) {
+        Objects.requireNonNull(type, "entity is required");
+        if (isInheritance(type)) {
+            Class<?> parent = type.getSuperclass();
+            String discriminatorColumn = getDiscriminatorColumn(parent);
+            String discriminatorValue = getDiscriminatorValue(type);
+            return Optional.of(new InheritanceMetadata(discriminatorValue, discriminatorColumn,
+                    parent, type));
+        } else if (type.getAnnotation(Inheritance.class) != null) {
+            String discriminatorColumn = getDiscriminatorColumn(type);
+            String discriminatorValue = getDiscriminatorValue(type);
+            return Optional.of(new InheritanceMetadata(discriminatorValue, discriminatorColumn,
+                    type, type));
+        }
+        return Optional.empty();
+    }
 
     /**
      * Check if the entity has the {@link jakarta.nosql.mapping.Inheritance} annotation
@@ -182,8 +344,36 @@ public interface Reflections {
      * @param entity the entity
      * @return true if it has the {@link jakarta.nosql.mapping.Inheritance} annotation
      */
-    boolean hasInheritanceAnnotation(Class<?> entity);
+    public boolean hasInheritanceAnnotation(Class<?> entity) {
+        Objects.requireNonNull(entity, "entity is required");
+        return entity.getAnnotation(Inheritance.class) != null;
+    }
 
 
+    private String getDiscriminatorColumn(Class<?> parent) {
+        return Optional
+                .ofNullable(parent.getAnnotation(DiscriminatorColumn.class))
+                .map(DiscriminatorColumn::value)
+                .orElse(DiscriminatorColumn.DEFAULT_DISCRIMINATOR_COLUMN);
+    }
+
+    private String getDiscriminatorValue(Class<?> entity) {
+        return Optional
+                .ofNullable(entity.getAnnotation(DiscriminatorValue.class))
+                .map(DiscriminatorValue::value)
+                .orElse(entity.getSimpleName());
+    }
+
+    private String readEntity(Class<?> entity) {
+        return Optional.ofNullable((Entity) entity.getAnnotation(Entity.class))
+                .map(Entity::value)
+                .filter(StringUtils::isNotBlank)
+                .orElse(entity.getSimpleName());
+    }
+
+    private boolean isInheritance(Class<?> entity) {
+        Class<?> superclass = entity.getSuperclass();
+        return superclass.getAnnotation(Inheritance.class) != null;
+    }
 
 }
