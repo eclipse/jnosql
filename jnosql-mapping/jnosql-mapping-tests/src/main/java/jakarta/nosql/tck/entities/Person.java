491aa04e669bd4b26b7c959763074c6f14ec2ae8
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-13 17:11:34 -0300

    update copyright
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-tests/src/main/java/jakarta/nosql/tck/entities/Person.java
+++ jnosql-mapping/jnosql-mapping-tests/src/main/java/jakarta/nosql/tck/entities/Person.java
@@ -1,16 +1,16 @@
 /*
- * Copyright (c) 2022 Contributors to the Eclipse Foundation
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v. 2.0 which is available at
- * http://www.eclipse.org/legal/epl-2.0.
+ *   Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
  *
- * This Source Code may also be made available under the following Secondary
- * Licenses when the conditions for such availability set forth in the Eclipse
- * Public License v. 2.0 are satisfied: GNU General Public License v2.0
- * w/Classpath exception which is available at
- * https://www.gnu.org/software/classpath/license.html.
+ *    You may elect to redistribute this code under either of these licenses.
  *
- * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+ *    Contributors:
+ *
+ *    Otavio Santana
  */
 package jakarta.nosql.tck.entities;
 
