165d8a032cda68176373b30932a2190cf744d425
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-15 07:21:36 -0300

    update jupiter module
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-jupiter/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ *   Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *    You may elect to redistribute this code under either of these licenses.
+ *
+ *    Contributors:
+ *
+ *    Otavio Santana
+ */
+module org.eclipse.jnosql.mapping.test {
+    requires jakarta.nosql.core;
+    requires jakarta.data.api;
+    requires jakarta.cdi;
+    requires org.junit.jupiter.api;
+    exports org.eclipse.jnosql.mapping.test.jupiter;
+    exports org.eclipse.jnosql.mapping.test.entities;
+    exports org.eclipse.jnosql.mapping.test.entities.inheritance;
+    opens org.eclipse.jnosql.mapping.test.entities;
+    opens org.eclipse.jnosql.mapping.test.entities.inheritance;
+    exports org.eclipse.jnosql.mapping.test.entities.constructor;
+    opens org.eclipse.jnosql.mapping.test.jupiter;
+}
---
+++ jnosql-mapping/jnosql-jupiter/src/main/java/org/eclipse/jnosql/mapping/test/entities/Car.java
@@ -0,0 +1,54 @@
+/*
+ *   Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *    You may elect to redistribute this code under either of these licenses.
+ *
+ *    Contributors:
+ *
+ *    Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.test.entities;
+
+import org.eclipse.jnosql.mapping.Convert;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+@Entity
+public class Car {
+
+    @Id
+    @Convert(PlateConverter.class)
+    private Plate plate;
+
+    private String name;
+
+
+    public Plate getPlate() {
+        return plate;
+    }
+
+    public void setPlate(Plate plate) {
+        this.plate = plate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "plate=" + plate +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
