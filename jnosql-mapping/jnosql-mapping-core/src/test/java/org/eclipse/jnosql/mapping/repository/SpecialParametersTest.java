af6e990d2c30013c4d51bd816a31f9bfa66457b5
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-22 08:32:41 +0000

    Update test on special paramters
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/repository/SpecialParametersTest.java
+++ jnosql-mapping/jnosql-mapping-core/src/test/java/org/eclipse/jnosql/mapping/repository/SpecialParametersTest.java
@@ -15,13 +15,16 @@
 package org.eclipse.jnosql.mapping.repository;
 
 import jakarta.data.repository.Pageable;
+import jakarta.data.repository.Sort;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SpecialParametersTest {
 //should return pageable
+// should return pageable with sort
 //should return sorts
 //should keep the precedence
 
@@ -42,7 +45,46 @@ class SpecialParametersTest {
         Pageable pageable = Pageable.ofPage(10);
         SpecialParameters parameters = SpecialParameters.of(new Object[]{10, "Otavio", pageable});
         Assertions.assertFalse(parameters.isEmpty());
-        Assertions.assertEquals(parameters, parameters.getPageable().orElseThrow());
+        Assertions.assertEquals(pageable, parameters.getPageable().orElseThrow());
         Assertions.assertTrue(parameters.isSortEmpty());
+    }
+
+    @Test
+    public void shouldReturnPageableWithSort() {
+        Pageable pageable = Pageable.ofPage(10).sortBy(Sort.asc("name"),
+                Sort.desc("age"));
+        SpecialParameters parameters = SpecialParameters.of(new Object[]{10, "Otavio", pageable});
+        Assertions.assertFalse(parameters.isEmpty());
+        Assertions.assertEquals(pageable, parameters.getPageable().orElseThrow());
+        Assertions.assertFalse(parameters.isSortEmpty());
+        assertThat(parameters.getSorts()).hasSize(2)
+                .contains(Sort.asc("name"),
+                        Sort.desc("age"));
+    }
+
+    @Test
+    public void shouldReturnSort() {
+        Sort sort = Sort.asc("name");
+        SpecialParameters parameters = SpecialParameters.of(new Object[]{10, "Otavio", sort});
+        Assertions.assertFalse(parameters.isEmpty());
+        Assertions.assertTrue(parameters.getPageable().isEmpty());
+        Assertions.assertFalse(parameters.isSortEmpty());
+        assertThat(parameters.getSorts()).hasSize(1)
+                .contains(Sort.asc("name"));
+    }
+
+    @Test
+    public void shouldKeepOrder() {
+        Sort sort = Sort.asc("name");
+        Pageable pageable = Pageable.ofPage(10).sortBy(Sort.asc("name"),
+                Sort.desc("age"));
+
+        SpecialParameters parameters = SpecialParameters.of(new Object[]{10, "Otavio", sort, pageable});
+        Assertions.assertFalse(parameters.isEmpty());
+        Assertions.assertEquals(pageable, parameters.getPageable().orElseThrow());
+        Assertions.assertFalse(parameters.isSortEmpty());
+        assertThat(parameters.getSorts()).hasSize(3)
+                .containsExactly(sort, Sort.asc("name"),
+                        Sort.desc("age"));
     }
 }
