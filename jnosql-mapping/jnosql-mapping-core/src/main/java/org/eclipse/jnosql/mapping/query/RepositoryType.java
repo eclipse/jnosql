34c28f8aeb353bcdc70a5a12fcbfea9636c211c7
Otavio Santana <otaviopolianasantana@gmail.com>
2023-01-22 17:52:31 +0000

    fix return type
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/query/RepositoryType.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/query/RepositoryType.java
@@ -19,7 +19,9 @@ import jakarta.data.repository.PageableRepository;
 import jakarta.data.repository.Query;
 
 import java.lang.reflect.Method;
+import java.util.EnumSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.Predicate;
 
 /**
@@ -36,9 +38,21 @@ public enum RepositoryType {
      */
     FIND_BY("findBy"),
     /**
-     * Delete query method returning either no result (void) or the delete count. It starts with "deleteBy" key word
+     * Delete query method returning either no result (void) or the delete count. It starts with "deleteBy" keyword
      */
     DELETE_BY("deleteBy"),
+    /**
+     * Method that has the "FindAll" keyword
+     */
+    FIND_ALL("findAll"),
+    /**
+     * Count projection returning a numeric result. It starts with "countBy" keyword
+     */
+    COUNT_BY("countBy"),
+    /**
+     * Exists projection, returning typically a boolean result. It starts with "existsBy" keyword
+     */
+    EXISTS_BY("existsBy"),
     UNKNOWN(""),
     /**
      * Methods from {@link Object}
@@ -47,14 +61,12 @@ public enum RepositoryType {
     /**
      * Method that has {@link Query} annotation
      */
-    JNOSQL_QUERY(""),
-    /**
-     * Method that has the "FindAll" keyword
-     */
-    FIND_ALL("findAll");
+    JNOSQL_QUERY("");
 
-    private static final Predicate<Class<?>> IS_REPOSITORY_METHOD =  Predicate.<Class<?>>isEqual(CrudRepository.class)
+    private static final Predicate<Class<?>> IS_REPOSITORY_METHOD = Predicate.<Class<?>>isEqual(CrudRepository.class)
             .or(Predicate.<Class<?>>isEqual(PageableRepository.class));
+
+    private static final Set<RepositoryType> KEY_WORLD_METHODS = EnumSet.of(FIND_BY, DELETE_BY, COUNT_BY, EXISTS_BY);
     private final String keyword;
 
     RepositoryType(String keyword) {
@@ -82,13 +94,11 @@ public enum RepositoryType {
         }
 
         String methodName = method.getName();
-        if ("findAll".equals(methodName)) {
+        if (FIND_ALL.keyword.equals(methodName)) {
             return FIND_ALL;
-        } else if (methodName.startsWith(FIND_BY.keyword)) {
-            return FIND_BY;
-        } else if (methodName.startsWith(DELETE_BY.keyword)) {
-            return DELETE_BY;
         }
-        return UNKNOWN;
+        return KEY_WORLD_METHODS.stream()
+                .filter(k -> methodName.startsWith(k.keyword))
+                .findFirst().orElse(UNKNOWN);
     }
 }
