ffe16c21485ff78905516c0d0acf60f4e3897c12
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-03 11:07:27 +0000

    decrease the number of events on column
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/ColumnEventPersistManager.java
+++ jnosql-mapping/jnosql-mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/ColumnEventPersistManager.java
@@ -15,25 +15,17 @@
 package org.eclipse.jnosql.mapping.column;
 
 
-import org.eclipse.jnosql.communication.column.ColumnDeleteQuery;
-import org.eclipse.jnosql.communication.column.ColumnEntity;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
-import org.eclipse.jnosql.communication.column.ColumnQuery;
 import org.eclipse.jnosql.mapping.EntityPostPersist;
 import org.eclipse.jnosql.mapping.EntityPrePersist;
 
 /**
  * This interface represents the manager of events. When an entity be either saved or updated an event will be fired.
  * This order going to be:
- * 1) firePreEntity
- * 2) firePreColumnEntity
- * 3) firePreColumn
- * 4) firePostColumn
- * 5) firePostEntity
- * 6) firePostColumnEntity
+ * 1) firePreColumn
+ * 2) firePostColumn
  *
  * @see ColumnWorkflow
  */
@@ -46,20 +38,6 @@ public class ColumnEventPersistManager {
 
     @Inject
     private Event<EntityPostPersist> entityPostPersistEvent;
-
-    @Inject
-    private Event<EntityColumnPrePersist> entityColumnPrePersist;
-
-    @Inject
-    private Event<EntityColumnPostPersist> entityColumnPostPersist;
-
-    @Inject
-    private Event<ColumnQueryExecute> columnQueryExecute;
-
-    @Inject
-    private Event<ColumnDeleteQueryExecute> columnDeleteQueryExecute;
-
-
 
     /**
      * Fire an event once the method is called
@@ -81,21 +59,4 @@ public class ColumnEventPersistManager {
         entityPostPersistEvent.fire(EntityPostPersist.of(entity));
     }
 
-    /**
-     * Fire an event before the query is executed
-     *
-     * @param query the query
-     */
-    public void firePreQuery(ColumnQuery query) {
-        columnQueryExecute.fire(new ColumnQueryExecute(query));
-    }
-
-    /**
-     * Fire an event before the delete query is executed
-     *
-     * @param query the query
-     */
-    public void firePreDeleteQuery(ColumnDeleteQuery query) {
-        columnDeleteQueryExecute.fire(new ColumnDeleteQueryExecute(query));
-    }
 }
