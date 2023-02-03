b8fb7db67fdcf424e034060af791cb1df7fb15c6
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-03 11:23:11 +0000

    remove unecessary events
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/DocumentEventPersistManager.java
+++ jnosql-mapping/jnosql-mapping-document/src/main/java/org/eclipse/jnosql/mapping/document/DocumentEventPersistManager.java
@@ -15,13 +15,10 @@
 package org.eclipse.jnosql.mapping.document;
 
 
-import org.eclipse.jnosql.communication.document.DocumentDeleteQuery;
-import org.eclipse.jnosql.communication.document.DocumentEntity;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
-import org.eclipse.jnosql.communication.document.DocumentQuery;
+import org.eclipse.jnosql.communication.document.DocumentEntity;
 import org.eclipse.jnosql.mapping.EntityPostPersist;
 import org.eclipse.jnosql.mapping.EntityPrePersist;
 
@@ -32,46 +29,11 @@ import org.eclipse.jnosql.mapping.EntityPrePersist;
 public class DocumentEventPersistManager {
 
     @Inject
-    private Event<DocumentEntityPrePersist> documentEntityPrePersistEvent;
-
-    @Inject
-    private Event<DocumentEntityPostPersist> documentEntityPostPersistEvent;
-
-    @Inject
     private Event<EntityPrePersist> entityPrePersistEvent;
 
     @Inject
     private Event<EntityPostPersist> entityPostPersistEvent;
 
-    @Inject
-    private Event<EntityDocumentPrePersist> entityDocumentPrePersist;
-
-    @Inject
-    private Event<EntityDocumentPostPersist> entityDocumentPostPersist;
-
-    @Inject
-    private Event<DocumentQueryExecute> documentQueryExecute;
-
-    @Inject
-    private Event<DocumentDeleteQueryExecute> documentDeleteQueryExecute;
-
-    /**
-     * Fire an event after the conversion of the entity to communication API model.
-     *
-     * @param entity the entity
-     */
-    public void firePreDocument(DocumentEntity entity) {
-        documentEntityPrePersistEvent.fire(new DocumentEntityPrePersist(entity));
-    }
-
-    /**
-     * Fire an event after the response from communication layer
-     *
-     * @param entity the entity
-     */
-    public void firePostDocument(DocumentEntity entity) {
-        documentEntityPostPersistEvent.fire(new DocumentEntityPostPersist(entity));
-    }
 
     /**
      * Fire an event after convert the {@link DocumentEntity},
@@ -95,41 +57,4 @@ public class DocumentEventPersistManager {
         entityPostPersistEvent.fire(EntityPostPersist.of(entity));
     }
 
-    /**
-     * Fire an event after firePreEntity
-     *
-     * @param entity the entity
-     * @param <T>    the entity type
-     */
-    public <T> void firePreDocumentEntity(T entity) {
-        entityDocumentPrePersist.fire(new EntityDocumentPrePersist(entity));
-    }
-
-    /**
-     * Fire an event after firePostEntity
-     *
-     * @param entity the entity
-     * @param <T>    the entity kind
-     */
-    public <T> void firePostDocumentEntity(T entity) {
-        entityDocumentPostPersist.fire(new EntityDocumentPostPersist(entity));
-    }
-
-    /**
-     * Fire an event before the query is executed
-     *
-     * @param query the query
-     */
-    public void firePreQuery(DocumentQuery query) {
-        documentQueryExecute.fire(new DocumentQueryExecute(query));
-    }
-
-    /**
-     * Fire an event before the delete query is executed
-     *
-     * @param query the query
-     */
-    public void firePreDeleteQuery(DocumentDeleteQuery query) {
-        documentDeleteQueryExecute.fire(new DocumentDeleteQueryExecute(query));
-    }
 }
