3dd71f7a2e5b85ed5ee5dfaf6e5d52ab8d11fc9d
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-04 15:29:59 +0000

    fix imports on repository observer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/repository/RepositoryObserverParser.java
+++ jnosql-mapping/jnosql-mapping-core/src/main/java/org/eclipse/jnosql/mapping/repository/RepositoryObserverParser.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.StringJoiner;
 
 public class RepositoryObserverParser {
 
