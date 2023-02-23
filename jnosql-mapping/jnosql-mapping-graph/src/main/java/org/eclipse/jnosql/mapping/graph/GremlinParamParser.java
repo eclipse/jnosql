07032aa2d57c8743b1c05be0fb97105fbb709bd0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-23 08:11:37 +0000

    create javadoc to param parser at gremlin
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GremlinParamParser.java
+++ jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/GremlinParamParser.java
@@ -14,6 +14,14 @@
  */
 package org.eclipse.jnosql.mapping.graph;
 
+/**
+ * This singleton has the goal to interpolate params inside the Gremlin query.
+ * Thus, given the query:
+ * ""g.V().hasLabel(@param)" where the params is {"param":"Otavio"}
+ * It should return the query to: g.V().hasLabel("Otavio")
+ * It should check the Gremlin query options:
+ * https://github.com/apache/tinkerpop/blob/e1396223ea9e1d6240c1f051036cbb5507f47f8d/gremlin-language/src/main/antlr4/Gremlin.g4
+ */
 enum GremlinParamParser {
     INSTANCE;
 }
