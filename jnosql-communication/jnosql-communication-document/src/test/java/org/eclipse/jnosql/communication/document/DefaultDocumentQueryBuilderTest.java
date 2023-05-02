2e9e6e097829d1b256bc1800615540f2893a4f22
Otavio Santana <otaviopolianasantana@gmail.com>
2023-05-02 17:29:34 +0100

    style: remove unsed imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-document/src/test/java/org/eclipse/jnosql/communication/document/DefaultDocumentQueryBuilderTest.java
+++ jnosql-communication/jnosql-communication-document/src/test/java/org/eclipse/jnosql/communication/document/DefaultDocumentQueryBuilderTest.java
@@ -25,13 +25,11 @@ import java.util.UUID;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
