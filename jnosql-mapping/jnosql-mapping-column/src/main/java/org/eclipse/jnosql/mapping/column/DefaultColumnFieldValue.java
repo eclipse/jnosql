3ea761d5ced15f74e21cf5a819266bc2b9dfa013
Otavio Santana <otaviopolianasantana@gmail.com>
2023-07-31 07:01:51 +0100

    refac: move default fielddata as api
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/DefaultColumnFieldValue.java
+++ jnosql-mapping/jnosql-mapping-column/src/main/java/org/eclipse/jnosql/mapping/column/DefaultColumnFieldValue.java
@@ -20,7 +20,7 @@ import org.eclipse.jnosql.mapping.Converters;
 import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
 import org.eclipse.jnosql.mapping.metadata.MappingType;
 import org.eclipse.jnosql.mapping.metadata.FieldValue;
-import org.eclipse.jnosql.mapping.reflection.DefaultFieldValue;
+import org.eclipse.jnosql.mapping.metadata.DefaultFieldValue;
 import org.eclipse.jnosql.mapping.reflection.GenericFieldMetadata;
 
 import java.util.ArrayList;
