5eac3d81ec11f9e7f4a5a35469d4667c3145e513
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-09 05:08:00 +0100

    test: create EmbeddeFieldMetadata
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/EmbeddedFieldMetadataTest.java
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.reflection;
+
+import org.assertj.core.api.Assertions;
+import org.eclipse.jnosql.mapping.metadata.ClassConverter;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.metadata.FieldMetadata;
+import org.eclipse.jnosql.mapping.test.entities.Worker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmbeddedFieldMetadataTest {
+
+    private EmbeddedFieldMetadata metadata;
+
+    @BeforeEach
+    public void setUp() {
+        ClassConverter converter = new ReflectionClassConverter();
+        EntityMetadata entity = converter.apply(Worker.class);
+        metadata = (EmbeddedFieldMetadata) entity.fieldsGroupByName().get("job");
+    }
+
+    @Test
+    public void shouldId(){
+        Assertions.assertThat(metadata.isId()).isFalse();
+    }
+
+    @Test
+    public void shouldToString(){
+        Assertions.assertThat(metadata.toString()).isNotEmpty();
+    }
+
+    @Test
+    public void shouldEqualsHasCode(){
+        Assertions.assertThat(metadata).isEqualTo(metadata);
+        Assertions.assertThat(metadata.hashCode()).isEqualTo(metadata.hashCode());
+    }
+
+
+}
--- jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/FieldMetadataTest.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/reflection/FieldMetadataTest.java
@@ -40,7 +40,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FieldMetadataTest {
 
 
-    @Inject
     private ClassConverter converter;
 
 
