79c635bbc1d50597bac4add452264d69af8518c0
Otavio Santana <otaviopolianasantana@gmail.com>
2023-08-29 16:23:41 +0100

    docs: create keyvalue atabase documentation
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-key-value/src/main/java/org/eclipse/jnosql/mapping/keyvalue/KeyValueDatabase.java
+++ jnosql-mapping/jnosql-mapping-key-value/src/main/java/org/eclipse/jnosql/mapping/keyvalue/KeyValueDatabase.java
@@ -22,10 +22,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * This annotation is used to generate List, Set, Queue, and Map instances using a key-value database
+ * obtained from a BucketManagerFactory. The annotation specifies the bucket name for the database.
+ * An example demonstrating injection of various data structures using the {@link KeyValueDatabase} annotation.
+ *
+ * <p>Usage:</p>
+ * <pre>{@code
+ * // Inject a List<String> instance from the "names" bucket in the key-value database.
+ * @Inject
+ * @KeyValueDatabase("names")
+ * private List<String> names;
+ *
+ * // Inject a Set<String> instance from the "fruits" bucket in the key-value database.
+ * @Inject
+ * @KeyValueDatabase("fruits")
+ * private Set<String> fruits;
+ *
+ * // Inject a Queue<String> instance from the "orders" bucket in the key-value database.
+ * @Inject
+ * @KeyValueDatabase("orders")
+ * private Queue<String> orders;
+ *
+ * // Inject a Map<String, String> instance from the "orders" bucket in the key-value database.
+ * @Inject
+ * @KeyValueDatabase("orders")
+ * private Map<String, String> map;
+ * }</pre>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.METHOD})
 @Qualifier
 public @interface KeyValueDatabase {
 
+    /**
+     * Specifies the name of the bucket in the key-value database that will be used for data storage.
+     *
+     * @return The name of the bucket.
+     */
     @Nonbinding String value();
 }
