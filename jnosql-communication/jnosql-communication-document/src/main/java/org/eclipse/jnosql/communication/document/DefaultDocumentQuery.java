dc64486b2ba60c16f64601a354460fe77a12399e
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-11 07:59:58 +0000

    refactor: update default document query to record
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-document/src/main/java/org/eclipse/jnosql/communication/document/DefaultDocumentQuery.java
+++ jnosql-communication/jnosql-communication-document/src/main/java/org/eclipse/jnosql/communication/document/DefaultDocumentQuery.java
@@ -21,55 +21,19 @@ import jakarta.data.repository.Sort;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 import static java.util.Collections.unmodifiableList;
 import static java.util.Optional.ofNullable;
 
-class DefaultDocumentQuery implements DocumentQuery {
+record DefaultDocumentQuery(long limit, long skip, String name,
+                            List<String> documents, List<Sort> sorts, DocumentCondition documentCondition) implements DocumentQuery {
 
-    private final long limit;
 
-    private final long skip;
-
-    private final String documentCollection;
-
-    private final DocumentCondition condition;
-
-    private final List<Sort> sorts;
-
-    private final List<String> documents;
-
-    DefaultDocumentQuery(long limit, long skip, String documentCollection,
-                         List<String> documents, List<Sort> sorts, DocumentCondition condition) {
-
-        this.limit = limit;
-        this.skip = skip;
-        this.documentCollection = documentCollection;
-        this.condition = ofNullable(condition).map(DocumentCondition::readOnly).orElse(null);
-        this.sorts = sorts;
-        this.documents = documents;
-    }
-
-    @Override
-    public long limit() {
-        return limit;
-    }
-
-    @Override
-    public long skip() {
-        return skip;
-    }
-
-    @Override
-    public String name() {
-        return documentCollection;
-    }
 
     @Override
     public Optional<DocumentCondition> condition() {
-        return ofNullable(condition);
+        return ofNullable(documentCondition).map(DocumentCondition::readOnly);
     }
 
     @Override
@@ -80,38 +44,6 @@ class DefaultDocumentQuery implements DocumentQuery {
     @Override
     public List<String> documents() {
         return unmodifiableList(documents);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof DocumentQuery that)) {
-            return false;
-        }
-        return limit == that.limit() &&
-                skip == that.skip() &&
-                Objects.equals(documentCollection, that.name()) &&
-                Objects.equals(condition, that.condition().orElse(null)) &&
-                Objects.equals(sorts, that.sorts()) &&
-                Objects.equals(documents, that.documents());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(limit, skip, documentCollection, condition, sorts, documents);
-    }
-
-    @Override
-    public String toString() {
-        return "DocumentQuery{" + "maxResult=" + limit +
-                ", firstResult=" + skip +
-                ", documentCollection='" + documentCollection + '\'' +
-                ", condition=" + condition +
-                ", sorts=" + sorts +
-                ", documents=" + documents +
-                '}';
     }
 
     static DocumentQuery countBy(DocumentQuery query) {
