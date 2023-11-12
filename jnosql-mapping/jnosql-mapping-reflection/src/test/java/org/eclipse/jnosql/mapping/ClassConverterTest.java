fb64beb9d1482f9626c330e465527bdf8dd1f75e
Maximillian Arruda <dearrudam@gmail.com>
2023-11-12 04:19:02 -0300

    refactor: remove public modifier at test class and its methods
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/ClassConverterTest.java
+++ jnosql-mapping/jnosql-mapping-reflection/src/test/java/org/eclipse/jnosql/mapping/ClassConverterTest.java
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 class ClassConverterTest {
 
     @Test
-    public void shouldCreateClassConverter() {
+    void shouldCreateClassConverter() {
         ClassConverter converter = ClassConverter.load();
         assertNotNull(converter);
     }
