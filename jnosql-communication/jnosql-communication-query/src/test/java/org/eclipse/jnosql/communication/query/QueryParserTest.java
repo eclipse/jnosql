3a12928f3b5f371097f06d56dbd16783b5878a96
Maximillian Arruda <dearrudam@gmail.com>
2023-11-12 04:35:39 -0300

    refactor: remove public modifier at test class and its methods
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/QueryParserTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/QueryParserTest.java
@@ -25,24 +25,23 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-
-public class QueryParserTest {
+class QueryParserTest {
 
 
     @ParameterizedTest
     @ArgumentsSource(DeleteQueryArgumentProvider.class)
-    public void shouldExecuteQuery(String query) {
+    void shouldExecuteQuery(String query) {
         testQuery(query);
     }
 
     @Test
-    public void shouldIgnoreComments() {
+    void shouldIgnoreComments() {
         testQuery("//ignore this line \n delete from Person");
     }
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ArgumentsSource(WrongDeleteQueryArgumentProvider.class)
-    public void shouldNotExecute(String query) {
+    void shouldNotExecute(String query) {
         Assertions.assertThrows(QueryException.class, () -> testQuery(query));
     }
 
