f25ee7bce55390f6e42c476d7dad7230d0fe7a1b
Otavio Santana <otaviopolianasantana@gmail.com>
2023-12-06 16:32:43 +0000

    test: create entity
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/entities/WorkflowStep.java
@@ -0,0 +1,56 @@
+/*
+ *   Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *    All rights reserved. This program and the accompanying materials
+ *    are made available under the terms of the Eclipse Public License v1.0
+ *    and Apache License v2.0 which accompanies this distribution.
+ *    The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *    and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *    You may elect to redistribute this code under either of these licenses.
+ *
+ *    Contributors:
+ *
+ *    Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.document.entities;
+
+import jakarta.nosql.Column;
+import jakarta.nosql.Entity;
+import jakarta.nosql.Id;
+
+@Entity("workflow_step")
+public class WorkflowStep {
+
+    @Id
+    private String id;
+
+    @Column("_key")
+    private String key;
+
+    @Column
+    private String workflowSchemaKey;
+
+    @Column
+    private String stepName;
+
+    @Column
+    private MainStepType mainStepType;
+
+    @Column
+    private Integer stepNo;
+
+
+    @Column
+    @Nullable
+    @Index
+    private String componentConfigurationKey;
+
+    @Column
+    @Nullable
+    @Index
+    private String relationTypeKey;
+
+    @Column
+    @Nullable
+    private List<Transition> availableTransitions;
+}
