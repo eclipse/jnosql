97c64bc2f88cb0147e4d28350e244b624fabf54e
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-04 18:20:36 +0000

    style: organize imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-column/src/test/java/org/eclipse/jnosql/mapping/column/query/ColumnParameterBasedQueryTest.java
+++ jnosql-mapping/jnosql-mapping-column/src/test/java/org/eclipse/jnosql/mapping/column/query/ColumnParameterBasedQueryTest.java
@@ -42,7 +42,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @EnableAutoWeld
 @AddPackages(value = {Converters.class, ColumnEntityConverter.class})
