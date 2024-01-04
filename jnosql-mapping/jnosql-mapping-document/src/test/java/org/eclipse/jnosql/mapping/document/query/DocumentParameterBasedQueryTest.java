4782a52ea37866e7c1156ad409813a01153cdc1e
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-04 18:23:31 +0000

    test: create document paramter query based
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/query/DocumentParameterBasedQueryTest.java
+++ jnosql-mapping/jnosql-mapping-document/src/test/java/org/eclipse/jnosql/mapping/document/query/DocumentParameterBasedQueryTest.java
@@ -14,15 +14,33 @@
  */
 package org.eclipse.jnosql.mapping.document.query;
 
+import jakarta.data.Sort;
+import jakarta.data.page.Pageable;
+import jakarta.inject.Inject;
+import org.assertj.core.api.SoftAssertions;
+import org.eclipse.jnosql.communication.Condition;
+import org.eclipse.jnosql.communication.TypeReference;
+import org.eclipse.jnosql.communication.document.Document;
+import org.eclipse.jnosql.communication.document.DocumentCondition;
+import org.eclipse.jnosql.communication.document.DocumentQuery;
 import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
 import org.eclipse.jnosql.mapping.document.DocumentEntityConverter;
 import org.eclipse.jnosql.mapping.document.MockProducer;
+import org.eclipse.jnosql.mapping.document.entities.Person;
 import org.eclipse.jnosql.mapping.document.spi.DocumentExtension;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
+import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
 import org.eclipse.jnosql.mapping.reflection.Reflections;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -33,4 +51,78 @@ import static org.junit.jupiter.api.Assertions.*;
 @AddExtensions({EntityMetadataExtension.class, DocumentExtension.class})
 class DocumentParameterBasedQueryTest {
 
+
+    @Inject
+    private EntitiesMetadata entitiesMetadata;
+
+    private EntityMetadata metadata;
+    @BeforeEach
+    void setUp(){
+        this.metadata = entitiesMetadata.get(Person.class);
+    }
+
+    @Test
+    void shouldCreateQuerySingleParameter(){
+        Map<String, Object> params = Map.of("name", "Ada");
+        var query = DocumentParameterBasedQuery.INSTANCE.toQuery(params, null, metadata);
+
+        SoftAssertions.assertSoftly(soft ->{
+            soft.assertThat(query.limit()).isEqualTo(0L);
+            soft.assertThat(query.skip()).isEqualTo(0L);
+            soft.assertThat(query.name()).isEqualTo("Person");
+            soft.assertThat(query.sorts()).isEmpty();
+            soft.assertThat(query.condition()).isNotEmpty();
+            soft.assertThat(query.condition()).get().isEqualTo(DocumentCondition.eq(Document.of("name", "Ada")));
+        });
+    }
+
+    @Test
+    void shouldCreateQueryMultipleParams(){
+        Map<String, Object> params = Map.of("name", "Ada", "age", 10);
+        var query = DocumentParameterBasedQuery.INSTANCE.toQuery(params, null, metadata);
+
+        SoftAssertions.assertSoftly(soft ->{
+            soft.assertThat(query.limit()).isEqualTo(0L);
+            soft.assertThat(query.skip()).isEqualTo(0L);
+            soft.assertThat(query.name()).isEqualTo("Person");
+            soft.assertThat(query.sorts()).isEmpty();
+            soft.assertThat(query.condition()).isNotEmpty();
+            var condition = query.condition().orElseThrow();
+            soft.assertThat(condition.condition()).isEqualTo(Condition.AND);
+            soft.assertThat(condition.document().get(new TypeReference<List<DocumentCondition>>() {
+            })).contains(DocumentCondition.eq(Document.of("name", "Ada")),
+                    DocumentCondition.eq(Document.of("age", 10)));
+        });
+
+    }
+
+    @Test
+    void shouldCreateQueryEmptyParams(){
+        Map<String, Object> params = Collections.emptyMap();
+        var query = DocumentParameterBasedQuery.INSTANCE.toQuery(params, null, metadata);
+
+        SoftAssertions.assertSoftly(soft ->{
+            soft.assertThat(query.limit()).isEqualTo(0L);
+            soft.assertThat(query.skip()).isEqualTo(0L);
+            soft.assertThat(query.name()).isEqualTo("Person");
+            soft.assertThat(query.sorts()).isEmpty();
+            soft.assertThat(query.condition()).isEmpty();
+        });
+    }
+
+    @Test
+    void shouldCreateQueryPageable(){
+        Map<String, Object> params = Map.of("name", "Ada");
+        var pageable = Pageable.ofPage(2).size(10).sortBy(Sort.asc("name"));
+        var query = DocumentParameterBasedQuery.INSTANCE.toQuery(params, pageable, metadata);
+
+        SoftAssertions.assertSoftly(soft ->{
+            soft.assertThat(query.name()).isEqualTo("Person");
+            soft.assertThat(query.condition()).isNotEmpty();
+            soft.assertThat(query.condition()).get().isEqualTo(DocumentCondition.eq(Document.of("name", "Ada")));
+            soft.assertThat(query.limit()).isEqualTo(10L);
+            soft.assertThat(query.skip()).isEqualTo(10L);
+            soft.assertThat(query.sorts()).hasSize(1).contains(Sort.asc("name"));
+        });
+    }
 }
