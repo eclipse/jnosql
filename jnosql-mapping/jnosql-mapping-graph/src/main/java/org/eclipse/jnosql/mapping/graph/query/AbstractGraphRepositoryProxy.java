136a36f08c6b818defe07eb12c223efbaeda8079
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-05 04:53:36 +0000

    feat: update at abstracatgraph repository var
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/query/AbstractGraphRepositoryProxy.java
+++ jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/mapping/graph/query/AbstractGraphRepositoryProxy.java
@@ -98,7 +98,7 @@ abstract class AbstractGraphRepositoryProxy<T, K> extends AbstractRepositoryProx
     protected Object executeParameterBased(Object instance, Method method, Object[] params) {
         Class<?> type = entityMetadata().type();
         Map<String, Object> parameters = RepositoryReflectionUtils.INSTANCE.getBy(method, params);
-        String methodName = "findBy" + parameters.keySet().stream()
+        var methodName = "findBy" + parameters.keySet().stream()
                 .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
                 .collect(Collectors.joining("And"));
         Supplier<Stream<?>> querySupplier = () -> {
