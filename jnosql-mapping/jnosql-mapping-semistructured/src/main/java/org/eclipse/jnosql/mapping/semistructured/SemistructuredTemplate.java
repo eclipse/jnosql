60f5cb4628986758fb8b36f83abe8d0949f6f8e0
Otavio Santana <otaviopolianasantana@gmail.com>
2024-03-02 09:31:41 +0000

    docs: update documentation to SemistructuredTemplate
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/SemistructuredTemplate.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/main/java/org/eclipse/jnosql/mapping/semistructured/SemistructuredTemplate.java
@@ -12,6 +12,8 @@ package org.eclipse.jnosql.mapping.semistructured;
 
 import jakarta.nosql.PreparedStatement;
 import jakarta.nosql.Template;
+import org.eclipse.jnosql.communication.column.ColumnDeleteQuery;
+import org.eclipse.jnosql.communication.column.ColumnQuery;
 
 import java.util.Optional;
 import java.util.stream.Stream;
@@ -91,4 +93,65 @@ public interface SemistructuredTemplate extends Template {
      * @throws UnsupportedOperationException if the provider does not support query by text
      */
     PreparedStatement prepare(String query);
+
+    /**
+     * Deletes an entity
+     *
+     * @param query query to delete an entity
+     * @throws NullPointerException when query is null
+     */
+    void delete(ColumnDeleteQuery query);
+
+    /**
+     * Finds entities from query
+     *
+     * @param query - query to figure out entities
+     * @param <T>   the instance type
+     * @return entities found by query
+     * @throws NullPointerException when query is null
+     */
+    <T> Stream<T> select(ColumnQuery query);
+
+    /**
+     * Returns the number of items in the column family that match a specified query.
+     * @param query the query
+     * @return the number of documents from query
+     * @throws NullPointerException when query is null
+     */
+    long count(ColumnQuery query);
+
+    /**
+     * Returns whether an entity that match a specified query.
+     * @param query the query
+     * @return true if an entity with the given query exists, false otherwise.
+     * @throws NullPointerException when query it null
+     */
+    boolean exists(ColumnQuery query);
+
+    /**
+     * Returns a single entity from query
+     *
+     * @param query - query to figure out entities
+     * @param <T>   the instance type
+     * @return an entity on {@link Optional} or {@link Optional#empty()} when the result is not found.
+     * @throws NullPointerException     when query is null
+     */
+    <T> Optional<T> singleResult(ColumnQuery query);
+
+    /**
+     * Returns all entities on the database
+     * @param type the entity type filter
+     * @return the {@link Stream}
+     * @param <T> the entity type
+     * @throws NullPointerException when type is null
+     */
+    <T> Stream<T> findAll(Class<T> type);
+
+    /**
+     * delete all entities from the database
+     * @param type the entity type filter
+     * @param <T> the entity type
+     * @throws NullPointerException when type is null
+     */
+    <T> void deleteAll(Class<T> type);
 }
