17da25ff33c06f2000bc262c4300f48d6b801986
Otavio Santana <otaviopolianasantana@gmail.com>
2024-03-02 05:14:24 +0000

    docs: enhance database manager factory
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/DatabaseManagerFactory.java
+++ jnosql-communication/jnosql-communication-semistructured/src/main/java/org/eclipse/jnosql/communication/semistructured/DatabaseManagerFactory.java
@@ -19,8 +19,18 @@ package org.eclipse.jnosql.communication.semistructured;
 import java.util.function.Function;
 
 /**
- * The manager factory instance creates a manager instance from the database name.
- * It should return a {@link NullPointerException} when the {@link String} parameter is null.
+ * This interface represents a factory for creating database manager instances based on a database name.
+ * It should throw a {@link NullPointerException} when the {@link String} parameter is null.
+ *
+ * <p>The {@link DatabaseManagerFactory} extends {@link java.util.function.Function}, indicating that it can be
+ * used as a function to create database manager instances from database names.</p>
+ *
+ * <p>Implementations of this interface are expected to provide the necessary logic to instantiate appropriate
+ * {@link DatabaseManager} instances based on the provided database name.</p>
+ *
+ * <p>Additionally, this interface extends {@link AutoCloseable}, indicating that implementations may manage
+ * resources that require cleanup. The {@link #close()} method should be called to release any resources
+ * held by the factory.</p>
  *
  * @see DatabaseManager
  */
@@ -28,7 +38,9 @@ public interface DatabaseManagerFactory extends Function<String, DatabaseManager
 
 
     /**
-     * closes a resource
+     * Closes any resources held by the factory.
+     *
+     * <p>Note: Some databases may not perform any specific actions upon closing.</p>
      */
     void close();
 }
