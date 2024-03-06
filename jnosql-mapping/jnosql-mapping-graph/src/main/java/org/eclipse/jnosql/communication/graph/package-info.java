9ed30bc51f0c4d54526c6c2786b0ddce54b4a84e
Otavio Santana <otaviopolianasantana@gmail.com>
2024-03-06 07:03:13 +0000

    docs: create documentation to communication graph
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/communication/graph/package-info.java
+++ jnosql-mapping/jnosql-mapping-graph/src/main/java/org/eclipse/jnosql/communication/graph/package-info.java
@@ -12,4 +12,20 @@
  *
  *   Otavio Santana
  */
+
+/**
+ * This package provides communication facilities and abstractions for interacting with graph databases
+ * in the JNoSQL project. It contains interfaces, classes, and utilities that enable developers to
+ * communicate with and manage graph databases.
+ * <p>
+ * The core interface in this package is {@link org.eclipse.jnosql.communication.graph.GraphDatabaseManager},
+ * which extends {@link org.eclipse.jnosql.communication.semistructured.DatabaseManager}
+ * and acts as a specialized extension for managing graph databases. Implementations of this interface
+ * provide methods for interacting with the underlying graph database, executing graph traversals,
+ * and performing other graph-related operations.
+ * <p>
+ * Additionally, this package may contain specific implementations, adapters, or utilities tailored
+ * for working with different graph database technologies supported by JNoSQL.
+ * </p>
+ */
 package org.eclipse.jnosql.communication.graph;
