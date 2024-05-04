365e914871273b06e9065b05bd7da6bfe8b60b87
Otavio Santana <otaviopolianasantana@gmail.com>
2024-05-04 21:54:22 +0100

    feat: create delete jdql
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-communication/jnosql-communication-query/src/main/java/org/eclipse/jnosql/communication/query/data/DeleteJDQL.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  and Apache License v2.0 which accompanies this distribution.
+ *  The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *  and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *  You may elect to redistribute this code under either of these licenses.
+ *  Contributors:
+ *  Otavio Santana
+ */
+package org.eclipse.jnosql.communication.query.data;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.eclipse.jnosql.communication.query.DeleteQuery;
+import org.eclipse.jnosql.query.grammar.data.JDQLParser;
+
+import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class DeleteJDQL extends AbstractWhere implements BiFunction<String, String, DeleteQuery> {
+
+    @Override
+    public DeleteQuery apply(String query, String entity) {
+        Objects.requireNonNull(query, " query is required");
+        this.entity = entity;
+        runQuery(query);
+        if(this.entity == null) {
+            throw new IllegalArgumentException("The entity is required in the query");
+        }
+        return DeleteQuery.of(entity, where);
+    }
+
+    @Override
+    Function<JDQLParser, ParseTree> getParserTree() {
+        return JDQLParser::delete_statement;
+    }
+}
