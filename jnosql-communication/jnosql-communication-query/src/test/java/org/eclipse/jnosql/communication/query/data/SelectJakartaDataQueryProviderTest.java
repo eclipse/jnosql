a95f9a654c98f96b251ce2a6ac478755d75f76d2
Otavio Santana <otaviopolianasantana@gmail.com>
2024-07-26 17:16:40 +0100

    test: create scenario to jakarta data query provider
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/SelectJakartaDataQueryProviderTest.java
+++ jnosql-communication/jnosql-communication-query/src/test/java/org/eclipse/jnosql/communication/query/data/SelectJakartaDataQueryProviderTest.java
@@ -80,7 +80,7 @@ class SelectJakartaDataQueryProviderTest {
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"", " "})
     void shouldReturnErrorWhenEntityIsMissing(String query) {
-        Assertions.assertThrows(IllegalArgumentException.class, () ->{
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
             selectProvider.apply(query, null);
         });
     }
@@ -88,7 +88,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"FROM entity ORDER BY name ASC", "ORDER BY name ASC"})
-    void shouldQueryOrder(String query){
+    void shouldQueryOrder(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -101,7 +101,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"FROM entity ORDER BY name DESC", "ORDER BY name DESC"})
-    void shouldQueryOrderDesc(String query){
+    void shouldQueryOrderDesc(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -114,7 +114,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"FROM entity ORDER BY name ASC, age DESC", "ORDER BY name ASC, age DESC"})
-    void shouldQueryOrders(String query){
+    void shouldQueryOrders(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -127,7 +127,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"SELECT name, age FROM entity", "SELECT name, age"})
-    void shouldSelectFields(String query){
+    void shouldSelectFields(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -141,7 +141,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE age = 10", "FROM entity WHERE age = 10"})
-    void shouldEq(String query){
+    void shouldEq(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -159,7 +159,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE salary = 10.15", "FROM entity WHERE salary = 10.15"})
-    void shouldEqDouble(String query){
+    void shouldEqDouble(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -177,7 +177,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE name = \"Otavio\"", "FROM entity WHERE name = \"Otavio\""})
-    void shouldEqString(String query){
+    void shouldEqString(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -195,7 +195,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE name = 'Otavio'", "FROM entity WHERE name = 'Otavio'"})
-    void shouldEqStringSingleQuote(String query){
+    void shouldEqStringSingleQuote(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -213,7 +213,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE name = :name", "FROM entity WHERE name = :name"})
-    void shouldEQQueryWithCondition(String query){
+    void shouldEQQueryWithCondition(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -231,7 +231,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE name = ?1", "FROM entity WHERE name = ?1"})
-    void shouldEQQueryWithConditionPosition(String query){
+    void shouldEQQueryWithConditionPosition(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -249,7 +249,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE active = TRUE", "FROM entity WHERE active = TRUE"})
-    void shouldUseSpecialExpressionTrue(String query){
+    void shouldUseSpecialExpressionTrue(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -267,7 +267,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE active = FALSE", "FROM entity WHERE active = FALSE"})
-    void shouldUseSpecialExpressionFalse(String query){
+    void shouldUseSpecialExpressionFalse(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -285,7 +285,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE age < 10", "FROM entity WHERE age < 10"})
-    void shouldUseSpecialExpressionLesser(String query){
+    void shouldUseSpecialExpressionLesser(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -303,7 +303,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE age > 10", "FROM entity WHERE age > 10"})
-    void shouldUseSpecialExpressionGreater(String query){
+    void shouldUseSpecialExpressionGreater(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -321,7 +321,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE age <= 10", "FROM entity WHERE age <= 10"})
-    void shouldUseSpecialExpressionLesserThanEquals(String query){
+    void shouldUseSpecialExpressionLesserThanEquals(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -339,7 +339,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE age >= 10", "FROM entity WHERE age >= 10"})
-    void shouldUseSpecialExpressionGreaterThanEquals(String query){
+    void shouldUseSpecialExpressionGreaterThanEquals(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -358,7 +358,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = {"WHERE days = java.time.DayOfWeek.MONDAY", "FROM entity WHERE days = java.time.DayOfWeek.MONDAY"})
-    void shouldEqUsingEnumLiteral(String query){
+    void shouldEqUsingEnumLiteral(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -376,10 +376,9 @@ class SelectJakartaDataQueryProviderTest {
     }
 
 
-
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = "SELECT COUNT (THIS) WHERE age = 10")
-    void shouldAggregate(String query){
+    void shouldAggregate(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -398,7 +397,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = "SELECT hexadecimal WHERE hexadecimal IS NULL")
-    void shouldQueryIsNull(String query){
+    void shouldQueryIsNull(String query) {
         SelectQuery selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -417,7 +416,7 @@ class SelectJakartaDataQueryProviderTest {
 
     @ParameterizedTest(name = "Should parser the query {0}")
     @ValueSource(strings = "SELECT hexadecimal WHERE hexadecimal IS NOT NULL")
-    void shouldQueryIsNotNull(String query){
+    void shouldQueryIsNotNull(String query) {
         var selectQuery = selectProvider.apply(query, "entity");
 
         SoftAssertions.assertSoftly(soft -> {
@@ -432,6 +431,32 @@ class SelectJakartaDataQueryProviderTest {
             var queryCondition = notCondition.get().get(0);
             soft.assertThat(queryCondition.name()).isEqualTo("hexadecimal");
             soft.assertThat(queryCondition.value()).isEqualTo(NullQueryValue.INSTANCE);
+            soft.assertThat(selectQuery.isCount()).isFalse();
+        });
+    }
+
+    @ParameterizedTest(name = "Should parser the query {0}")
+    @ValueSource(strings = "WHERE isOdd = false AND numType = java.time.DayOfWeek.MONDAY")
+    void shouldQueryConditionEnum(String query) {
+        var selectQuery = selectProvider.apply(query, "entity");
+
+        SoftAssertions.assertSoftly(soft -> {
+            soft.assertThat(selectQuery.fields()).isEmpty();
+            soft.assertThat(selectQuery.entity()).isEqualTo("entity");
+            soft.assertThat(selectQuery.orderBy()).isEmpty();
+            soft.assertThat(selectQuery.where()).isNotEmpty();
+            var where = selectQuery.where().orElseThrow();
+            var condition = where.condition();
+            soft.assertThat(condition.condition()).isEqualTo(Condition.AND);
+            var andCondition = (ConditionQueryValue) condition.value();
+            var first = andCondition.get().get(0);
+            var second = andCondition.get().get(1);
+            soft.assertThat(first.condition()).isEqualTo(Condition.EQUALS);
+            soft.assertThat(first.name()).isEqualTo("isOdd");
+            soft.assertThat(first.value()).isEqualTo(BooleanQueryValue.FALSE);
+            soft.assertThat(second.condition()).isEqualTo(Condition.EQUALS);
+            soft.assertThat(second.name()).isEqualTo("numType");
+            soft.assertThat(second.value().get()).isEqualTo(DayOfWeek.MONDAY);
             soft.assertThat(selectQuery.isCount()).isFalse();
         });
     }
