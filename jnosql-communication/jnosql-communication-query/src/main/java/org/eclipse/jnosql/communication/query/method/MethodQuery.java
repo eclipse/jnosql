e4ff64ac104adde1a000191f5b4ac249641cd8b2
Otavio Santana <otaviopolianasantana@gmail.com>
2024-07-28 08:48:37 +0100

    refactoring: update methodquery
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-communication/jnosql-communication-query/src/main/java/org/eclipse/jnosql/communication/query/method/MethodQuery.java
+++ jnosql-communication/jnosql-communication-query/src/main/java/org/eclipse/jnosql/communication/query/method/MethodQuery.java
@@ -18,17 +18,32 @@ import java.util.WeakHashMap;
 import java.util.function.Supplier;
 import java.util.regex.Pattern;
 
+/**
+ * This class represents a method query tokenizer. It processes query strings,
+ * breaking them into tokens based on predefined patterns and applying specific
+ * logic to handle the "First" keyword based on its position.
+ * The class also caches processed query strings to optimize repeated queries.
+ * It implements the Supplier interface to provide the processed query string
+ * when needed.
+ */
 public final class MethodQuery implements Supplier<String> {
 
+    /**
+     * The limit to the first keyword, after this limit the first keyword will be ignored
+     */
+    private static final int LIMIT_FIRST = 10;
+
     private final String value;
-    private static final Pattern PATTERN = Pattern.compile(
+
+    private static final Pattern TOKENIZER_PATTERN = Pattern.compile(
             "findBy|deleteBy|countAll|countBy|existsBy|OrderBy|"
-                    + "First(?=\\d*By)|First(?=By)|(?<=First\\d{1,})By|(?<=First)By|(?<!First)By|IgnoreCase|"
+                    + "First(?=\\d*By)|First(?=By\\b)|(?<=First\\d{1,})By|(?<=First)By(?![a-zA-Z])|(?<!First)By(?![a-zA-Z])|IgnoreCase|"
                     + "And|Or(?!der)|Null|Not|Equals|GreaterThanEqual|True|False|Contains|EndsWith|StartsWith|"
                     + "LessThanEqual|GreaterThan|LessThan|Between|In|Like|Asc|Desc"
     );
 
     private static final Map<String, String> CACHE = Collections.synchronizedMap(new WeakHashMap<>());
+
     private MethodQuery(String value) {
         this.value = value;
     }
@@ -62,11 +77,23 @@ public final class MethodQuery implements Supplier<String> {
 
     public static MethodQuery of(String query) {
         Objects.requireNonNull(query, "query is required");
-        String value = CACHE.get(query);
-        if (Objects.isNull(value)) {
-            value = PATTERN.matcher(query).replaceAll(" $0 ").trim().replaceAll("\\s+", " ");
-            CACHE.put(query, value);
+        return new MethodQuery(CACHE.computeIfAbsent(query, q -> {
+            String tokenized = TOKENIZER_PATTERN.matcher(q).replaceAll(" $0 ").trim().replaceAll("\\s+", " ");
+            return adjustFirstKeywordPosition(tokenized);
+        }));
+    }
+
+    private static String adjustFirstKeywordPosition(String query) {
+        StringBuilder result = new StringBuilder();
+        int currentPosition = 0;
+        for (String token : query.split(" ")) {
+            if (currentPosition >= LIMIT_FIRST && token.equals("First")) {
+                result.append(token);
+            } else {
+                result.append(" ").append(token);
+            }
+            currentPosition += token.length();
         }
-        return new MethodQuery(value);
+        return result.toString().trim();
     }
 }
