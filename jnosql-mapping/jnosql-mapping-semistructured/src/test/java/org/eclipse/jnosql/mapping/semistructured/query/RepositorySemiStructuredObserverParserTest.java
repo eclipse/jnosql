2761673bbb90c1bd34a7a610a2e0f8101e0bab14
Otavio Santana <otaviopolianasantana@gmail.com>
2024-08-06 18:30:13 +0100

    test: enhnace repository semistructure observer
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/RepositorySemiStructuredObserverParserTest.java
+++ jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/query/RepositorySemiStructuredObserverParserTest.java
@@ -14,18 +14,71 @@
  */
 package org.eclipse.jnosql.mapping.semistructured.query;
 
+import jakarta.inject.Inject;
+import org.eclipse.jnosql.mapping.core.Converters;
+import org.eclipse.jnosql.mapping.core.spi.EntityMetadataExtension;
+import org.eclipse.jnosql.mapping.metadata.EntitiesMetadata;
 import org.eclipse.jnosql.mapping.metadata.EntityMetadata;
+import org.eclipse.jnosql.mapping.reflection.Reflections;
+import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
+import org.eclipse.jnosql.mapping.semistructured.MockProducer;
+import org.eclipse.jnosql.mapping.semistructured.entities.Person;
+import org.jboss.weld.junit5.auto.AddExtensions;
+import org.jboss.weld.junit5.auto.AddPackages;
+import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@EnableAutoWeld
+@AddPackages(value = {Converters.class, EntityConverter.class})
+@AddPackages(MockProducer.class)
+@AddPackages(Reflections.class)
+@AddExtensions({EntityMetadataExtension.class})
 class RepositorySemiStructuredObserverParserTest {
+
+    @Inject
+    private EntitiesMetadata entitiesMetadata;
+
+    private RepositorySemiStructuredObserverParser parser;
+
+    @BeforeEach
+    void setUp() {
+        EntityMetadata entityMetadata = entitiesMetadata.get(Person.class);
+        this.parser = (RepositorySemiStructuredObserverParser) RepositorySemiStructuredObserverParser.of(entityMetadata);
+    }
 
     @Test
     void shouldCreateFromRepository() {
         EntityMetadata entityMetadata = Mockito.mock(EntityMetadata.class);
         var parser = RepositorySemiStructuredObserverParser.of(entityMetadata);
         assertNotNull(parser);
+    }
+
+    @Test
+    void shouldFireEntity() {
+        String entity = "entity";
+        assertEquals("Person", parser.fireEntity(entity));
+    }
+
+    @Test
+    void shouldFireSelectField() {
+       String field = "id";
+       assertEquals("_id", parser.fireSelectField("entity", field));
+    }
+
+    @Test
+    void shouldFireSortProperty() {
+        String field = "id";
+        assertEquals("_id", parser.fireSortProperty("entity", field));
+    }
+
+
+    @Test
+    void shouldFireConditionField() {
+        String field = "id";
+        assertEquals("_id", parser.fireConditionField("entity", field));
     }
 }
