bc42ca720fe994a0e3210b5a2f70f49d800b803c
Otavio Santana <otaviopolianasantana@gmail.com>
2024-08-07 21:04:33 +0100

    feat: generate field and paramter to map
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ jnosql-mapping/jnosql-mapping-api-core/src/main/java/org/eclipse/jnosql/mapping/metadata/MapParameterMetaData.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the Eclipse Public License v1.0
+ *   and Apache License v2.0 which accompanies this distribution.
+ *   The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ *   and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ *   You may elect to redistribute this code under either of these licenses.
+ *
+ *   Contributors:
+ *
+ *   Otavio Santana
+ */
+package org.eclipse.jnosql.mapping.metadata;
+
+import java.util.Map;
+
+/**
+ * The MapParameterMetaData interface extends the {@link ParameterMetaData} interface and provides
+ * additional information about a parameter with a generic type for maps.
+ *
+ * <p>This interface is used to represent parameters of generic types, where the type may be a map
+ * containing keys and values of specific types.</p>
+ *
+ * @see ParameterMetaData
+ */
+public interface MapParameterMetaData extends ParameterMetaData {
+
+    /**
+     * Returns the {@link Class} representing the type of keys in the map.
+     *
+     * @return the key type of the map parameter
+     */
+    Class<?> keyType();
+
+    /**
+     * Returns the {@link Class} representing the type of values in the map.
+     *
+     * @return the value type of the map parameter
+     */
+    Class<?> valueType();
+
+    /**
+     * Returns an instance of the {@link Map} interface representing the map type
+     * for the parameter.
+     *
+     * @return an instance of the map type for the parameter
+     */
+    Map<?, ?> mapInstance();
+}
