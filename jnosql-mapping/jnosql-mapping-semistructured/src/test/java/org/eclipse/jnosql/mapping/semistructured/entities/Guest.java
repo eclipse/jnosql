eb28840dd29d7a3f8b3cb1c237eb67d63c8908d2
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-15 04:55:42 +0100

    feat: include a converter case when it is flat
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/entities/Guest.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.jnosql.mapping.semistructured.entities;
-
-import jakarta.nosql.Column;
-import jakarta.nosql.Embeddable;
-
-@Embeddable
-public record Guest (@Column String documentNumber, @Column String name) {
-}
--- jnosql-mapping/jnosql-mapping-semistructured/src/test/java/org/eclipse/jnosql/mapping/semistructured/entities/Room.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.jnosql.mapping.semistructured.entities;
-
-import jakarta.nosql.Column;
-import jakarta.nosql.Entity;
-import jakarta.nosql.Id;
-
-@Entity
-public record Room (@Id int number, @Column Guest guest) {
-}
